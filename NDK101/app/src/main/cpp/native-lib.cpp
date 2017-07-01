7f4d73a68d3818f27b5971d516e4834289e0badb
songzhw <songzhw2012@gmail.com>
2017-07-01 19:03:13 -0400

    add c++

---
+++ NDK101/app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,8 @@
+#include <jni.h>
+#include <string>
+
+extern "C" jstring Java_ca_six_ndk101_MainActivity_stringFromJNI(
+        JNIEnv* env, jobject /* this */) {
+    std::string hello = "Hello from C++";
+    return env->NewStringUTF(hello.c_str());
+}
