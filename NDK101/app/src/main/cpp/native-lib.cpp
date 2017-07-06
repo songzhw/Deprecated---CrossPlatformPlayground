6fa375d9b76b991703c9a42b10abaee2db269f1b
songzhw <songzhw2012@gmail.com>
2017-07-05 21:34:10 -0400

    jni

--- NDK101/app/src/main/cpp/native-lib.cpp
+++ NDK101/app/src/main/cpp/native-lib.cpp
@@ -1,6 +1,11 @@
 #include <jni.h>
 #include <string>
+#include <map>
 #include <android/log.h>
+using namespace std;
+
+// 注意: 全局变量只能声明,不能赋值. 所以赋值语句要写在JNI_OnLoad()里面去
+map<int, string> player;
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -13,19 +18,17 @@ Java_ca_six_ndk101_MainActivity_stringFromJNI(
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved){
     __android_log_print(ANDROID_LOG_ERROR, "szw", "%s", "native onLoad()");
+    player[23] = "Jorden";
+    player[33] = "Kobe";
     return JNI_VERSION_1_6;
 }
 
-// 不知道何时被调用, 一直没看到相关日志
-JNIEXPORT void JNICALL JNI_OnUnLoad(JavaVM* vm, void* reserved){
-    __android_log_print(ANDROID_LOG_ERROR, "szw", "%s", "native onUnLoad()");
-}
 
 extern "C"
 JNIEXPORT jstring JNICALL
 Java_ca_six_ndk101_MainActivity_getValue(
     JNIEnv* env, jobject /* this */, jint key) {
-    char* chars = new char[30];
-    sprintf(chars, "a key from %d", key);
+    char* chars = new char[50];
+    sprintf(chars, "a key from %d -> %s", key,  player[key].c_str());
     return env->NewStringUTF(chars);
 }
