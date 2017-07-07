1cbe6dd4258d9ac5429d57ef2ad254b3636f2b73
songzhw <songzhw2012@gmail.com>
2017-07-06 21:42:23 -0400

    jni : error

--- NDK101/app/src/main/cpp/native-lib.cpp
+++ NDK101/app/src/main/cpp/native-lib.cpp
@@ -6,8 +6,7 @@ using namespace std;
 
 #define logd(...) __android_log_print(ANDROID_LOG_DEBUG, "szw", __VA_ARGS__)
 
-// 注意: 全局变量只能声明,不能赋值. 所以赋值语句要写在JNI_OnLoad()里面去
-map<int, string> player;
+map<int, jobject> player;
 
 extern "C"
 JNIEXPORT jstring JNICALL
@@ -20,27 +19,15 @@ Java_ca_six_ndk101_MainActivity_stringFromJNI(
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void* reserved){
     logd("native onLoad()");
-    player[23] = "Jorden";
-    player[33] = "Kobe";
     return JNI_VERSION_1_6;
 }
 
-
-extern "C"
-JNIEXPORT void JNICALL
-Java_ca_six_ndk101_MainActivity_setValue(JNIEnv *env, jobject instance, jint key, jstring value_) {
-    const char *value = env->GetStringUTFChars(value_, 0);
-
-    string valueInMap(value);
-    player[key] = valueInMap;
-
-    env->ReleaseStringUTFChars(value_, value);
+JNIEXPORT jobject JNICALL
+Java_ca_six_ndk101_MainActivity_getValue(JNIEnv *env, jobject instance, jint key) {
+    return player[key];
 }
 
-extern "C"
-JNIEXPORT jstring JNICALL
-Java_ca_six_ndk101_MainActivity_getValue(JNIEnv* env, jobject /* this */, jint key) {
-    char* chars = new char[50];
-    sprintf(chars, "a key from %d -> %s", key,  player[key].c_str());
-    return env->NewStringUTF(chars);
+JNIEXPORT void JNICALL
+Java_ca_six_ndk101_MainActivity_setValue(JNIEnv *env, jobject instance, jint key, jobject value) {
+    player[key] = value;
 }
--- NDK101/app/src/main/java/ca/six/ndk101/MainActivity.java
+++ NDK101/app/src/main/java/ca/six/ndk101/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import ca.six.ndk101.model.Player;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -11,22 +13,19 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setValue(33,"Shark");
-        setValue(13, "Songzhw");
-
+        setValue(33, new Player(33, "Shark"));
+        setValue(13, new Player(13, "Songzhw"));
 
         TextView tv = (TextView)findViewById(R.id.sample_text);
         StringBuilder sb = new StringBuilder();
-        sb.append(stringFromJNI()+"\n");
         sb.append(getValue(23)+"\n");
         sb.append(getValue(13)+"\n");
         sb.append(getValue(1)+"\n"); //=> get a null
         tv.setText(sb.toString());
     }
 
-    public native String stringFromJNI();
-    public native String getValue(int key);
-    public native void setValue(int key, String value);
+    public native Player getValue(int key);
+    public native void setValue(int key, Player value);
 
     static {
         System.loadLibrary("native-lib");
