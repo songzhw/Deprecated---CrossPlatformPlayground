84a333e8a320165fb21a7efdada56c453f07a2c2
songzhw <songzhw2012@gmail.com>
2018-07-31 14:33:56 -0400

    add interactivity

--- Flutter/first_flutter/first_flutter/lib/RandomWordsStates.dart
+++ Flutter/first_flutter/first_flutter/lib/RandomWordsStates.dart
@@ -2,44 +2,56 @@ import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
 
 class RandomWordsWidget extends StatefulWidget {
-    @override
-    RandomWordsState createState() => new RandomWordsState();
+  @override
+  RandomWordsState createState() => new RandomWordsState();
 }
 
 class RandomWordsState extends State<RandomWordsWidget> {
-    final Set<WordPair> _saved = new Set<WordPair>();
-    final List<WordPair> _suggestions = <WordPair>[];
-    final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = new Set<WordPair>();
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
 
-    @override
-    Widget build(BuildContext context) {
-        return _buildSuggestions();
-    }
+  @override
+  Widget build(BuildContext context) {
+    return _buildSuggestions();
+  }
 
-    Widget _buildSuggestions() {
-        return new ListView.builder(
-            padding: const EdgeInsets.all(16.0),
-            itemBuilder: (BuildContext _context, int pos){
-                if(pos.isOdd) {
-                    return new Divider();
-                }
-                final int index = pos ~/ 2;
-                if(index >= _suggestions.length){
-                    _suggestions.addAll(generateWordPairs().take(10));
-                }
-                return _buildRow(_suggestions[index]);
-            }
-        );
-    }
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int pos) {
+          if (pos.isOdd) {
+            return new Divider();
+          }
+          final int index = pos ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
 
-    Widget _buildRow(WordPair wordPair){
-        final bool isAlreadySaved = _saved.contains(wordPair);
-        return new ListTile(
-            title: new Text(wordPair.asPascalCase, style: _biggerFont),
-            trailing: new Icon(
-                isAlreadySaved ? Icons.favorite : Icons.favorite_border,
-                color: isAlreadySaved ? Colors.red : null,
-            ),
-        );
+  Widget _buildRow(WordPair wordPair) {
+    final bool isAlreadySaved = _saved.contains(wordPair);
+    return new ListTile(
+      title: new Text(wordPair.asPascalCase, style: _biggerFont),
+      trailing: new Icon(
+        isAlreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: isAlreadySaved ? Colors.red : null,
+      ),
+      onTap: (){
+        setState( (){ //react式. 调用setState()会引起State.build()的方法被调用 (类似MVVM)
+          toggle(isAlreadySaved, wordPair);
+        });
+      },
+    );
+  }
+
+  void toggle(bool isSaved, WordPair wordPair) {
+    if (isSaved) {
+      _saved.remove(wordPair);
+    } else {
+      _saved.add(wordPair);
     }
+  }
 }
