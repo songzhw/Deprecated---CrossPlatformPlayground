f0f449359b3cde9b1e093d892b2f377d2efe943c
songzhw <songzhw2012@gmail.com>
2018-08-02 10:04:05 -0400

    textfield & controller

--- Flutter/friendly_chat/friendly_chat/lib/main.dart
+++ Flutter/friendly_chat/friendly_chat/lib/main.dart
@@ -9,13 +9,40 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class ChatScreen extends StatelessWidget{
+class ChatScreen extends StatefulWidget {
+  @override
+  State createState() => new ChatState();
+}
+
+class ChatState extends State<ChatScreen>{
+  var _textController = new TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     var scaffold = new Scaffold(
       appBar: new AppBar(title: new Text("FriendlyChat"),),
+      body: _buildTextComposer(),
     );
     return scaffold;
   }
+
+  Widget _buildTextComposer(){
+    var textField = new TextField(
+      controller: _textController,
+      onSubmitted: _onSumit,
+      decoration: new InputDecoration.collapsed(hintText: "Type to sned"),
+    );
+
+    return new Container(
+      margin: const EdgeInsets.symmetric(horizontal: 8.0), //单位相当于andorid中的dp
+      child: textField,
+    );
+
+  }
+
+  void _onSumit(String text) {
+    _textController.clear();
+  }
+
 
 }
