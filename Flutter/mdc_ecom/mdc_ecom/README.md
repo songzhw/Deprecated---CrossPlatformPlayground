4702522605de2dce23ab336397f7db5c5367b448
songzhw <songzhw2012@gmail.com>
2018-08-06 15:22:41 -0400

    init project

---
+++ Flutter/mdc_ecom/mdc_ecom/README.md
@@ -0,0 +1,8 @@
+# mdc_ecom
+
+e-commerce
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.io/).
---
+++ Flutter/mdc_ecom/mdc_ecom/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:mdc_ecom/login_page.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      home: new Scaffold(
+          appBar: AppBar(title: Text("test"),),
+          body: Image.asset('assets/diamond.png')
+
+      ),
+    );
+  }
+}
