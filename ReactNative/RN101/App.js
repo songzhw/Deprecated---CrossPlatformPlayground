fffdb11f7a166ebccc5444f7ad443909361565de
songzhw <songzhw2012@gmail.com>
2018-09-22 11:02:20 -0400

    flex

--- ReactNative/RN101/App.js
+++ ReactNative/RN101/App.js
@@ -13,7 +13,7 @@ export default class App extends Component {
       //   <Greeting name="Java" id="100" />
       //   <Blink myText="I am React Native" />
       // </View>
-      <View style={{flex:1}}>
+      <View style={{height:300}}>
         <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
         <View style={{flex: 2, backgroundColor: 'skyblue'}}/>
         <View style={{flex: 3, backgroundColor: 'steelblue'}}/>
