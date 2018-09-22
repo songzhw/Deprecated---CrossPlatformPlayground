414a8b42bbe8ffd3178540834545be536e5e91ce
songzhw <songzhw2012@gmail.com>
2018-09-21 20:01:28 -0400

    state is the data that would change later

--- ReactNative/RN101/App.js
+++ ReactNative/RN101/App.js
@@ -7,11 +7,11 @@ export default class App extends Component {
 
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Hello World: </Text>
+        <Text>Hello World </Text>
         <Image source={pic} style={{ width: 386, height: 220 }} />
         <Greeting name="React Native" id="23"/>
         <Greeting name="Java" id="100"/>
+        <Blink myText="I am React Native"/>
       </View>
     );
   }
@@ -30,6 +30,27 @@ class Greeting extends Component {
   render() {
     return (
       <Text> Hello : {this.props.name}  - {this.props.id} </Text>
+    )
+  }
+}
+
+// may use Redux or Mobx to modify the state rather than calling "setState()" directly
+class Blink extends Component {
+  constructor(props){
+    super(props)
+    this.state = {isShowingText : true}
+
+    setInterval( () => {
+      this.setState( oldState => {
+        return {isShowingText: !oldState.isShowingText}
+      })
+    }, 1200)
+  }
+
+  render(){
+    let display = this.state.isShowingText ? this.props.myText : ' '
+    return (
+      <Text> {display} </Text>
     )
   }
 }
