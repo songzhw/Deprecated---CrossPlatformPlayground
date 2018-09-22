bcba07b753a60b228d86b7aadaa97cbe692c6143
songzhw <songzhw2012@gmail.com>
2018-09-22 11:01:56 -0400

    flex

--- ReactNative/RN101/App.js
+++ ReactNative/RN101/App.js
@@ -1,17 +1,22 @@
 import React, { Component } from 'react';
-import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default class App extends Component {
   render() {
     let pic = { uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg' }
 
     return (
-      <View style={styles.container}>
-        <Text style={{ color: 'red', fontSize: 33 }}>Hello World </Text>
-        <Image source={pic} style={{ width: 386, height: 220 }} />
-        <Greeting name="React Native" id="23" />
-        <Greeting name="Java" id="100" />
-        <Blink myText="I am React Native" />
+      // <View style={styles.container}>
+      //   <Text style={{ color: 'red', fontSize: 33 }}>Hello World </Text>
+      //   <Image source={pic} style={{ width: 386, height: 220 }} />
+      //   <Greeting name="React Native" id="23" />
+      //   <Greeting name="Java" id="100" />
+      //   <Blink myText="I am React Native" />
+      // </View>
+      <View style={{flex:1}}>
+        <View style={{flex: 1, backgroundColor: 'powderblue'}}/>
+        <View style={{flex: 2, backgroundColor: 'skyblue'}}/>
+        <View style={{flex: 3, backgroundColor: 'steelblue'}}/>
       </View>
     );
   }
