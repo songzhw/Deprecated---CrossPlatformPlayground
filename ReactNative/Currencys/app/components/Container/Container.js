ea9144ed07a6393cfbb43d29b4fb776719b94699
songzhw <songzhw2012@gmail.com>
2018-10-12 23:12:17 -0400

    one error I got it before

--- ReactNative/Currencys/app/components/Container/Container.js
+++ ReactNative/Currencys/app/components/Container/Container.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types' // no longer belong to the 'react' framework
 import { View } from 'react-native'
 import styles from './styles'
 
