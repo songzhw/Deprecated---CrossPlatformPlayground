b132af9c0c4c8a772eeef74d75dcd1618c52b63e
songzhw <songzhw2012@gmail.com>
2018-10-14 19:04:05 -0400

    prefectch image

--- ReactNative/Currencys/app/components/Logo/Logo.js
+++ ReactNative/Currencys/app/components/Logo/Logo.js
@@ -3,12 +3,24 @@ import { View, Text, ImageBackground, Image } from 'react-native'
 
 import styles from './styles'
 
+let cached = Image.prefetch('https://facebook.github.io/react/logo-og.png')
+cached.then(
+  () => {
+    console.log('Load successfully')
+  },
+  (error) => {
+    console.log('load error'+error)
+  }
+)
+
 const Logo = () => (
   <View style={styles.container}>
     <Image
       source={{ uri: 'https://facebook.github.io/react/logo-og.png' }}
       style={{ width: 200, height: 200 }}
-      blurRadius={5}
+      onLoadStart={() => console.log('start')}
+      onLoad={() => console.log('load')}
+      onLoadEnd={() => console.log('end')}
     />
     <Text style={styles.text}> Currency Converter</Text>
   </View>
