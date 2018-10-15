1c4ea86b1c42fd56d922f58dde0a4df2bad0d9b9
Zhengwang Song <zhengwang.song@rakuten.com>
2018-10-15 09:24:21 -0400

    refactor the Home screen

--- ReactNative/Currencys/app/screens/Home.js
+++ ReactNative/Currencys/app/screens/Home.js
@@ -1,13 +1,24 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { StatusBar } from 'react-native'
 
 import { Container } from '../components/Container'
 import { Logo } from '../components/Logo'
 
-export default () => (
-  <Container>
-    <StatusBar translucent={false} barStyle="light-content" />
-    <Logo />
-  </Container>
-)
+const BASE_CURRENCEY = 'CAD'
+const QUOTE_CURRENCY = 'RMB'
+const BASE_PRICE = '100'
+const QUOTE_PRICE = '5.1'
+
+class Home extends Component {
+  render() {
+    return (
+      <Container>
+        <StatusBar translucent={false} barStyle="light-content" />
+        <Logo />
+      </Container>
+    )
+  }
+}
+
+export default Home
 // vs light这个theme中 html.attribute是f00红色, 不是错误, 注意哦
