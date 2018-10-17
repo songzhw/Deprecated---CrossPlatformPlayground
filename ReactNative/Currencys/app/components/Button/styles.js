4e74ad1f2e94b74abd594e83466315205f7820d6
songzhw <songzhw2012@gmail.com>
2018-10-16 20:57:18 -0400

    add styles

---
+++ ReactNative/Currencys/app/components/Button/styles.js
@@ -0,0 +1,18 @@
+import ExtStyles from 'react-native-extended-stylesheet'
+
+export default ExtStyles.create({
+  container: {
+    flexDirection: 'row',
+    marginTop: 30,
+  },
+  image: {
+    width: 30,
+    height: 30,
+    marginRight: 20,
+  },
+  text: {
+    fontSize: 17,
+    fontWeight: '300',
+    color: '$white',
+  },
+})
