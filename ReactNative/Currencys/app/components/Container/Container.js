b729faf84509e158e9ce134f9cf17e96b79cd83f
songzhw <songzhw2012@gmail.com>
2018-10-16 20:30:02 -0400

    close keyboard when tapping outside of the text input

--- ReactNative/Currencys/app/components/Container/Container.js
+++ ReactNative/Currencys/app/components/Container/Container.js
@@ -1,12 +1,14 @@
 import PropTypes from 'prop-types' // no longer belong to the 'react' framework
 import React from 'react'
-import { View } from 'react-native'
+import { View, TouchableWithoutFeedback, Keyboard } from 'react-native'
 import styles from './styles'
 
 const Container = ({ children }) => (
-  <View style={styles.container}>
-    {children}
-  </View>
+  // 增加TouchableWithoutFeedback, 是想在键盘弹出时, 在键盘外点击就能dismiss软键盘
+  // 不带<View>就会报错, 说React.Children only expected to receive a single React element child
+  <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <View style={styles.container}>{children}</View>
+  </TouchableWithoutFeedback>
 )
 
 Container.propTypes = {
