af3f53b1fe55b9cf6cf49b59a076f6030b523dc4
songzhw <songzhw2012@gmail.com>
2018-10-20 12:11:42 -0400

    wrap_content size

--- ReactNative/Currencys/App.js
+++ ReactNative/Currencys/App.js
@@ -1,3 +1,17 @@
 import App from './app/index'
 
-export default App
+import React from 'react'
+import { View, ScrollView, Text, StatusBar } from 'react-native'
+const height = StatusBar.currentHeight
+const Demo = () => (
+  <ScrollView style={{ flex: 1 }}>
+    <View style={{ alignSelf: 'baseline' }}>
+      <Text style={{ backgroundColor: '#f00', marginTop: height }}>
+        What a lovely day!
+      </Text>
+    </View>
+    <Text style={{ height: 1000 }}> New things</Text>
+  </ScrollView>
+)
+
+export default Demo
