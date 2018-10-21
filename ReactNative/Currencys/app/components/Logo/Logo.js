280ba8fd17cff719ad21ee3e3f62a4df4e540b80
songzhw <songzhw2012@gmail.com>
2018-10-20 21:39:59 -0400

    animate the logo: change the width/height dynamically

--- ReactNative/Currencys/app/components/Logo/Logo.js
+++ ReactNative/Currencys/app/components/Logo/Logo.js
@@ -1,23 +1,45 @@
-import React from 'react'
-import { View, Text, ImageBackground, Image } from 'react-native'
+import React, { Component } from 'react'
+import {
+  View,
+  Text,
+  Keyboard,
+  Animated,
+  Platform,
+  StyleSheet,
+} from 'react-native'
 
 import styles from './styles'
 
-const Logo = () => (
-  <View style={styles.container}>
-    <ImageBackground
-      resizeMode="contain"
-      style={styles.containerImage}
-      source={require('./images/background.png')}
-    >
-      <Image
-        resizeMode="contain"
-        style={styles.logo}
-        source={require('./images/logo.png')}
-      />
-    </ImageBackground>
-    <Text style={styles.text}>Currencey Converter</Text>
-  </View>
-)
+const ANIMATION_DURATION = 300
+
+class Logo extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      containerSize: new Animated.Value(styles.$maxContainerSize),
+      imageSize: new Animated.Value(styles.$maxImageSize),
+    }
+  }
+
+  render() {
+    const { containerSize, imageSize } = this.state
+
+    return (
+      <View style={styles.container}>
+        <Animated.View>
+          <Animated.Image
+            resizeMode="contain"
+            source={require('./images/background.png')}
+          />
+          <Animated.Image
+            resizeMode="contain"
+            source={require('./images/logo.png')}
+          />
+        </Animated.View>
+        <Text style={styles.text}>Currency Converter</Text>
+      </View>
+    )
+  }
+}
 
 export default Logo
--- ReactNative/Currencys/app/components/Logo/styles.js
+++ ReactNative/Currencys/app/components/Logo/styles.js
@@ -1,20 +1,24 @@
 import { Dimensions } from 'react-native'
 import ExtStyle from 'react-native-extended-stylesheet'
 
-const imageWidth = Dimensions.get('window').width / 2
+const LogoSize = Dimensions.get('window').width / 2
 
 export default ExtStyle.create({
+  $minContainerSize: LogoSize / 2,
+  $minImageSize: LogoSize / 4,
+  $maxContainerSize: LogoSize,
+  $maxImageSize: LogoSize / 2,
   container: {
     alignItems: 'center',
   },
   containerImage: {
     alignItems: 'center',
     justifyContent: 'center',
-    width: imageWidth,
-    height: imageWidth,
+    width: '$maxContainerSize',
+    height: '$maxContainerSize',
   },
   logo: {
-    width: imageWidth / 2,
+    width: '$maxImageSize',
   },
   text: {
     color: '$white',
