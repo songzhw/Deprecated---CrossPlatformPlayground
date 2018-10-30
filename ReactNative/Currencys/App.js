43ae0dcd8919ece56371ae6d32801fd1af9ae44f
songzhw <songzhw2012@gmail.com>
2018-10-29 20:57:07 -0400

    communicate between parent and child components

--- ReactNative/Currencys/App.js
+++ ReactNative/Currencys/App.js
@@ -1,15 +1,34 @@
-import App from './app/index'
+// import App from './app/index'
 
-// import React from 'react'
-// import { View, ScrollView, Text, StatusBar } from 'react-native'
-// const height = StatusBar.currentHeight
-// const Demo = () => (
-//   <ScrollView style={{ flex: 1, backgroundColor: 'blue' }}>
-//     <Text style={{ backgroundColor: '#f00', marginTop: height }}>
-//       What a lovely day!
-//     </Text>
-//   </ScrollView>
-// )
-// export default Demo
+import React, { Component } from 'react'
+import { View, Text, StatusBar, Button } from 'react-native'
+import Child from './temp'
 
-export default App
+class Parent extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: 'child->parent',
+    }
+  }
+  
+  onChildClick(name) {
+    this.setState({ name: name })
+  }
+  
+  render() {
+    return (
+      <View style={{ flex: 1 }}>
+        <StatusBar translucent={false} barStyle="default"/>
+        <Child desp={this.state.name}
+               changeChild={value => this.onChildClick(value)}
+        />
+      </View>
+    )
+  }
+}
+
+
+export default Parent
+
+// export default App
---
+++ ReactNative/Currencys/temp.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+import { View, Text, StatusBar, Button } from 'react-native'
+
+
+class Child extends Component {
+  constructor(props) {
+    super(props)
+  }
+  
+  click() {
+    this.props.changeChild('newName')
+  }
+  
+  render() {
+    return (
+      <View>
+        <Text> Hello {this.props.desp} </Text>
+        <Button
+          title="click => pass event to parents"
+          onPress={this.click}
+        />
+      </View>
+    )
+  }
+}
+
+export default Child
