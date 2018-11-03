dfc1b2510cb1f45b06ad3758353e504dbe468110
songzhw <songzhw2012@gmail.com>
2018-11-02 22:09:16 -0400

    get data from API

--- ReactNative/MyCurrency/App.js
+++ ReactNative/MyCurrency/App.js
@@ -4,21 +4,30 @@ import {StyleSheet, Text, View, FlatList} from 'react-native';
 // 1. Home页, 只有一个ListView
 class App extends React.Component {
 
+  constructor(props){
+    super(props)
+    this.state = {list: []}
+  }
 
   componentDidMount() {
-
+    fetch("http://fixer.handlebarlabs.com/latest?base=CAD")
+      .then(resp => resp.json())
+      .then(respBody => Object.keys(respBody.rates) )
+      .then(currencies => {
+        this.setState(prevState => ({list: currencies}))
+      })
   }
 
   render() {
-    let currencies = {"base":"CAD","date":"2018-09-06","rates":{"AUD":1.0539,"BGN":1.2751,"BRL":3.1241,"CHF":0.7351,"CNY":5.18,"CZK":16.766,"DKK":4.8616,"EUR":0.65198,"GBP":0.58563,"HKD":5.9541,"HRK":4.8469,"HUF":212.86,"IDR":11295,"ILS":2.7191,"INR":54.582,"ISK":83.322,"JPY":84.463,"KRW":850.67,"MXN":14.582,"MYR":3.1373,"NOK":6.3737,"NZD":1.1496,"PHP":40.808,"PLN":2.8154,"RON":3.0242,"RUB":51.881,"SEK":6.9049,"SGD":1.0432,"THB":24.86,"TRY":4.9734,"USD":0.75851,"ZAR":11.62}}
+    let {list} = this.state
 
     return <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <FlatList
-        data={Object.keys(currencies.rates)}
+        data={list}
         keyExtractor={item => item}
         renderItem={
-          ({item}) => <Text style={{fontSize: 32}}> {item} </Text>
+          ({item}) => <Text style={{fontSize: 22}}> {item} </Text>
         }
       />
     </View>;
