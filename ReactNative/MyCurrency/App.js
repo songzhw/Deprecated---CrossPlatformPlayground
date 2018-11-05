17dc3b7052897eaf260fc83da671f6ec4f70856c
songzhw <songzhw2012@gmail.com>
2018-11-04 20:04:38 -0500

    refactor

--- ReactNative/MyCurrency/App.js
+++ ReactNative/MyCurrency/App.js
@@ -1,10 +1,53 @@
-import React from 'react';
-import {Provider} from 'react-redux'
-import store from './app/redux/store'
-import Home from './app/screen/HomeScreen'
+import App from './app/index'
+export default App
 
-export default () => (
-  <Provider store={store}>
-    <Home/>
-  </Provider>
-)
+// import React, { Component } from 'react'
+// import { View, Text, StatusBar, Button } from 'react-native'
+// import Child from './temp'
+//
+// class Parent extends Component {
+//   constructor() {
+//     super()
+//     this.state = {
+//       name: 'child->parent',
+//     }
+//
+//     var names = [{ name: 'Bruce', age: 30 }, { name: 'Clark', age: 32 }]
+//
+//     var user = {
+//       names: [{ name: 'Kate', age: 11 }, { name: 'Jim', age: 12 }],
+//       selectOne: function() {
+//         let tmp = parseInt(Math.random() * 10)
+//         let randomIndex = tmp % 2
+//         let person = this.names[randomIndex]
+//         console.log(person)
+//       },
+//     }
+//
+//     user.selectOne() //结果不是Kate, 就是Jim
+//
+//     var another = user.selectOne
+//     console.log(this.names) //=> undefined
+//
+//   }
+//
+//   onChildClick(name) {
+//     this.setState({ name: name })
+//   }
+//
+//   render() {
+//     return (
+//       <View style={{ flex: 1 }}>
+//         <StatusBar translucent={false} barStyle="default"/>
+//         <Child desp={this.state.name}
+//                changeChild={value => this.onChildClick(value)}
+//         />
+//       </View>
+//     )
+//   }
+// }
+//
+//
+// export default Parent
+
+
---
+++ ReactNative/yyy/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Provider} from 'react-redux'
+import store from './app/redux/store'
+import Home from './app/screen/HomeScreen'
+
+export default () => (
+  <Provider store={store}>
+    <Home/>
+  </Provider>
+)
