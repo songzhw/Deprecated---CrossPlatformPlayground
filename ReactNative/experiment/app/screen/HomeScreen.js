57b74d67677aa411e16030522e9e432d84723e7f
songzhw <songzhw2012@gmail.com>
2018-11-10 14:37:39 -0500

    odd - styles does not work

--- ReactNative/experiment/app/screen/HomeScreen.js
+++ ReactNative/experiment/app/screen/HomeScreen.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
-import {View, Button} from 'react-native'
-import saga from "../redux/saga/saga";
+import {ScrollView, Button, StyleSheet} from 'react-native'
 
 class HomeScreen extends Component {
-  render(){
+  render() {
     return (
-      <View>
-        <Button title="Redux-saga" onPress={this.go2ReduxSaga}/>
-      </View>
+      <ScrollView style={{flex: 1}}>
+        <Button style={styles.aButton} title="Redux-saga" onPress={this.go2ReduxSaga}/>
+        <Button style={styles.aButton} title="App-State" onPress={this.go2AppState}/>
+      </ScrollView>
     )
   }
 
-  go2ReduxSaga = ()=> {
+  go2ReduxSaga = () => {
     this.props.navigation.navigate('Redux_Saga')
   }
+
+  go2AppState = () => {
+    this.props.navigation.navigate('App_State')
+  }
+
 }
+
+const styles = StyleSheet.create({
+  aButton: {
+    marginTop: 20,
+    fontSize: 30,
+    color: 'red'
+  }
+})
 
 export default HomeScreen
