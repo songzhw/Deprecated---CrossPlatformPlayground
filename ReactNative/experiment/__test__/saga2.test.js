e5ca562eb742f747938dfec773a4f390972fdf4b
songzhw <songzhw2012@gmail.com>
2018-11-19 14:41:00 -0500

    throw an error in test

--- ReactNative/experiment/__test__/saga2.test.js
+++ ReactNative/experiment/__test__/saga2.test.js
@@ -1,9 +1,11 @@
-import { call} from 'redux-saga/effects'
+import { call } from 'redux-saga/effects'
 import { expectSaga } from 'redux-saga-test-plan'
 import * as matchers from 'redux-saga-test-plan/matchers'
+import { throwError } from 'redux-saga-test-plan/providers'
 
 import { api } from '../app/temp/saga/api'
 import { petSaga } from '../app/temp/saga/saga2'
+
 
 
 test('02 mock', () => {
@@ -16,6 +18,17 @@ test('02 mock', () => {
       [call(api.fetchUser, 999), fakeUser],
       [matchers.call.fn(api.fetchPet), fakePet],
     ])
-    .put({type:'RECEIVE_PET', payload:{user: fakeUser, pet: fakePet}})
+    .put({ type: 'RECEIVE_PET', payload: { user: fakeUser, pet: fakePet } })
+    .run()
+})
+
+test('03 handle errors', () => {
+  const error = new Error('error')
+  return expectSaga(petSaga)
+    .dispatch({ type: 'REQUEST_PET', payload: 888 })
+    .provide([
+      [matchers.call.fn(api.fetchUser), throwError(error)],
+    ])
+    .put({ type: 'FAIL_FETCH', error })
     .run()
 })
