218b788c9d66c431d33824871def80b36c675f99
zsong <zhengwang.song@rakuten.com>
2018-11-25 14:23:07 -0500

    learn f8 app

--- ReactNative/Open_Src_Apps/1ibrary-front-end/js/SplashScreen.js
+++ ReactNative/Open_Src_Apps/1ibrary-front-end/js/SplashScreen.js
@@ -1,81 +1,79 @@
-import React, { Component } from 'react'
-import {
-  View
-} from 'react-native'
+import React, {Component} from 'react'
+import {View} from 'react-native'
 import RNSplashScreen from 'react-native-splash-screen'
-import { Actions } from 'react-native-router-flux'
-import { SCENE_INDEX, SCENE_LOGIN } from './constants/scene'
+import {Actions} from 'react-native-router-flux'
+import {SCENE_INDEX, SCENE_LOGIN} from './constants/scene'
 import Storage from './common/storage'
-import { setApiBaseUrl, setToken } from './network/HttpUtils'
+import {setApiBaseUrl, setToken} from './network/HttpUtils'
 import schools from './network/schools'
 import login from './network/login'
 import Toast from 'antd-mobile/lib/toast'
-import { connect } from 'react-redux'
-import { delay } from 'redux-saga'
+import {connect} from 'react-redux'
+import {delay} from 'redux-saga'
 import initApp from './redux/modules/init'
 import {isDev} from './common/util';
 
-function mapStateToProps (state) {
-  return {
-    user: state.user
-  }
+function mapStateToProps(state) {
+    return {
+        user: state.user
+    }
 }
 
 @connect(mapStateToProps)
 class SplashScreen extends Component {
 
-  async componentDidMount () {
-    const user = await Storage.get('user', {})
-    if (!user.account || !user.password) {
-      Actions[SCENE_LOGIN]()
-      RNSplashScreen.hide()
-      return
+    async componentDidMount() {
+        const user = await Storage.get('user', {})
+        if (!user.account || !user.password) {
+            Actions[SCENE_LOGIN]()
+            RNSplashScreen.hide()
+            return
+        }
+
+        await delay(200)
+
+        setApiBaseUrl(schools[user.school_id].host)
+
+        const {
+            uid,
+            token,
+            timestamp
+        } = this.props.user
+
+        setToken({
+            uid,
+            token,
+            timestamp
+        })
+
+        this.props.dispatch(initApp())
+
+        try {
+            login(user.account, user.password, user.school_id)
+            Actions[SCENE_INDEX]()
+        } catch (e) {
+            console.log(e)
+            Toast.fail('自动登录失败', 1.5)
+            Actions[SCENE_LOGIN]()
+        }
+
+        RNSplashScreen.hide()
     }
 
-    await delay(200)
-
-    setApiBaseUrl(schools[user.school_id].host)
-
-    const {
-      uid,
-      token,
-      timestamp
-    } = this.props.user
-
-    setToken({
-      uid,
-      token,
-      timestamp
-    })
-
-    this.props.dispatch(initApp())
-
-    try {
-      login(user.account, user.password, user.school_id)
-      Actions[SCENE_INDEX]()
-    } catch (e) {
-      console.log(e)
-      Toast.fail('自动登录失败', 1.5)
-      Actions[SCENE_LOGIN]()
+    render() {
+        return <View/>
     }
-
-    RNSplashScreen.hide()
-  }
-
-  render () {
-    return <View />
-  }
 }
 
 export default SplashScreen
 
 if (isDev) {
-  global.XMLHttpRequest = global.originalXMLHttpRequest
-    ? global.originalXMLHttpRequest
-    : global.XMLHttpRequest
-  global.FormData = global.originalFormData
-    ? global.originalFormData
-    : global.FormData
+    global.XMLHttpRequest = global.originalXMLHttpRequest
+        ? global.originalXMLHttpRequest
+        : global.XMLHttpRequest
+    global.FormData = global.originalFormData
+        ? global.originalFormData
+        : global.FormData
 }
 
 console.disableYellowBox = true
@@ -87,17 +85,18 @@ console.disableYellowBox = true
  * 以下补丁同理
  */
 if (!(console.dir instanceof Function)) {
-  console.dir = console.log
+    console.dir = console.log
 }
 
 if (!(console.time instanceof Function)) {
-  console.time = console.log
+    console.time = console.log
 }
 
 if (!(console.timeEnd instanceof Function)) {
-  console.timeEnd = console.log
+    console.timeEnd = console.log
 }
 
 if (!global.URL) {
-  global.URL = function () {}
+    global.URL = function () {
+    }
 }
