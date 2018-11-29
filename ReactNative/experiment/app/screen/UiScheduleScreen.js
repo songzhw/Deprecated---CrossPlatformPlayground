b5f07d7be0a167e4d93540f15b1a8128b5b823e2
songzhw <songzhw2012@gmail.com>
2018-11-29 10:28:51 -0500

    learn ViewPagerAndroid (the viewPager needs a {flex:1} style, otherwise nothing is displaying

--- ReactNative/experiment/app/screen/UiScheduleScreen.js
+++ ReactNative/experiment/app/screen/UiScheduleScreen.js
@@ -1,19 +1,36 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Text } from 'react-native'
+import { View, StyleSheet, Text, ViewPagerAndroid } from 'react-native'
 import { connect } from 'react-redux'
 import ViewPager from '../component/ViewPager'
 
 class UiScheduleScreen extends Component {
   render() {
     return (
-      <View>
-        <ViewPager
-        />
-      </View>
+      <ViewPagerAndroid
+        style={styles.viewPager}
+        initialPage={0}>
+        <View style={styles.pageStyle}>
+          <Text>First page</Text>
+        </View>
+        <View style={styles.pageStyle}>
+          <Text>Second page</Text>
+        </View>
+        <View style={styles.pageStyle}>
+          <Text>Third page</Text>
+        </View>
+      </ViewPagerAndroid>
     )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  viewPager: {
+    flex: 1
+  },
+  pageStyle: {
+    alignItems: 'center',
+    padding: 20,
+  }
+})
 
 export default connect()(UiScheduleScreen)
