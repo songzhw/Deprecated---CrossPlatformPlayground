4b72d01736d0a1fcfde463ca380920596d5f399d
songzhw <songzhw2012@gmail.com>
2018-11-30 16:22:48 -0500

    still error: szw state = {"reducerCurrency":{"base":"USD","quote":"CAD","conversion":{},"error":null,"payload":{"newText":"default from reducer"}},"reducerSchedule":{"schedules":{}}}

--- ReactNative/experiment/app/redux/reducer_schedule.js
+++ ReactNative/experiment/app/redux/reducer_schedule.js
@@ -1,10 +1,14 @@
-import {GOT_SCHEDULE} from './action_schedule'
+import { GOT_SCHEDULE } from './action_schedule'
 
-export default (state = {}, action) => {
-  switch(action.type){
+const initialState = {
+  schedules: {},
+}
+
+export default (state = initialState, action) => {
+  switch (action.type) {
     case GOT_SCHEDULE:
       console.log(`szw schedule data =     ${action.data}`)
-      return { ...state, schedules: action.data}
+      return { ...state, schedules: action.data }
     default:
       return state
   }
