a4e26ffa99f2b475027116bbe495a9e6a9a4f521
songzhw <songzhw2012@gmail.com>
2018-12-02 14:17:21 -0500

    once I added the combineReducers, the state I got in LoginScreen became " mapStateToProps: {"reduceSession":{"succ":true,"session":"dadf907a-f5dc-11e8-8eb2-f2801f1b9fd1"},"reduceSchedule":{"day":1,"schedules":[]}} "

--- ReactNative/ex3/js/redux/store.js
+++ ReactNative/ex3/js/redux/store.js
@@ -1,17 +1,17 @@
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import createSagaMiddleware from 'redux-saga'
-import logger from 'redux-logger'
 import reduceSession from './reduceSession'
+import reduceSchedule from './reduceSchedule'
 import saga from './saga'
 
 const sagaMiddleware = createSagaMiddleware();
 const middleware = [sagaMiddleware];
 // if (process.env.NODE_ENV === 'development') {
-//   middleware.push(logger);
+//   middleware.push(logger);  // import logger from 'redux-logger'
 // }
 
-// const reducers = combineReducers({reducerCurrency, reducerSchedule})
-const store = createStore(reduceSession, applyMiddleware(...middleware));
+const reducers = combineReducers({reduceSession, reduceSchedule})
+const store = createStore(reducers, applyMiddleware(...middleware));
 
 sagaMiddleware.run(saga);
 
