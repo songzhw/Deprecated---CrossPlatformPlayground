c260c8d94060d02f8368a923e873861c9d2fb0ca
songzhw <songzhw2012@gmail.com>
2018-12-05 11:08:58 -0500

    manipulate data for section list

--- ReactNative/ex3/js/screen/ScheduleDay1.js
+++ ReactNative/ex3/js/screen/ScheduleDay1.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react'
-import {View, StyleSheet, Text, FlatList} from 'react-native'
-import {connect} from 'react-redux'
-import {tryDay1} from "../redux/actionSchedule";
+import React, { Component } from 'react'
+import { View, StyleSheet, Text, SectionList } from 'react-native'
+import { connect } from 'react-redux'
+import { tryDay1 } from '../redux/actionSchedule'
 
 class ScheduleDay1 extends Component {
 
@@ -15,10 +15,10 @@ class ScheduleDay1 extends Component {
     return (
       <View style={styles.root}>
         <Text style={styles.gantt}>Gantt Day 1</Text>
-        <FlatList
-          data={this.props.schedulers1}
-          renderItem={this.renderEachSchedule}
-          keyExtractor={(item, index) => `item ${index}`}
+        <SectionList
+          sections={this.props.schedulers1}
+          renderItem={ ({item}) => <Text> item.topic.title</Text> }
+          renderSectionHeader={({section}) => <Text style={{fontSize: 22}}> section.key</Text> }
         />
       </View>
     )
@@ -36,11 +36,11 @@ class ScheduleDay1 extends Component {
      原来我们写的是:  renderItem = {({item}) => <Text> {item.topic.title </Title>
      原来没有item也行, 是因为用了desctructure变量的特性
      */
-    let {item} = row
+    let { item } = row
     return (
       <View>
         <Text> {item.startTime} - {item.endTime}</Text>
-        <Text style={{fontSize: 22}}> {item.topic.title} </Text>
+        <Text style={{ fontSize: 22 }}> {item.topic.title} </Text>
       </View>
     )
   }
@@ -60,8 +60,33 @@ const styles = StyleSheet.create({
 
 const mapStateToProps = (state) => {
   console.log(`szw Day1 mapStateToProps: ${JSON.stringify(state)}`)
+  let { schedules } = state.reduceSchedule
+
+  // {"startTime":"10:00","endTime":"10:30","topic":{"title":"Android Dev in 10 minutes","desp":""}},
+  //没比较双层循环, 因为这会有大量的重复比对, 完全没必要. 所以这里借用一个map
+  let map = new Map()
+  for (let index in schedules) {  //index分别是0,1,2
+    let item = schedules[index]
+    let { startTime } = item
+    if (map.has(startTime)) {
+      let list = map.get(startTime)
+      list.push(item)
+    } else {
+      const list = []
+      list.push(item)
+      map.set(startTime, list)
+    }
+  }
+
+  let result = []
+  map.forEach(function(value, key, map) {
+    let obj = {}
+    obj['key'] = key
+    obj['data'] = value
+    result.push(obj)
+  })
   return {
-    schedulers1: state.reduceSchedule.schedules
+    schedulers1: result,
   }
 }
 
