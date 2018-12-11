1ea6bf00f87c9cad3f512b76ec6610710ac2fa78
songzhw <songzhw2012@gmail.com>
2018-12-11 12:43:39 -0500

    build the viewpager

---
+++ ReactNative/ex3/js/component/ViewPager.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { View, StyleSheet, ScrollView, ViewPagerAndroid, Platform } from 'react-native'
+
+export class ViewPager extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleHorizontalScroll = this.handleHorizontalScroll.bind(this)
+  }
+
+  render() {
+    if (Platform.OS === 'ios') {
+      return this.renderIOS()
+    } else {
+      return this.renderAndroid()
+    }
+  }
+
+  renderIOS() {
+    //TODO
+    return null
+  }
+
+  renderAndroid() {
+    return (
+      <ViewPagerAndroid
+        ref={c => (this._scrollview = c)}
+        onPageSelected={this.handleHorizontalScroll}
+        style={styles.container}
+      >
+        {this.renderContent()}
+      </ViewPagerAndroid>
+    )
+  }
+
+  renderContent() {
+    const style = Platform.OS === 'ios' && styles.card
+    return React.Children.map(this.props.children, (child, i) => (
+      <View style={style} key={'r_' + i}>
+        {child}
+      </View>
+    ))
+  }
+
+  handleHorizontalScroll(ev) {
+    let selectedIndex = ev.nativeEvent.position
+    console.log(`szw scrollListener: selected = ${selectedIndex}`)
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  scrollview: {
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  card: {
+    backgroundColor: 'transparent',
+  },
+})
