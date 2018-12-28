88fc43c34d3eb698a49749e6287e91c14e53a284
songzhw <songzhw2012@gmail.com>
2018-12-28 18:17:25 -0500

    finally get it work

--- ReactNative/ex4/App.js
+++ ReactNative/ex4/App.js
@@ -1,21 +1,10 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import {Provider} from 'react-redux'
+import Routes from './js/core/Routes'
+import store from './js/redux/store'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default () => (
+  <Provider store={store}>
+    <Routes/>
+  </Provider>
+)
