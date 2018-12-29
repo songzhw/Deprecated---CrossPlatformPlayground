cdf119ce8af2f7a5e8a17a48bad47be166cca2a8
songzhw <songzhw2012@gmail.com>
2018-12-28 21:38:55 -0500

    try to test saga

--- ReactNative/ex4/__tests__/sagas.test.js
+++ ReactNative/ex4/__tests__/sagas.test.js
@@ -1,21 +1,21 @@
-// import {tryFetchChallenge1} from "../js/redux/saga";
-// import {expectSaga} from "redux-saga-test-plan";
-// import * as matchers from 'redux-saga-test-plan/matchers'
-// import {doFetch} from "../js/core/HttpEngine";
-// import {onGotChallenge1Page} from "../js/redux/reduxChanllengeOne";
+import {tryFetchChallenge1} from "../js/redux/saga";
+import {expectSaga} from "redux-saga-test-plan";
+import * as matchers from 'redux-saga-test-plan/matchers'
+import {doFetch} from "../js/core/HttpEngine";
+import {onGotChallenge1Page} from "../js/redux/reduxChanllengeOne";
 
 test('one', () => {
   expect(1 + 1).toBe(2)
 })
 
-// test('fetch code1', () => {
-//   const result = [{id: 23}, {id: 24}]
-//   const expected = onGotChallenge1Page(result)
-//
-//   return expectSaga(tryFetchChallenge1, {})
-//     .provide([
-//       [matchers.call.fn(doFetch), result]
-//     ])
-//     .put(expected)
-//     .run()
-// })
+test('fetch code1', () => {
+  const result = [{id: 23}, {id: 24}]
+  const expected = onGotChallenge1Page(result)
+
+  return expectSaga(tryFetchChallenge1, {})
+    .provide([
+      [matchers.call.fn(doFetch), result]
+    ])
+    .put(expected)
+    .run()
+})
