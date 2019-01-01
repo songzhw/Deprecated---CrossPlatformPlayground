b5f2b40c7c6b7c912aeec365e85068882e7a29ed
songzhw <songzhw2012@gmail.com>
2019-01-01 12:55:50 -0500

    the original api has too many data(==500), then it's hard to test, so I changed the api soruce

--- ReactNative/cha1/js/core/HttpEngine.js
+++ ReactNative/cha1/js/core/HttpEngine.js
@@ -1,7 +1,6 @@
 export const doFetch = url => fetch(url)
 
-
-export const API_PAGE_LIST = "https://gist.githubusercontent.com/dsandin/c8ed6c5a9486f311f4725f221b912220/raw/8c6d2d8e1f339d02e0ff3d2990560a4862c4beae/users_page_list"
-// export const API_PAGE_1 = "7b7cd2b834abd8c10908803cac5d1dd3/raw/9a8c0270e0f7a778409b2996419bacdbb06edc87/users_page1"
-// export const API_PAGE_2 = "e451f042d2b78143141ea8ea7d97b03f/raw/9847b174d0f5f61701ad64ab73be568270eea3a3/users_page2"
-// export const API_PAGE_3 = "459ac3c73b5ea2a2c0b09015de85d930/raw/fc8af8115057bec36561e799aaf5a47ca12521b8/users_page3"
+export const API_PAGE_LIST = "http://www.mocky.io/v2/5c2ba8983000007400abafe8"
+export const API_PAGE_1 = "http://www.mocky.io/v2/5c21d5663500005900d5531c"
+export const API_PAGE_2 = "http://www.mocky.io/v2/5c2ba8293000006900abafe6"
+export const API_PAGE_3 = "http://www.mocky.io/v2/5c2ba8443000000e0fabafe7"
