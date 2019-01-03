461aa70dbe2fa85114955b75d74a9a98c4b5ed5c
songzhw <songzhw2012@gmail.com>
2019-01-03 14:39:33 -0500

    simulate: customize my own event

--- ReactNative/ex4/__tests__/UiesScreen.test.js
+++ ReactNative/ex4/__tests__/UiesScreen.test.js
@@ -1,43 +1,58 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
-import {Text} from 'react-native'
+import {Text, Button} from 'react-native'
 import UiesScreen from "../js/screen/UiesScreen";
 import {shallow} from "enzyme";
 
 describe('test Ui Screen', () => {
   test('check has one <Text/> child', ()=> {
-    const tree = shallow(<UiesScreen/>)
-    console.log(`2nd = ${JSON.stringify(tree.find(Text))}`) //=> {}
-    expect(tree.find(Text).length).toBe(1)
+    const wrapper = shallow(<UiesScreen/>)
+    expect(wrapper.find(Text).length).toBe(1)
   })
 
   test('check text in screen', () => {
-    const tree = shallow(<UiesScreen title="san" imageName={null} onPress={null}/>)
-    expect(tree.instance().props.title).toBe('san')
+    const wrapper = shallow(<UiesScreen title="san" imageName={null} onPress={null}/>)
+    expect(wrapper.instance().props.title).toBe('san')
   })
 
   test('check the click event: approach 1', ()=>{
     let num = 0
     let _press = () => num++
-    const tree = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
+    const wrapper = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
 
-    tree.simulate('press')
+    wrapper.simulate('press')
     expect(num).toBe(1)
   })
 
   test('check the click event: approach 2', ()=>{
     let _press = jest.fn()
-    const tree = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
+    const wrapper = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
 
-    tree.simulate('press')
+    wrapper.simulate('press')
     expect(_press).toBeCalled()
   })
 
   test('check set state/props methods', ()=>{
     let _press = jest.fn()
-    const tree = shallow(<UiesScreen/>)
-    tree.setProps({title: "23", imageName: null, onPress: _press})
-    let textView = tree.find(Text).getElement()  //=> {"key":null,"ref":null,"props":{"children":"23"},"_owner":null,"_store":{}}
+    const wrapper = shallow(<UiesScreen/>)
+    wrapper.setProps({title: "23", imageName: null, onPress: _press})
+
+    let textView = wrapper.find(Text).getElement()  //=> {"key":null,"ref":null,"props":{"children":"23"},"_owner":null,"_store":{}}
     expect(textView.props.children).toBe('23')
+  })
+
+  test('check the method to get state', ()=> {
+    let _press = jest.fn()
+    const wrapper = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
+    expect(wrapper.state()).toEqual({num: 10})
+    // 或是用 " expect(wrapper.state('num')).toEqual(10) "
+  })
+
+  test('check the simulate method', ()=> {
+    let _press = jest.fn()
+    const wrapper = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
+
+    wrapper.find(Button).simulate('kiss')
+    expect(wrapper.state('num')).toEqual(13)
   })
 })
--- ReactNative/ex4/js/screen/UiesScreen.js
+++ ReactNative/ex4/js/screen/UiesScreen.js
@@ -2,15 +2,26 @@ import React, {Component} from 'react'
 import {View, StyleSheet, Text, Button, TouchableOpacity, Image} from 'react-native'
 
 class UiesScreen extends Component {
+  state = {
+    num: 10
+  }
+
   render() {
     return (
       <TouchableOpacity onPress={this.props.onPress}>
         <Text>{this.props.title}</Text>
         <Image source={this.props.img}/>
-
+        <Button title="btn3" onPress={() => null} onKiss={this.onKiss}/>
       </TouchableOpacity>
     )
   }
+
+  onKiss = () => {
+    console.log(`szw onKiss() get invoked`)
+    let {num} = this.state
+    this.setState({num: num + 3})
+  }
+
 }
 
 export default UiesScreen
