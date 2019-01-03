32cc3ad594e791ac722c0195a53bdbd2a277e420
songzhw <songzhw2012@gmail.com>
2019-01-03 13:14:43 -0500

    compare the text in one component

--- ReactNative/ex4/__tests__/UiesScreen.test.js
+++ ReactNative/ex4/__tests__/UiesScreen.test.js
@@ -1,34 +1,43 @@
 import renderer from 'react-test-renderer'
 import React from 'react'
+import {Text} from 'react-native'
 import UiesScreen from "../js/screen/UiesScreen";
 import {shallow} from "enzyme";
 
 describe('test Ui Screen', () => {
-  test('renders correctly', () => {
-    const tree = renderer
-      .create(<UiesScreen/>)
-      .toJSON()
-    expect(tree).toMatchSnapshot()
+  test('check has one <Text/> child', ()=> {
+    const tree = shallow(<UiesScreen/>)
+    console.log(`2nd = ${JSON.stringify(tree.find(Text))}`) //=> {}
+    expect(tree.find(Text).length).toBe(1)
   })
 
-  test('second', () => {
-    const tree = setup(0)
+  test('check text in screen', () => {
+    const tree = shallow(<UiesScreen title="san" imageName={null} onPress={null}/>)
     expect(tree.instance().props.title).toBe('san')
   })
 
-
-  function setup(num) {
-    let _press = () => num++
-    const tree = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
-    return tree
-  }
-
-  test('three', ()=>{
+  test('check the click event: approach 1', ()=>{
     let num = 0
     let _press = () => num++
     const tree = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
 
     tree.simulate('press')
     expect(num).toBe(1)
+  })
+
+  test('check the click event: approach 2', ()=>{
+    let _press = jest.fn()
+    const tree = shallow(<UiesScreen title="san" imageName={null} onPress={_press}/>)
+
+    tree.simulate('press')
+    expect(_press).toBeCalled()
+  })
+
+  test('check set state/props methods', ()=>{
+    let _press = jest.fn()
+    const tree = shallow(<UiesScreen/>)
+    tree.setProps({title: "23", imageName: null, onPress: _press})
+    let textView = tree.find(Text).getElement()  //=> {"key":null,"ref":null,"props":{"children":"23"},"_owner":null,"_store":{}}
+    expect(textView.props.children).toBe('23')
   })
 })
--- ReactNative/ex4/js/screen/UiesScreen.js
+++ ReactNative/ex4/js/screen/UiesScreen.js
@@ -2,13 +2,12 @@ import React, {Component} from 'react'
 import {View, StyleSheet, Text, Button, TouchableOpacity, Image} from 'react-native'
 
 class UiesScreen extends Component {
-  index = 0
-
   render() {
     return (
       <TouchableOpacity onPress={this.props.onPress}>
         <Text>{this.props.title}</Text>
         <Image source={this.props.img}/>
+
       </TouchableOpacity>
     )
   }
