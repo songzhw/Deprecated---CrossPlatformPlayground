b80eed28b8025a66651b1342a4633d2c8f1c61c0
songzhw <songzhw2012@gmail.com>
2019-01-05 13:57:45 -0500

    action_down,  action_up时的反应; View中也要加入panResponder.panHandler属性才行

--- ReactNative/ex4/js/screen/GestureAnimScreen.js
+++ ReactNative/ex4/js/screen/GestureAnimScreen.js
@@ -1,18 +1,52 @@
 import React, {Component} from 'react'
-import {View, StyleSheet, Text} from 'react-native'
+import {View, StyleSheet, Text, PanResponder} from 'react-native'
 import {connect} from 'react-redux'
 
 const CIRCLE_SIZE = 76
 
 class GestureAnimScreen extends Component {
-  state = {bg: 'blue'}
+  state = {bg: 'green'}
 
   render() {
     return (
       <View
+        {...this.gesture.panHandlers}
         style={[styles.circle, {backgroundColor: this.state.bg}]}/>
     )
   }
+
+  componentWillMount() {
+    this.gesture = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => true,
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+
+      onPanResponderGrant: (evt, gestureState) => {
+        this.setState({...this.state, bg: 'red'})
+      },
+      onPanResponderRelease: (evt, gestureState) => {
+        this.setState({...this.state, bg: 'green'})
+      },
+
+      onPanResponderMove: (evt, gestureState) => {
+        // The most recent move distance is gestureState.move{X,Y}
+
+        // The accumulated gesture distance since becoming responder is
+        // gestureState.d{x,y}
+      },
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+
+      onPanResponderTerminate: (evt, gestureState) => {
+        // Another component has become the responder, so this gesture
+        // should be cancelled
+      },
+      onShouldBlockNativeResponder: (evt, gestureState) => {
+        // Returns whether this component should block native components from becoming the JS
+        // responder. Returns true by default. Is currently only supported on android.
+        return true;
+      },
+    })
+  }
+
 }
 
 const styles = StyleSheet.create({
