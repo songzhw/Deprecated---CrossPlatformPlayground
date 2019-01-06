a5ed43701c5c9a7fe2aa5b858ea70f8e82b85d4f
songzhw <songzhw2012@gmail.com>
2019-01-05 21:45:16 -0500

    test the functionality of dispatching actions

--- ReactNative/ex4/__tests__/FirstScreen.test.js
+++ ReactNative/ex4/__tests__/FirstScreen.test.js
@@ -4,6 +4,7 @@ import FirstScreen from "../js/screen/FirstScreen"
 import {shallow} from "enzyme"
 import configMockStore from 'redux-mock-store'
 import {Provider} from "react-redux";
+import {TRY_FIRST_PAGE_INFO, tryFirstPageInfo} from "../js/redux/actionFirst";
 
 describe('Test Redux + Screen', ()=> {
   let initState = {reducerFirst: {opened: []}}
@@ -16,7 +17,12 @@ describe('Test Redux + Screen', ()=> {
   })
 
   test('check has one <FlatList/> child', ()=> {
-    console.log(`FirstScreen ${wrapper.debug()}`)
     expect(wrapper.find(FirstScreen).length).toBe(1)
+  })
+
+  test('check props is correct', () => {
+    store.dispatch(tryFirstPageInfo())
+    let action = store.getActions()
+    expect(action[0].type).toBe(TRY_FIRST_PAGE_INFO)
   })
 })
