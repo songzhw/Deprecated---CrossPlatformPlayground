69d85481ee87da53f2328017eb4a358fcc95463e
songzhw <songzhw2012@gmail.com>
2019-01-05 22:35:45 -0500

    mount() seems not work in React-Native: error - "   It looks like you called `mount()` without a global document being loaded.   " "

--- ReactNative/ex4/__tests__/FirstScreen.test.js
+++ ReactNative/ex4/__tests__/FirstScreen.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {Text, FlatList, Image} from 'react-native'
-import FirstScreen from "../js/screen/FirstScreen"
-import {shallow} from "enzyme"
+import ConnectedFirstScreen, {FirstScreen} from "../js/screen/FirstScreen"
+import {mount, shallow} from "enzyme"
 import configMockStore from 'redux-mock-store'
 import {Provider} from "react-redux";
 import {TRY_FIRST_PAGE_INFO, tryFirstPageInfo} from "../js/redux/actionFirst";
@@ -13,11 +13,11 @@ describe('Test Redux + Screen', ()=> {
 
   beforeEach(()=> {
     store = mockStore(initState)
-    wrapper = shallow(<Provider store={store}><FirstScreen/></Provider>)
+    wrapper = mount(<Provider store={store}><ConnectedFirstScreen/></Provider>)
   })
 
-  test('check has one <FlatList/> child', ()=> {
-    expect(wrapper.find(FirstScreen).length).toBe(1)
+  test('check has one <ConnectedFirstScreen/> child', ()=> {
+    expect(wrapper.find(ConnectedFirstScreen).length).toBe(1)
   })
 
   test('check props is correct', () => {
@@ -25,4 +25,23 @@ describe('Test Redux + Screen', ()=> {
     let action = store.getActions()
     expect(action[0].type).toBe(TRY_FIRST_PAGE_INFO)
   })
+  // TODO -> -> 我去除store.dispatch()这句, 测试就失败了. 但我们componentWillMount()里不是发了这个嘛?! 为什么会失败?
+
+  test('check has one <FlatList/> child', ()=> {
+    let screen = wrapper.find(FirstScreen)
+    // let tmp = screen.children().at(0)
+    console.log(`(3). ${screen.debug()}`)
+    // console.log(`(4). ${tmp.debug()}`)
+    // expect(screen.find(FlatList).length).toBe(1)
+  })
+  /*
+  screen结果是:
+      <ContextConsumer>
+        [function bound renderWrappedComponent]
+      </ContextConsumer>
+
+  tmp的结果是:
+      (4). [function bound renderWrappedComponent]
+
+   */
 })
