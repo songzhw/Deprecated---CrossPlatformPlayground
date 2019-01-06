b049c9f918afd5c606c0f10c373292e6a7cb1716
songzhw <songzhw2012@gmail.com>
2019-01-06 14:23:19 -0500

    1. test renderItem() 2. error     Expected: {"data": {"author": "a", "img": null, "length": "2h", "progress": "10%", "title": "1"}}     Received: {"data": {"author": "a", "img": null, "length": "2h", "progress": "10%", "title": "1"}}

--- ReactNative/ex4/__tests__/FirstScreen.test.js
+++ ReactNative/ex4/__tests__/FirstScreen.test.js
@@ -5,67 +5,84 @@ import {shallow} from "enzyme"
 import configMockStore from 'redux-mock-store'
 import {Provider} from "react-redux";
 import {TRY_FIRST_PAGE_INFO, tryFirstPageInfo} from "../js/redux/actionFirst";
+import ReadedComponent from "../js/component/ReadedComponent";
 
-describe('Test UI', ()=> {
+describe('Test UI', () => {
   let wrapper
+  let open
 
-  beforeEach(()=>{
-    let props = {dispatch: (action)=>{}}
+  beforeEach(() => {
+    open = [
+      {img: null, title: '1', author: 'a', progress: '10%', length: "2h"},
+      {img: null, title: '2', author: 'b', progress: '93%', length: "1h30min"}
+    ]
+    let props = {
+      dispatch: (action) => {
+      }, open: open
+    }
     wrapper = shallow(<FirstScreen {...props}/>)
   })
 
-  test('exist one FlatList', ()=>{
+  test('exist one FlatList', () => {
     expect(wrapper.find(FlatList).length).toBe(1)
   })
+
+  test('render item - returned value', () => {
+    let item = wrapper.instance().renderItem(open[0])
+    expect(item.type).toBe(ReadedComponent)
+  })
+
+  test('render item - props', () => {
+    let data = {item: open[0]}
+    let item = wrapper.instance().renderItem(data)
+    expect(item.props).toBe({data: open[0]})
+  })
 })
 
 
-
-
-describe('Test Redux + Screen', ()=> {
-  let initState = {reducerFirst: {opened: []}}
-  let mockStore = configMockStore()
-  let wrapper, store
-
-  beforeEach(()=> {
-    store = mockStore(initState)
-    // wrapper = shallow(<Provider store={store}><ConnectedFirstScreen/></Provider>)
-    //   .find(ConnectedFirstScreen)
-    wrapper = shallow(<ConnectedFirstScreen/>, {context: store})
-  })
-
-  test('check props is correct', () => {
-    store.dispatch(tryFirstPageInfo())
-    let action = store.getActions()
-    expect(action[0].type).toBe(TRY_FIRST_PAGE_INFO)
-  })
-  // TODO -> -> 我去除store.dispatch()这句, 测试就失败了. 但我们componentWillMount()里不是发了这个嘛?! 为什么会失败?
-
-  // test('xxx', ()=> {
-  //   console.log(`(1) wrapper = ${wrapper.debug()}`)
-  //   expect(wrapper.find(FlatList).length).toBe(1)
-  // })
-
-  // test('check has one <ConnectedFirstScreen/> child', ()=> {
-  //   expect(wrapper.find(ConnectedFirstScreen).length).toBe(1)
-  // })
-  //
-  // test('check has one <FlatList/> child', ()=> {
-  //   let screen = wrapper.dive()
-  //   // let t2 = shallow(screen.getElement())
-  //   let tmp = screen.children().at(0)
-  //   console.log(`(3). ${screen.debug()}`)
-  //   console.log(`(4). ${tmp.debug()}`)
-  //   // expect(screen.find(FlatList).length).toBe(1)
-  // })
-  /*
-  screen结果是:
-      <ContextConsumer>
-        [function bound renderWrappedComponent]
-      </ContextConsumer>
-
-  tmp的结果是:
-      (4). [function bound renderWrappedComponent]
-
-   */
-})
+// describe('Test Redux + Screen', ()=> {
+//   let initState = {reducerFirst: {opened: []}}
+//   let mockStore = configMockStore()
+//   let wrapper, store
+//
+//   beforeEach(()=> {
+//     store = mockStore(initState)
+//     // wrapper = shallow(<Provider store={store}><ConnectedFirstScreen/></Provider>)
+//     //   .find(ConnectedFirstScreen)
+//     wrapper = shallow(<ConnectedFirstScreen/>, {context: store})
+//   })
+//
+//   test('check props is correct', () => {
+//     store.dispatch(tryFirstPageInfo())
+//     let action = store.getActions()
+//     expect(action[0].type).toBe(TRY_FIRST_PAGE_INFO)
+//   })
+//   // TODO -> -> 我去除store.dispatch()这句, 测试就失败了. 但我们componentWillMount()里不是发了这个嘛?! 为什么会失败?
+//
+//   test('xxx', ()=> {
+//     console.log(`(1) wrapper = ${wrapper.debug()}`)
+//     expect(wrapper.find(FlatList).length).toBe(1)
+//   })
+//
+//   // test('check has one <ConnectedFirstScreen/> child', ()=> {
+//   //   expect(wrapper.find(ConnectedFirstScreen).length).toBe(1)
+//   // })
+//   //
+//   // test('check has one <FlatList/> child', ()=> {
+//   //   let screen = wrapper.dive()
+//   //   // let t2 = shallow(screen.getElement())
+//   //   let tmp = screen.children().at(0)
+//   //   console.log(`(3). ${screen.debug()}`)
+//   //   console.log(`(4). ${tmp.debug()}`)
+//   //   // expect(screen.find(FlatList).length).toBe(1)
+//   // })
+//
+//   // screen结果是:
+//   //     <ContextConsumer>
+//   //       [function bound renderWrappedComponent]
+//   //     </ContextConsumer>
+//   //
+//   // tmp的结果是:
+//   //     (4). [function bound renderWrappedComponent]
+//
+// })
