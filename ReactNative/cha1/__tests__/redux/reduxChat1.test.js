31b77fde7fcb2c1bf9c8160757c8cf9ce26c6c9d
songzhw <songzhw2012@gmail.com>
2019-01-08 13:24:26 -0500

    still can not catch the error

--- ReactNative/cha1/__tests__/redux/reduxChat1.test.js
+++ ReactNative/cha1/__tests__/redux/reduxChat1.test.js
@@ -5,10 +5,16 @@ test('fetch page action', () => {
   expect(action).toEqual({type: TRY_CHALLENGE1_PAGE, page: 1, index: 'b'})
 })
 
-test('fetch cha1 data originally', () => {
+test('fetch cha1 data originally, got a non-array, should have error', () => {
   let action = onGotChallenge1Page(23, 0)
+  // let newState = reducer({}, action)
+  expect(reducer({}, action)).toThrow(new TypeError('CreateListFromArrayLike called on non-object'))
+})
+
+test('fetch cha1 data originally', () => {
+  let action = onGotChallenge1Page([{id:1}, {id:2}], 0)
   let newState = reducer({}, action)
-  expect(newState).toEqual({payload: 23, index: 0})
+  expect(newState).toEqual({payload: [{id:1}, {id:2}], index: 0})
 })
 
 test('sort ascending', () => {
