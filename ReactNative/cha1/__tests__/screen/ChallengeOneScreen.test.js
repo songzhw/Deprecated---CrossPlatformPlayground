fc6bddc49cfa6b043f8c4a74371be30f8c5adc95
songzhw <songzhw2012@gmail.com>
2019-01-08 15:04:41 -0500

    add one more test

--- ReactNative/cha1/__tests__/screen/ChallengeOneScreen.test.js
+++ ReactNative/cha1/__tests__/screen/ChallengeOneScreen.test.js
@@ -3,6 +3,7 @@ import {FlatList} from 'react-native'
 import {shallow} from "enzyme"
 import {ChallengeOneScreen as Screen, Layout, Sort} from "../../js/screen/ChallengeOneScreen";
 import {CLEAR_CHA1, fetchChallengeOnePage, SORT_AZ_ACTION, SORT_ZA_ACTION} from "../../js/redux/reduxChanllengeOne";
+import {fetchPageList} from "../../js/redux/reduxPages";
 
 describe('test UI', () => {
   let wrapper
@@ -17,6 +18,11 @@ describe('test UI', () => {
       users: []
     }
     wrapper = shallow(<Screen {...props}/>)
+  })
+
+  test('fetch pages data at first', ()=>{
+    let action = fetchPageList()
+    expect(dispatchFun).toBeCalledWith(action)
   })
 
   test('FlatList exists', () => {
@@ -61,8 +67,10 @@ describe('test UI', () => {
   })
 
   test('unmounting will dispatch CLEAR_CHA1 action', () => {
+    wrapper.unmount()
     expect(dispatchFun).toBeCalledWith({type: CLEAR_CHA1})
   })
 
 })
 
+// 最后没有测试的就只有 sort的其它分支(ZA, default), 以及mapStateToPrps方法了
