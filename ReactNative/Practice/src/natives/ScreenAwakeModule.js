f485320d3aacdb1f218a6ba3064a058c9cd832b4
songzhw <songzhw2012@gmail.com>
2019-01-09 21:30:47 -0500

    refactor

--- ReactNative/Practice/src/natives/ScreenAwakeModule.js
+++
@@ -1,7 +0,0 @@
-import {NativeModules} from 'react-native'
-
-export const ScreenAwakeModule = NativeModules.Module_ScreenAwake
-
-export const getSystemVolumn = (callback) => {
-  NativeModules.Module_Volumn.getSystemVolumn(callback)
-}
