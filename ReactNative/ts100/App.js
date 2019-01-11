490a3882ae2b42b405bef4331acb836b294fd5f6
songzhw <songzhw2012@gmail.com>
2019-01-11 12:22:05 -0500

    Add typescript, and it works. Just I need to run 'tsc App.tsx -t es2015' to make the emulator reflect the change. Should have some way to improve this

--- ReactNative/ts100/App.js
+++ ReactNative/ts100/App.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
+    render() {
+        return (<View style={styles.container}>
+        <Text>TypeScript World 101 </Text>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
+      </View>);
+    }
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
