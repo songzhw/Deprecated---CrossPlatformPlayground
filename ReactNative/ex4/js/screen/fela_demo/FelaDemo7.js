ddd73f2ac84aa031e3facd4ada1c1f2278ce3f28
songzhw <songzhw2012@gmail.com>
2019-01-24 09:27:27 -0500

    [deprecated] this cannot get the theme from grandparent componet

--- ReactNative/ex4/js/screen/fela_demo/FelaDemo7.js
+++ ReactNative/ex4/js/screen/fela_demo/FelaDemo7.js
@@ -1,32 +1,12 @@
 import React, {PureComponent} from 'react'
 import {View, Text, Button} from 'react-native'
-import {connect as connectFela,} from 'react-fela'
+import {connect as connectFela, createComponent as createFelaComponent} from 'react-fela'
 import {connect as connectRedux} from "react-redux";
 
-const rules = ({propFontSize}) => ({
-  root: {
-    backgroundColor: '#43a047',
-  },
-  size: {
-    color: 'blue',
-    fontSize: propFontSize
-  }
+const rules = ({theme}) => ({
+  color: theme.basicText.color,
+  fontWeight: theme.basicText.fontWeight
 })
-
-class Tmp2 extends PureComponent {
-  render(){
-    console.log(`szw fela7 props = ${JSON.stringify(this.props)}`)
-    let {txt, styles} = this.props
-    return (
-      <View style={styles.root}>
-        <Text style={styles.size}> {txt} </Text>
-      </View>
-    )
-  }
-}
-
-const ConnectedTmp = connectFela(rules)(Tmp2)
-
 
 class FelaDemo7 extends PureComponent {
   state = {
@@ -35,9 +15,10 @@ class FelaDemo7 extends PureComponent {
 
   render() {
     console.log(`szw demo7 : ${JSON.stringify(this.props)}`)
+    let Tmp = createFelaComponent(rules, Text)
     return (
       <View style={{flex: 1}}>
-        <ConnectedTmp propFontSize={this.state.num} txt={this.state.num}/>
+        <Tmp>{this.state.num}</Tmp>
         <Button title="+ 6" onPress={this.onPlusOne}/>
       </View>
     )
