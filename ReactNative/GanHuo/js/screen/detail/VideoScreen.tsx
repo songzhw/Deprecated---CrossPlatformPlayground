1f511fae2ca920078613ace244c11c995db04a6d
songzhw <songzhw2012@gmail.com>
2019-01-28 21:25:27 -0500

    add <Video>, and get an error: "Accessing view manager configs directly off UIManager via UIManager['RCTVideo'] is no longer supported. Use UIManager.getViewManagerConfig('RCTVideo') instead"

--- ReactNative/GanHuo/js/screen/detail/VideoScreen.tsx
+++ ReactNative/GanHuo/js/screen/detail/VideoScreen.tsx
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import {connect} from 'react-redux'
+import {INavigationProps, IReduxProps} from "../../core/CoreProps";
+import Video from "react-native-video";
 
-class VideoScreen extends Component{
+interface Props extends IReduxProps, INavigationProps{}
+class VideoScreen extends Component<Props>{
+  url = ""
+  private player: Video | null = null;
+
+  componentWillMount() {
+    this.url = this.props.navigation.getParam("video", "")
+  }
+
+
   render(){
+    console.log(`szw video = ${this.url}`)
     return (
       <View style={styles.root}>
         <Text>VideoScreen Screen</Text>
+        <Video
+          ref={(ref) => this.player = ref}
+          source={{uri: this.url}}
+          style={styles.videoPlayer}
+        />
       </View>
     )
   }
@@ -16,6 +33,9 @@ const styles = StyleSheet.create({
   root: {
     flex: 1,
   },
+  videoPlayer: {
+    height: 200
+  }
 })
 
 const mapStateToProps = (state: any) => {
