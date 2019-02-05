c657ef3aed02aaf912d685ae53fc29d497307235
songzhw <songzhw2012@gmail.com>
2019-02-04 21:01:12 -0500

    Warning: Cannot update during an existing state transition (such as within `render`). Render methods should be a pure function of props and state.   Error: Invariant Violation: Invariant Violation: Maximum update depth exceeded. This can happen when a component repeatedly calls setState inside componentWillUpdate or componentDidUpdate. React limits the number of nested updates to prevent infinite loops.

--- ReactNative/ex4/js/component/drawer/SixDrawerLayout.js
+++ ReactNative/ex4/js/component/drawer/SixDrawerLayout.js
@@ -9,14 +9,23 @@ class SixDrawerLayout extends React.Component {
 
   render() {
     return (
-      <TouchableWithoutFeedback onPress={this.onPressEmptyBackground}>
-        <View style={styles.root}>
-          {this.props.children}
-          <View style={[styles.drawerRoot, {width: this.props.drawerWidth, transform: [{translateX: this.state.transform}]}]}>
+      <View style={styles.root}>
+        {this.props.children}
+
+        <View style={styles.drawerRoot}>
+          <View style={[styles.drawerContainer,
+            {width: this.props.drawerWidth, transform: [{translateX: this.state.transform}]}]}>
             {this.props.renderDrawer()}
           </View>
+
+          <TouchableWithoutFeedback onPress={this.closeDrawer()}
+                                    style={[styles.drawerShadow, {left: this.props.drawerWidth}]}>
+            <View/>
+          </TouchableWithoutFeedback>
+
         </View>
-      </TouchableWithoutFeedback>
+      </View>
+
     )
   }
 
@@ -33,6 +42,7 @@ class SixDrawerLayout extends React.Component {
   }
 
   closeDrawer = () => {
+    console.log(`szw close drawer`)
     if (this.isOpen) {
       this.setState({...this.state, transform: -1 * this.props.drawerWidth})
     }
@@ -46,12 +56,25 @@ class SixDrawerLayout extends React.Component {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    backgroundColor: '#0000007e',
+
   },
   drawerRoot: {
+    // backgroundColor: '#0000007e',
     position: 'absolute',
     top: 0,
     bottom: 0,
+    left: 0,
+    right: 0
+  },
+  drawerContainer: {
+    flex: 1
+  },
+  drawerShadow: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#0000007e'
   }
 })
 
