50212e801424d7d62c1eb4ed13221b6d2e53d7c2
songzhw <songzhw2012@gmail.com>
2019-02-07 13:03:30 -0500

    all tests failed Now.
    "  SyntaxError: /Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/ex4/node_modules/react-native/jest/mockComponent.js: Support for the experimental syntax 'classProperties' isn't currently enabled (20:24)
       Add @babel/plugin-proposal-class-properties (https://git.io/vb4SL) to the 'plugins' section of your Babel config to enable transformation.
    ".
    
    This commit is to fix this error

--- ReactNative/GanHuo/js/core/styles/CommonStyles.ts
+++ ReactNative/GanHuo/js/core/styles/CommonStyles.ts
@@ -7,4 +7,13 @@ export const commonStyles = StyleSheet.create({
     width: Header.HEIGHT,
     height: Header.HEIGHT,
   },
+  txtTitle: {
+    fontSize: 20
+  },
+  txtSubtitle: {
+    fontSize: 19
+  },
+  txtContent: {
+    fontSize: 18
+  }
 })
