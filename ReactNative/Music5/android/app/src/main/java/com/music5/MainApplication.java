747c074e71eca707be67ffa63555c4df953b2d3c
songzhw <songzhw2012@gmail.com>
2019-02-10 21:45:37 -0500

    add react-native-localize

--- ReactNative/Music5/android/app/src/main/java/com/music5/MainApplication.java
+++ ReactNative/Music5/android/app/src/main/java/com/music5/MainApplication.java
@@ -3,6 +3,7 @@ package com.music5;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.brentvatne.react.ReactVideoPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNLocalizePackage(),
             new RNGestureHandlerPackage(),
             new ReactVideoPackage()
       );
