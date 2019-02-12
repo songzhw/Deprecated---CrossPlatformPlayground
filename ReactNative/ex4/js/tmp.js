09d356c2c9e771c43969c57503d83d7c38ee5563
songzhw <songzhw2012@gmail.com>
2019-02-11 21:19:27 -0500

    object for redux

--- ReactNative/ex4/js/tmp.js
+++ ReactNative/ex4/js/tmp.js
@@ -1,10 +1,20 @@
-let ary = [1, 2, 3, 4, 5]
+let item = {
+  id: 23,
+  name: 'szw',
+  isMale: false
+}
 
-// slice(0,1): 只取第[0,1).   ;   slice(2), 相当于[2, end]
-let result = [...ary.slice(0, 1),  ...ary.slice(2)]
+let r1 = item
+console.log(r1 === item)  //=> true
 
-console.log(ary) //=> [1,2,3,4,5]
-console.log(result) //=> [1,3,4,5]
+let r2 = Object.assign({}, item, {id: 100, isMale: true})
+console.log(`${JSON.stringify(r2)}`)  //=> {"id":100,"name":"szw","isMale":true}
+console.log(r2 === item)  //=> false
 
-let r2 = [...ary.slice(0,1), 10, ...ary.slice(2)]
-console.log(r2) //=> [ 1, 10, 3, 4, 5 ]
+let r3 = {...item, id: 100}
+console.log(`${JSON.stringify(r3)}`)  //=> {"id":100,"name":"szw","isMale":false}
+console.log(r3 === item)  //=> false
+
+let r4 = {...item, age: 12}
+console.log(r4)  //=> { id: 23, name: 'szw', isMale: false, age: 12 }
+console.log(r4 === item)  //=> false
