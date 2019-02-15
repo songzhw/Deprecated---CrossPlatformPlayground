42a3221d0269b4e295f9c5186b78f3a4b677f13c
songzhw <songzhw2012@gmail.com>
2019-02-15 18:28:09 -0500

    modify the axios (error and resp are both okay to use)

--- ReactNative/RN5/js/core/HttpEngine.js
+++ ReactNative/RN5/js/core/HttpEngine.js
@@ -1,1 +1,21 @@
+import Axios from 'axios'
+
+export const axios = Axios.create({
+  baseURL: "http://www.mocky.io/v2/",
+  timeout: 3000,
+  // headers: {"key" : "value"}
+})
+
+axios.interceptors.response.use(
+  (resp) => {
+    console.log(`szw resp = ${JSON.stringify(resp)}`)
+    return resp
+  },
+  (error) => {
+    console.log(`szw error = ${JSON.stringify(error)}`)
+  })
+
+
+
 export const doFetch = id => fetch(`http://www.mocky.io/v2/${id}`)
+
--- ReactNative/RN5/js/screen/third_party/AxiosScreen.js
+++ ReactNative/RN5/js/screen/third_party/AxiosScreen.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
-import axios from 'axios'
+import {axios} from '../../core/HttpEngine'
 
 class AxiosScreen extends Component{
 
   componentWillMount() {
-    // resp is : { config, data, headers, request,  }. Real response json is in the "resp.data"
-    axios.get("https://www.mocky.io/v2/5c03483b3000002b00bb9408")
-      .then((resp) => console.log(`szw ${JSON.stringify(resp)}`))
-      .catch(error => console.error("szw" + error))
-      .then( () => console.log("szw request's done~"))
+    axios.get("5c03483b3000002b00bb9408")
+      .then((resp) => console.log(`\nszw work = ${JSON.stringify(resp.data)}`))
+      .then( () => console.log(`szw work done`))
   }
 
   render(){
