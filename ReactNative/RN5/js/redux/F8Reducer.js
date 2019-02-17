dc53a9baca7d8a612d435daf1db687cda908114c
songzhw <songzhw2012@gmail.com>
2019-02-17 17:50:52 -0500

    now the saga library makes sense: 1. no need to call httpEngine;  2. easier to test http code;  3. no need to dispatch another action within a reducer (which is anti-pattern, as it is not a pure function anymore)

--- ReactNative/RN5/js/redux/F8Reducer.js
+++ ReactNative/RN5/js/redux/F8Reducer.js
@@ -1,3 +1,7 @@
+import {doFetch} from "../core/HttpEngine";
+
+export const REDUCER_F8 = "f8"
+
 export const TRY_LOGIN = 'TRY_LOGIN'
 export const tryLogin = () => ({
   type: TRY_LOGIN
@@ -10,18 +14,40 @@ export const loginSucc = resp => ({
 })
 
 const initState = {
-  succ: false,
-  session: ""
+  payload: {}
 }
 
 export default (state = initState, action) => {
-  switch (action.type) {
-    case LOGIN_SUCC:
-      console.log(`szw reducer LOGIN_SUCC`)
-      const {succ, session} = action
-      return {succ: succ, session: session}
-    default:
-      return state
+  let {type, nextType, apiId} = action
+  if (!type.startsWith(REDUCER_F8)) {
+    return initState
+  }
+  if (type.startsWith(`${REDUCER_F8}_request_`)) {
+    doFetch(apiId)
+      .then(response => {
+        let resp = response.json()
+        let newAction = createHttpResponseAction(nextType, resp)
+        dispatch(newActio)
+      })
+  }
+  if (type.startsWith(`${REDUCER_F8}_response_`)) {
+
+  }
+}
+
+export function createHttpAction(reducerName, startType, endType, apiId) {
+  // TODO may need to add payload too
+  return {
+    type: `${reducerName}_request_${startType}`,
+    nextType: `${reducerName}_response_${endType}`,
+    apiId : apiId
+  }
+}
+
+export function createHttpResponseAction(type, payload){
+  return {
+    type: type,
+    payload: payload
   }
 }
 
--- ReactNative/RN5/js/redux/saga.js
+++ ReactNative/RN5/js/redux/saga.js
@@ -5,19 +5,9 @@ import {API_ID_LOGIN} from "../core/apiConstants";
 
 
 const rootSaga = function* () {
-  yield takeEvery(TRY_LOGIN, tryLogin)
 }
 
 const tryLogin = function* (action) {
-  try {
-    console.log(`szw saga tryLogin()`)
-    const rawResp = yield call(doFetch, API_ID_LOGIN)
-    const resp = yield rawResp.json()
-    const newAction = loginSucc(resp)
-    yield put(newAction)
-  } catch (err) {
-    console.log(`saga.tryLogin error = ${err}`)
-  }
 }
 
 
