d94fb98ae80d34232a511867e168d6bad548379c
songzhw <songzhw2012@gmail.com>
2019-02-18 21:44:12 -0500

    remove one item, and return new object

--- ReactNative/RN5/js/tmp.js
+++ ReactNative/RN5/js/tmp.js
@@ -4,17 +4,12 @@ let item = {
   isMale: false
 }
 
-let r1 = item
-console.log(r1 === item)  //=> true
+let two = Object.keys(item)
+  .reduce((accumulatedObj, key) => {
+    if(key !== 'isMale') {
+      return {...accumulatedObj, [key]: item[key]}
+    }
+    return accumulatedObj
+  }, {}) // reduce(lambda, initObject)
 
-let r2 = Object.assign({}, item, {id: 100, isMale: true})
-console.log(`${JSON.stringify(r2)}`)  //=> {"id":100,"name":"szw","isMale":true}
-console.log(r2 === item)  //=> false
-
-let r3 = {...item, id: 100}
-console.log(`${JSON.stringify(r3)}`)  //=> {"id":100,"name":"szw","isMale":false}
-console.log(r3 === item)  //=> false
-
-let r4 = {...item, age: 12}
-console.log(r4)  //=> { id: 23, name: 'szw', isMale: false, age: 12 }
-console.log(r4 === item)  //=> false
+console.log(two)
