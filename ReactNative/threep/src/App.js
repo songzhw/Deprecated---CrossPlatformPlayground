f5f81cd952fefa815232ee9503222cc79d19784f
songzhw <songzhw2012@gmail.com>
2019-02-20 11:52:42 -0500

    Button looks okay in all three platforms

--- ReactNative/threep/src/App.js
+++ ReactNative/threep/src/App.js
@@ -8,7 +8,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -27,6 +27,7 @@ export default class App extends Component<Props> {
         <Text style={styles.welcome}>Welcome to React Native (3 platfrom)!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <Button onPress={()=> console.log(`szw click`)} title="click me"/>
       </View>
     );
   }
