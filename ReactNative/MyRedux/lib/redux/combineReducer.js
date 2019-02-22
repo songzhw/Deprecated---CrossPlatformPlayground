44e8156993961929981c69019c23ee7ccd23df1f
songzhw <songzhw2012@gmail.com>
2019-02-22 16:01:54 -0500

    final version of `combineReducers`

--- ReactNative/MyRedux/lib/redux/combineReducer.js
+++ ReactNative/MyRedux/lib/redux/combineReducer.js
@@ -1,5 +1,6 @@
 // 类似组合模式, 仍要返回一个reducers
 
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 /* @param reducers : 格式如: {r1, r2}*/
 const combineReducers = reducers => {
   return (state = {}, action) => {
@@ -7,25 +8,14 @@ const combineReducers = reducers => {
       .reduce(
         (accumulatedState, key) => {
           let currentReducer = reducers[key];
-          let stateInThisStage = currentReducer(state, action);
+          let stateInThisStage = currentReducer(state[key], action);
           return { ...accumulatedState, [key]: stateInThisStage };
         },
         {});
   };
 };
 
-const r1 = (state, action) => {
-  return { ...state, id: action.id };
-};
-
-const r2 = (state, action) => {
-  return { ...state, name: action.name };
-};
-
-let state = { r1: { id: 100 }, r2: { name: '200' } };
-const reducers = combineReducers({ r1, r2 });
-let newState = reducers(state, { id: 23, name: "szw", age: 25 });
-console.log(newState);
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 /*
 [Demo 1]
@@ -37,15 +27,13 @@ console.log(newState);
     return { ...state, name: action.name };
   };
 
-  let state = { one: 1, two: 2 };
+  let state = { r1: { id: 100, name: "100" }, r2: { id: 200, name: "200" } };
   const reducers = combineReducers({ r1, r2 });
   let newState = reducers(state, { id: 23, name: "szw", age: 25 });
-  console.log(newState);
-  //=> { r1: { one: 1, two: 2, id: 23 },
-  //     r2: { one: 1, two: 2, name: 'szw' }
-  //   }
+  console.log(newState); //=> { r1: { id: 23, name: '100' }, r2: { id: 200, name: 'szw' } }
 */
 
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 
 /*
 [Demo 2]
@@ -58,18 +46,21 @@ console.log(newState);
     return { ...state, name: action.name };
   };
 
+
   const r3 = (state, action) => {
     return { ...state, color: action.color };
   };
 
-  let state = { one: 1, two: 2 };
+  let state = {
+    reducers1: { r1: { id: 100, name: "100" }, r2: { id: 200, name: "200" } },
+    r3: { id: 300 }
+  };
   const reducers1 = combineReducers({ r1, r2 });
   const reducers = combineReducers({ reducers1, r3 });
   let newState = reducers(state, { id: 23, name: "szw", age: 25, color: "red" });
   console.log(newState);
-  //=> { reducers1:
-    //    { r1: { one: 1, two: 2, id: 23 },
-    //      r2: { one: 1, two: 2, name: 'szw' } },
-    //   r3: { one: 1, two: 2, color: 'red' }
-    // }
+  //=> { reducers1: { r1: { id: 23, name: '100' }, r2: { id: 200, name: 'szw' } },
+  //     r3:        { id: 300, color: 'red' }
+  //   }
 */
+// = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
