bfb99155eba61dec3d65999e3ba507bad306c519
songzhw <songzhw2012@gmail.com>
2019-02-22 15:23:11 -0500

    write combineReducer: error - currentReducer is not a function

---
+++ ReactNative/MyRedux/lib/redux/combineReducer.js
@@ -0,0 +1,27 @@
+// 类似组合模式, 仍要返回一个reducers
+/* @param reducers : 格式如: {r1, r2}*/
+const combineReducers = reducers => {
+  return (state = {}, action) => {
+    return Object.keys(reducers)
+      .reduce(
+        (accumulatedState, currentReducer) => {
+          let stateInThisStage = currentReducer(state, action);
+          return { ...state, ...stateInThisStage };
+        },
+        state);
+  };
+};
+
+
+const r1 = (state, action) => {
+  return { ...state, id: action.id };
+};
+
+const r2 = (state, action) => {
+  return { ...state, name: action.name };
+};
+
+let state = { one: 1, two: 2 };
+const reducers = combineReducers({ r1, r2 });
+let newState = reducers(state, { id: 23, name: "szw", age: 25 });
+console.log(newState);
