15ae3c31d57f3c3924feda5ffd746468171c83f2
songzhw <songzhw2012@gmail.com>
2019-02-22 15:28:21 -0500

    combined reducers is still okay to use

--- ReactNative/MyRedux/lib/redux/combineReducer.js
+++ ReactNative/MyRedux/lib/redux/combineReducer.js
@@ -14,6 +14,23 @@ const combineReducers = reducers => {
 };
 
 
+/*
+[Demo ]
+  const r1 = (state, action) => {
+    return { ...state, id: action.id };
+  };
+
+  const r2 = (state, action) => {
+    return { ...state, name: action.name };
+  };
+
+  let state = { one: 1, two: 2 };
+  const reducers = combineReducers({ r1, r2 });
+  let newState = reducers(state, { id: 23, name: "szw", age: 25 });
+  console.log(newState); //=> { one: 1, two: 2, id: 23, name: 'szw' }
+ */
+
+
 const r1 = (state, action) => {
   return { ...state, id: action.id };
 };
@@ -22,7 +39,12 @@ const r2 = (state, action) => {
   return { ...state, name: action.name };
 };
 
+const r3 = (state, action) => {
+  return { ...state, color: action.color };
+};
+
 let state = { one: 1, two: 2 };
-const reducers = combineReducers({ r1, r2 });
-let newState = reducers(state, { id: 23, name: "szw", age: 25 });
-console.log(newState);
+const reducers1 = combineReducers({ r1, r2 });
+const reducers = combineReducers({ reducers1, r3 });
+let newState = reducers(state, { id: 23, name: "szw", age: 25, color: "red" });
+console.log(newState); //=> { one: 1, two: 2 }
