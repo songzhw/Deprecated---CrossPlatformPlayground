691426388bcdf61c075f8600ae998b7a8e115d1f
songzhw <songzhw2012@gmail.com>
2019-02-27 12:33:34 -0500

    change the BeatingHeart from class component to functional component (1. this.props.foo turns into the foo argument of this function;  2. no render method, just return the jsx in the lambda)

--- ReactNative/preTaste/src/component/BeatingHeart.js
+++ ReactNative/preTaste/src/component/BeatingHeart.js
@@ -1,19 +1,13 @@
 import React from "react";
 import { View, StyleSheet, Animated, Image } from "react-native";
 
-class BeatingHeart extends React.Component {
-  state = {};
-
-  render() {
-    return (
-      <Animated.View style={[styles.animHeart, { transform: [{ scale: this.props.scale }] }]}>
-        <Image source={require("../../assets/icon_red_heart.png")}
-               resizeMode="contain"
-               style={styles.heart}/>
-      </Animated.View>
-    );
-  }
-}
+const BeatingHeart = ({ scale }) => (
+  <Animated.View style={[styles.animHeart, { transform: [{ scale: scale }] }]}>
+    <Image source={require("../../assets/icon_red_heart.png")}
+           resizeMode="contain"
+           style={styles.heart}/>
+  </Animated.View>
+);
 
 const styles = StyleSheet.create({
   animHeart: {
