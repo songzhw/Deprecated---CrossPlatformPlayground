caa993d62d05854026e9912c7fa05abaa7c5685b
songzhw <songzhw2012@gmail.com>
2019-02-27 12:46:46 -0500

    anim once, then got the error : "_this.pulse" is not a function

--- ReactNative/preTaste/src/screen/Pulse_Func_Screen.js
+++ ReactNative/preTaste/src/screen/Pulse_Func_Screen.js
@@ -1,19 +1,43 @@
-import React, { Component } from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, { useEffect, useRef } from "react";
+import { View, StyleSheet, Text, Animated } from "react-native";
+import BeatingHeart from "../component/BeatingHeart";
 
-class Pulse_Func_Screen extends Component {
-  render() {
-    return (
-      <View style={styles.root}>
-        <Text>Pulse_Func_Screen Screen</Text>
-      </View>
-    );
-  }
-}
+const usePulse = () => {
+  const scale = new Animated.Value(1);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => pulse(), 500);
+    return () => clearTimeout(timeout);
+  }, []);
+
+  const pulse = () => {
+    // 顺序执行
+    Animated.sequence([
+      Animated.timing(scale, { toValue: 1.2 }),
+      Animated.timing(scale, { toValue: 0.8 })
+    ])
+      .start(() => this.pulse());
+    // 无限执行
+  };
+
+  return scale;
+};
+
+const Pulse_Func_Screen = () => {
+  const scale = usePulse();
+
+  return (
+    <View style={styles.root}>
+      <BeatingHeart scale={scale}/>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   root: {
-    flex: 1
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "space-around"
   }
 });
 
