0002e018dbce143bb23c3f7581bdcccb71f47346
songzhw <songzhw2012@gmail.com>
2019-02-28 16:30:58 -0500

    test saga, failed?

---
+++ ReactNative/RN5/__tests__/saga.test.js
@@ -0,0 +1,23 @@
+import { call } from "redux-saga/effects";
+import { expectSaga } from "redux-saga-test-plan";
+import * as matchers from "redux-saga-test-plan/matchers";
+import saga from "../js/redux/saga";
+import { GOT_SCHEDULE, TRY_SCHEDULE } from "../js/redux/F8Reducer";
+import { axios } from "../js/core/HttpEngine";
+import { API_ID_DAY1, API_ID_DAY2 } from "../js/core/apiConstants";
+
+test("fetch schdules", () => {
+  jest.setTimeout(300000);
+  expectSaga.DEFAULT_TIMEOUT = 300000;
+  return expectSaga(saga)
+    .dispatch({ type: TRY_SCHEDULE })
+    .provide([
+      [call(axios.get, API_ID_DAY1), { data: { r1: "r1" } }],
+      [call(axios.get, API_ID_DAY2), { data: { r2: "r2" } }]
+    ])
+    .put({
+      type: GOT_SCHEDULE,
+      payload: { day1: { r1: "r1" }, day2: { r2: "r2" } }
+    })
+    .run();
+});
