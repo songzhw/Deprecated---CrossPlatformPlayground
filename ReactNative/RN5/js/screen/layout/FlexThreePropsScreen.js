d66a63f26880dfc3d532dcd0a3cd8c058447e7a6
songzhw <songzhw2012@gmail.com>
2019-03-01 11:47:13 -0500

    flex, flexGrow

--- ReactNative/RN5/js/screen/layout/FlexThreePropsScreen.js
+++ ReactNative/RN5/js/screen/layout/FlexThreePropsScreen.js
@@ -1,32 +1,34 @@
 import React, { Component } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
-class FlexThreePropsScreen extends Component {
-  render() {
-    return (
-      <View style={styles.root}>
-        <Text style={styles.title} numberOfLines={1}>箱根温泉</Text>
-        <Text style={styles.flags} numberOfLines={1}>温泉</Text>
-        <Text style={styles.price} numberOfLines={1}>FlexThreePropsScreen Screen</Text>
-      </View>
-    );
-  }
-}
+const FlexThreePropsScreen = () => (
+  <View style={styles.root}>
+    <Text style={styles.title} numberOfLines={1}>箱根温泉箱根温泉箱根温泉箱根温泉箱根温泉</Text>
+    <Text style={styles.flags} numberOfLines={1}>温泉</Text>
+    <Text style={styles.price} numberOfLines={1}>FlexThreePropsScreen Screen</Text>
+  </View>
+);
 
 const styles = StyleSheet.create({
   // 一个红色边框, 表示边界
   root: {
-    flex: 1,
     height: 50,
+    margin: 20,
     flexDirection: "row",
     borderWidth: 2,
     borderColor: "#E91e63"
   },
-  title: {},
-  flags: {},
+  title: {
+    flexGrow: 1,
+    backgroundColor: "#ff9800"
+  },
+  flags: {
+    flexGrow: 1,
+    backgroundColor: "#7e57c2"
+  },
   price: {
-    backgroundColor: "#90caf9",
-    textAlign: "right"
+    flexGrow: 1,
+    backgroundColor: "#90caf9"
   }
 });
 
