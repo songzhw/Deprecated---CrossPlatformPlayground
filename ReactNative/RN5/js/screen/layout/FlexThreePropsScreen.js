604a308cb487ef2618081f34df4e27506d0d9b35
songzhw <songzhw2012@gmail.com>
2019-03-01 10:29:43 -0500

    if you set the "flex: 1" to the root view, then your "height" props will be invalid

--- ReactNative/RN5/js/screen/layout/FlexThreePropsScreen.js
+++ ReactNative/RN5/js/screen/layout/FlexThreePropsScreen.js
@@ -5,15 +5,28 @@ class FlexThreePropsScreen extends Component {
   render() {
     return (
       <View style={styles.root}>
-        <Text>FlexThreePropsScreen Screen</Text>
+        <Text style={styles.title} numberOfLines={1}>箱根温泉</Text>
+        <Text style={styles.flags} numberOfLines={1}>温泉</Text>
+        <Text style={styles.price} numberOfLines={1}>FlexThreePropsScreen Screen</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  // 一个红色边框, 表示边界
   root: {
-    flex: 1
+    flex: 1,
+    height: 50,
+    flexDirection: "row",
+    borderWidth: 2,
+    borderColor: "#E91e63"
+  },
+  title: {},
+  flags: {},
+  price: {
+    backgroundColor: "#90caf9",
+    textAlign: "right"
   }
 });
 
