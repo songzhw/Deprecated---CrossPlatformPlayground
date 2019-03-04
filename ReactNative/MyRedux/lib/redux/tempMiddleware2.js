7dc8134ccd4b0eca9d0da3a3116eace253f095a9
songzhw <songzhw2012@gmail.com>
2019-03-04 06:59:00 -0500

    middleware prototype 5

--- ReactNative/MyRedux/lib/redux/tempMiddleware2.js
+++ ReactNative/MyRedux/lib/redux/tempMiddleware2.js
@@ -1,30 +1,28 @@
 import { createStore } from "./createStore";
 
-const addLogToDispatch = (store) => (next) => {
-
-  // return another dispatch(action) method
-  return (action) => {
-    console.group(action.type);
-    console.log("%c previous state", "color: gray", store.getState());
-    const ret = next(action);
-    console.log("%c next state", "color: green", store.getState());
-    console.group(action.type);
-    return ret;
-  };
+// const addLogToDispatch = (store) => (next) => {
+//   return (action) => {
+const addLogToDispatch = (store) => (next) => (action) => {
+  console.group(action.type);
+  console.log("%c previous state", "color: gray", store.getState());
+  const ret = next(action);
+  console.log("%c next state", "color: green", store.getState());
+  console.group(action.type);
+  return ret;
 };
 
-const addHttpToDispatch = (store) => (next) => {
-  return (action) => {
-    if (action.type === "ACTION_HTTP_REQUEST") {
-      console.log(`Mimic fetch(action.url, action.header, action.body)`);
-      let fakeResponseAction = { type: ACTION_HTTP_RESPONSE, payload: { name: "resp" } };
-      return next(fakeResponseAction);
-    }
-  };
+// const addHttpToDispatch = (store) => (next) => {
+//   return (action) => {
+const addHttpToDispatch = (store) => (next) => (action) => {
+  if (action.type === "ACTION_HTTP_REQUEST") {
+    console.log(`Mimic fetch(action.url, action.header, action.body)`);
+    let fakeResponseAction = { type: ACTION_HTTP_RESPONSE, payload: { name: "resp" } };
+    return next(fakeResponseAction);
+  }
 };
 
 
-const configStore2 = (reducer) => {
+const configStore3 = (reducer) => {
   const store = createStore(reducer);
 
   const middlewares = [];
@@ -36,5 +34,5 @@ const configStore2 = (reducer) => {
 
 
 const wrapDispatchWithMiddlewares = (store, middlewares) => {
-  middlewares.forEach(middleware => store.dispatch = middleware(store)(store.dispatch));
+  middlewares.slice().reverse().forEach(middleware => store.dispatch = middleware(store)(store.dispatch));
 };
