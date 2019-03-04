74509ddbcabd209844a1b87c9998d4618823b4a7
songzhw <songzhw2012@gmail.com>
2019-03-03 22:39:19 -0500

    middleware prototype 2

--- ReactNative/MyRedux/lib/redux/tempMiddleware1.js
+++ ReactNative/MyRedux/lib/redux/tempMiddleware1.js
@@ -1,13 +1,13 @@
 import { createStore } from "./createStore";
 
 const addLogToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
+  const next = store.dispatch;
 
   // return another dispatch(action) method
   return (action) => {
     console.group(action.type);
     console.log("%c previous state", "color: gray", store.getState());
-    const ret = rawDispatch(action);
+    const ret = next(action);
     console.log("%c next state", "color: green", store.getState());
     console.group(action.type);
     return ret;
@@ -15,13 +15,13 @@ const addLogToDispatch = (store) => {
 };
 
 const addHttpToDispatch = (store) => {
-  const rawDispatch = store.dispatch;
+  const next = store.dispatch;
 
   return (action) => {
     if (action.type === "ACTION_HTTP_REQUEST") {
       console.log(`Mimic fetch(action.url, action.header, action.body)`);
       let fakeResponseAction = { type: ACTION_HTTP_RESPONSE, payload: { name: "resp" } };
-      return rawDispatch(fakeResponseAction);
+      return next(fakeResponseAction);
     }
   };
 };
