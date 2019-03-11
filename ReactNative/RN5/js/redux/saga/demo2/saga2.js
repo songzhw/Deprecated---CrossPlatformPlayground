89ddac5d8ffc595ecb44e64356570c09a01d3ce7
songzhw <songzhw2012@gmail.com>
2019-03-11 11:24:31 -0400

    use fork() to delay the channel/newspaper work, at the mean time we listen to the channel

--- ReactNative/RN5/js/redux/saga/demo2/saga2.js
+++ ReactNative/RN5/js/redux/saga/demo2/saga2.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put, take } from "redux-saga/effects";
+import { takeEvery, call, put, take, fork, delay } from "redux-saga/effects";
 import { CHANNEL_DEMO2, CHANNEL_REQUEST2, getChannelResultAction2 } from "../../research/channel2Reducer";
 import Newspaper from "../ex/Newspaper";
 import tripleChannel from "./channel2";
@@ -8,7 +8,9 @@ const sagas = function* () {
   yield takeEvery(CHANNEL_DEMO2, onGotAction);
 };
 
-const onRequest = function* (aciton) {
+const onRequest = function* (action) {
+  yield fork(delayJob, action);
+
   const channel = yield call(tripleChannel, Newspaper);
   try {
     while (true) {
@@ -20,8 +22,13 @@ const onRequest = function* (aciton) {
   }
 };
 
+const delayJob = function* (action) {
+  yield delay(100);
+  yield call(onGotAction, action);
+};
+
 const onGotAction = function* (action) {
-  console.log(`szw saga2 got action : ${JSON.stringify(action)}`);
+  console.log(`szw saga2 got action`);
   Newspaper.createNews();
 };
 
--- ReactNative/RN5/js/screen/redux/SagaChannelScreen.js
+++ ReactNative/RN5/js/screen/redux/SagaChannelScreen.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { View, StyleSheet, Text, Button } from "react-native";
 import { connect } from "react-redux";
 import { getChannelDemoAction } from "../../redux/research/channelDemoReducer";
-import { getChannelDemoAction2 } from "../../redux/research/channel2Reducer";
+import { requestChannel2 } from "../../redux/research/channel2Reducer";
 
 class SagaChannelScreen extends Component {
   render() {
@@ -11,7 +11,7 @@ class SagaChannelScreen extends Component {
       <View style={styles.root}>
         <Text>SagaChannelScreen Screen</Text>
         <Button title="send action * 2" onPress={() => this.props.dispatch(getChannelDemoAction())}/>
-        <Button title="send action * 3" onPress={() => this.props.dispatch(getChannelDemoAction2())}/>
+        <Button title="send action * 3" onPress={() => this.props.dispatch(requestChannel2())}/>
         <Text> {this.props.text1} </Text>
         <Text> {this.props.text2} </Text>
       </View>
