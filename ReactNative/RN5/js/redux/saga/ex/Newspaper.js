d486b196d1aa0ece3d399a0ee38d8dc887891721
songzhw <songzhw2012@gmail.com>
2019-03-11 10:44:25 -0400

    remove the unlimited loop

--- ReactNative/RN5/js/redux/saga/ex/Newspaper.js
+++ ReactNative/RN5/js/redux/saga/ex/Newspaper.js
@@ -19,6 +19,11 @@ export function createNews() {
     listenerFunc(id++);
   }, 1000);
 
+  setTimeout(() => {
+    clearInterval(handler);
+    console.log(`szw newspaper finishes!`)
+  }, 3333);
+
   /*
   // 这一段代码就会有问题, saga与reducer就收不到gkwr
   for (let i = 0; i < 100; i++) {
