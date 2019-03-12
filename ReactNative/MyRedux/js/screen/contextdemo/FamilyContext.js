128138534d210df4b6168a3e5eebd64f69900a64
songzhw <songzhw2012@gmail.com>
2019-03-11 20:43:01 -0400

    higher-order function

--- ReactNative/MyRedux/js/screen/contextdemo/FamilyContext.js
+++ ReactNative/MyRedux/js/screen/contextdemo/FamilyContext.js
@@ -4,3 +4,17 @@ const FamilyContext = React.createContext({});
 
 export const FamilyProvider = FamilyContext.Provider;
 export const FamilyConsumer = FamilyContext.Consumer;
+
+function throttle(fn, intervalTime) {
+  var isDoingIt = false;
+  return function() {
+    if (isDoingIt) return;
+
+    isDoingIt = true;
+    fn.apply(this, arguments);
+    setTimeout(() => isDoingIt = false, intervalTime);
+  };
+}
+
+// window.onresize = function() {
+window.onresize = throttle(() => console.log(`resize`));
