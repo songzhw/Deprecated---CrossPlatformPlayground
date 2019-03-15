ce1c36d447da5b4df23fe3d6b9913915ed23ff92
songzhw <songzhw2012@gmail.com>
2019-03-15 13:14:23 -0400

    encryption

--- ReactNative/tmp/lib_play/src/redux/store.js
+++ ReactNative/tmp/lib_play/src/redux/store.js
@@ -4,6 +4,8 @@ import { createLogger } from "redux-logger";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 
+import createEncryptor from 'redux-persist-transform-encrypt'
+
 import InputNameReducer from "./InputNameReducer";
 import GoodsReducer from "./GoodsReducer";
 
@@ -12,10 +14,18 @@ middlewares.push(createLogger());
 
 const reducer = combineReducers({ InputNameReducer, GoodsReducer });
 
+const encryptor = createEncryptor({
+  secretKey: 'my-super-secret-key',
+  onError: function(error) {
+    console.log(`szw persist error when encrypt : ${JSON.stringify(error)}`)
+  }
+})
+
 const persistRuducerConfig = {
   key: "k2",
   storage,
-  whitelist: ['InputNameReducer']
+  whitelist: ['InputNameReducer'],
+  transforms: [encryptor]
 };
 const persistedReducer = persistReducer(persistRuducerConfig, reducer);
 
