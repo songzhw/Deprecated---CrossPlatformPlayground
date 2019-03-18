09928a0e517257a73dccb99b87e3420aef156bcc
songzhw <songzhw2012@gmail.com>
2019-03-18 16:28:42 -0400

    update my Button

--- ReactNative/RN5/js/screen/hooks/HooksAsyncTrapScreen.js
+++ ReactNative/RN5/js/screen/hooks/HooksAsyncTrapScreen.js
@@ -3,11 +3,19 @@ import { View, StyleSheet, Text } from "react-native";
 import Button from "../../component/Button";
 
 const HooksAsyncTrapScreen = () => {
+  function onStart() {
+    console.log(`szw start`);
+  };
+
+  function onStop() {
+    console.log(`szw stop`);
+  };
+
   return (
     <View style={styles.root}>
       <Text style={styles.lable}>HooksAsyncTrapScreen Screen</Text>
-      <Button text="Start" style={styles.btn}/>
-      <Button text="Stop" style={styles.btn}/>
+      <Button text="Start" style={styles.btn} onPress={()=> onStart()}/>
+      <Button text="Stop" style={styles.btn} onPress={onStop}/>
     </View>
   );
 };
@@ -16,7 +24,7 @@ const HooksAsyncTrapScreen = () => {
 const styles = StyleSheet.create({
   root: {
     flex: 1,
-    alignItems: 'center'
+    alignItems: "center"
   },
   lable: {
     fontSize: 22
