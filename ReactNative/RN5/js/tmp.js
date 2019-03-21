caa7b24d8b62332e1715b1431e8ccd7e7e653e34
songzhw <songzhw2012@gmail.com>
2019-03-21 13:48:14 -0400

    what is if(obj)

---
+++ ReactNative/RN5/js/tmp.js
@@ -0,0 +1,38 @@
+let one = {};
+let two;
+let three = { id: 23 };
+
+if(one){
+  console.log(`one ✔`)
+} else {
+  console.log(`one X`)
+}
+
+
+if(two){
+  console.log(`two ✔`)
+} else {
+  console.log(`two X`)
+}
+
+if(three){
+  console.log(`three ✔`)
+} else {
+  console.log(`three X`)
+}
+console.log(``)
+// = = = = = = = = = = = =
+let four = null
+let five = undefined
+
+if(four){
+  console.log(`four ✔`)
+} else {
+  console.log(`four X`)
+}
+
+if(five){
+  console.log(`five ✔`)
+} else {
+  console.log(`five X`)
+}
