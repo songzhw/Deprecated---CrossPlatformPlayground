a3273d5d2d5454fc4cf6738ecb03d0d73336b1f6
songzhw <songzhw2012@gmail.com>
2019-03-26 16:04:33 -0400

    so I took a different approach to pass the test, and it succeed!

--- ReactNative/RN5/__tests__/MySecondContainer.test.js
+++ ReactNative/RN5/__tests__/MySecondContainer.test.js
@@ -11,7 +11,30 @@ describe("second", () => {
         onPress={func}
       />
     );
-    wrapper.simulate("press");
-    expect(func).toBeCalled();
+
+    // wrapper.simulate("press");
+    // expect(func).toBeCalled();
+
+    const target = wrapper.findWhere(item => item.prop("onPress") !== undefined);
+    expect(target.prop("onPress")).toBe(func);
+
   });
 });
+
+/*
+console.log(wrapper.debug())的结果:
+
+      <MyButtonContainer>
+        <MyButton title="Inner2" onPress={[Function: mockConstructor]} />
+      </MyButtonContainer>
+
+ */
+
+/*
+下面的测试是失败的:
+    wrapper.simulate("press");
+    expect(func).toBeCalled();
+
+可见simulate(event)只能传递一层
+
+ */
