4c59949f2bab1618761d78b2bf2648bd89845d2e
songzhw <songzhw2012@gmail.com>
2019-04-01 09:28:31 -0400

    useEffect is async

--- ReactNative/RN5/js/screen/hooks/VariableScopeIssue.js
+++ ReactNative/RN5/js/screen/hooks/VariableScopeIssue.js
@@ -5,15 +5,25 @@ export const VariableScopeIssue = () => {
   const [count, setCount] = useState(0);
   const [computed, setComputed] = useState(0);
 
+  console.log(`szw render before useEffect, count=${count}, computer=${computed}`);
   useEffect(() => {
+    console.log(`szw render within useEffect, count=${count}, computer=${computed}`);
     setComputed(count + 1);
   }, [count]);
+  console.log(`szw render after  useEffect, count=${count}, computer=${computed}`);
 
   let text = `count = ${count}, computed = ${computed}`;
   return (
     <View>
-      <Text> {text} </Text>
+      <Text style={styles.text}> {text} </Text>
       <Button title={"+ 1"} onPress={() => setCount(count + 1)}/>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  text: {
+    color: "black",
+    fontSize: 22
+  }
+});
