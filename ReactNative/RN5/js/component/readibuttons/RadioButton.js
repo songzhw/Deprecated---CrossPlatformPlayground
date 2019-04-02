d8e1bd5b1f42dabe10d36896288d10b3ad9e1994
songzhw <songzhw2012@gmail.com>
2019-04-01 21:22:32 -0400

    radio button

--- ReactNative/RN5/js/component/readibuttons/RadioButton.js
+++ ReactNative/RN5/js/component/readibuttons/RadioButton.js
@@ -1,16 +1,33 @@
 import React from "react";
 import { StyleSheet, View, Text } from "react-native";
 
-export const RadioButton = ({ text, height,  }) => {
+export const RadioButton = ({ text, circleSize = 24, textSize = 20 }) => {
+  const outterCircleStyle = {
+    width: circleSize,
+    height: circleSize,
+    borderWidth: circleSize * 0.1,
+    borderRadius: circleSize / 2,
+    borderColor: "blue"
+  };
+
+  const innerCircleStyle = {};
+
+  const textStyle = {
+    fontSize: textSize,
+    color: "black",
+    marginLeft: circleSize / 3
+  };
+
   return (
-    <View>
-      <View style={styles.outterCircle}/>
-      <Text>radio button</Text>
+    <View style={styles.root}>
+      <View style={outterCircleStyle}/>
+      <Text style={textStyle}>radio button</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  outterCircle: {},
-  innnerCiricle: {}
+  root: {
+    flexDirection: "row"
+  }
 });
