f6091a7f180351e4d379bff228fe97fd5e12fb54
songzhw <songzhw2012@gmail.com>
2019-04-05 23:33:08 -0400

    add another component

---
+++ React/react101/src/Rtutor/WarningBeforeLearning.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+class WarningBeforeLearning extends React.Component {
+  public render() {
+    return (
+      <div className="confirm-wrapper confirm-visible">
+        <div className="confirm-container">
+          <div className="confirm-title-container">
+            <span>Start to learn React + Typescript</span>
+          </div>
+          <div className="confirm-content-container">
+            <p>Are you ready?</p>
+          </div>
+          <div className="confirm-buttons-container">
+            <button>No, thanks</button>
+            <button>Yes, please</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default WarningBeforeLearning;
--- React/react101/src/app/App.tsx
+++ React/react101/src/app/App.tsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
-import logo from "./res/logo.svg";
-import "./css/App.css";
+import logo from "./logo.svg";
+import "./App.css";
+import WarningBeforeLearning from "../Rtutor/WarningBeforeLearning";
 
 class App extends Component {
   public render() {
@@ -8,12 +9,16 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-            Hello, React (+ TypeScript)
+          Hello, React (+ TypeScript)
           <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
-          <a className="App-link" href="https://www.typescriptlang.org/docs/handbook/basic-types.html" target="_blank"> Learn TypeScript </a>
+          <a className="App-link" href="https://www.typescriptlang.org/docs/handbook/basic-types.html"
+             target="_blank"> Learn TypeScript </a>
+
+          <WarningBeforeLearning/>
         </header>
       </div>
     );
   }
 }
+
 export default App;
