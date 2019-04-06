b2ae499f5748b2f1c16164dfa20f19a22ed3d087
songzhw <songzhw2012@gmail.com>
2019-04-05 21:32:28 -0400

    first screen

--- React/react101/src/App.tsx
+++ React/react101/src/App.tsx
@@ -8,17 +8,9 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo"/>
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            Hello, React (+ TypeScript)
+          <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer"> Learn React </a>
+          <a className="App-link" href="https://www.typescriptlang.org/docs/handbook/basic-types.html" target="_blank"> Learn TypeScript </a>
         </header>
       </div>
     );
