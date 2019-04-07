2bacf9fee18380d0afde086ee513694a7f287690
songzhw <songzhw2012@gmail.com>
2019-04-07 16:45:52 -0400

    useState() with Type

--- React/react101/src/Rtutor/router/MovieScreen.tsx
+++ React/react101/src/Rtutor/router/MovieScreen.tsx
@@ -1,9 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
+
+export interface IMovie {
+  cover: string;
+  title: string;
+  genres: string[];
+}
+
+interface IState {
+  movies: IMovie[];
+}
 
 export const MovieScreen: React.FunctionComponent = () => {
+  const [movies, setMovies] = useState([{id:0, cover:"", title:""}]);
 
   useEffect(() => {
-    fetch("https://www.mocky.io/v2/5caa5b8e3000001607904577")
+    fetch("https://www.mocky.io/v2/5caa6095300000440790457c")
       .then(response => {
         if (response.status !== 200) {
           console.error("response error : code = ", response.status);
@@ -11,12 +22,19 @@ export const MovieScreen: React.FunctionComponent = () => {
         }
 
         response.json()
-          .then(resp => console.log(resp));
+          .then(resp => setMovies(resp));
       })
       .catch(err => console.error(err));
   }, []);
 
   return (
-    <div/>
+    <div>
+      <p>Welcome!</p>
+      <ul>
+        {movies.map( movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 };
