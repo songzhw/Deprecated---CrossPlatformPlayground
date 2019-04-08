ff44ff848731eb72f4c7eaf0ca3775945f494728
songzhw <songzhw2012@gmail.com>
2019-04-08 16:14:07 -0400

    add a notFound page

---
+++ React/react101/src/Rtutor/router/NotFoundScreen.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+export const NotFoundScreen = () => {
+  return (
+    <div>
+      <h2> Page not found</h2>
+    </div>
+  );
+};
+
--- React/react101/src/core/Routes.tsx
+++ React/react101/src/core/Routes.tsx
@@ -4,6 +4,7 @@ import { Header } from "../Rtutor/router/Header";
 import { AdminScreen } from "../Rtutor/router/AdminScreen";
 import { MovieListScreen } from "../Rtutor/router/MovieListScreen";
 import { MovieScreen } from "../Rtutor/router/MovieScreen";
+import { NotFoundScreen } from "../Rtutor/router/NotFoundScreen";
 
 export const Routes: React.FunctionComponent = () => {
   return (
@@ -13,6 +14,7 @@ export const Routes: React.FunctionComponent = () => {
         <Route exact={true} path="/movie" component={MovieListScreen}/>
         <Route path="/admin" component={AdminScreen}/>
         <Route path="/movie/:id" component={MovieScreen}/>
+        <Route component={NotFoundScreen}/>
       </div>
     </BrowserRouter>
   );
