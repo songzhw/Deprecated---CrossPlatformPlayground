2376fb221885e22d9063ad688cedcd830928d8e3
songzhw <songzhw2012@gmail.com>
2019-04-07 21:37:17 -0400

    add a stylesheet

--- React/react101/src/Rtutor/router/Header.tsx
+++ React/react101/src/Rtutor/router/Header.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import iconBack from './icon_back.png'
 import { Link } from "react-router-dom";
+import './Header.css'
 
 export const Header: React.FunctionComponent = () => {
   return (
-    <header style={{backgroundColor: 'blue'}}>
-      <img src={iconBack} alt="back"/>
-      <Link to="/movie"> Movies </Link>
-      <Link to="/admin"> Admin </Link>
+    <header className="header">
+      <img src={iconBack} alt="back" className="imgBack"/>
+      <Link to="/movie" className="headerLink"> Movies </Link>
+      <Link to="/admin" className="headerLink"> Admin </Link>
     </header>
   )
 };
