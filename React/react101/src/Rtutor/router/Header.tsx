a46286434579c9e651b29dc7b8bf1d53d8381d01
songzhw <songzhw2012@gmail.com>
2019-04-07 21:48:58 -0400

    NavLink and styles

--- React/react101/src/Rtutor/router/Header.tsx
+++ React/react101/src/Rtutor/router/Header.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import iconBack from './icon_back.png'
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import './Header.css'
 
 export const Header: React.FunctionComponent = () => {
   return (
     <header className="header">
       <img src={iconBack} alt="back" className="imgBack"/>
-      <Link to="/movie" className="headerLink"> Movies </Link>
-      <Link to="/admin" className="headerLink"> Admin </Link>
+      <NavLink to="/movie" className="headerLink" activeClassName="activeHeaderLink"> Movies </NavLink>
+      <NavLink to="/admin" className="headerLink" activeClassName="activeHeaderLink"> Admin </NavLink>
     </header>
   )
 };
