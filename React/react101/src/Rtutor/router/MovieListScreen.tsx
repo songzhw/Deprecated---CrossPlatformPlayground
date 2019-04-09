96f73895d28076f04659de09c9215f685a99780f
songzhw <songzhw2012@gmail.com>
2019-04-09 09:43:17 -0400

    query parameters

--- React/react101/src/Rtutor/router/MovieListScreen.tsx
+++ React/react101/src/Rtutor/router/MovieListScreen.tsx
@@ -13,8 +13,8 @@ interface IState {
   searchKeyword: string;
 }
 
-export class MovieListScreen extends React.Component<{}, IState> {
-  public state : IState = { movies: [], searchKeyword: "" }; // 不加这个":IState", 后面使用时就会报错. 试一下就知道了
+export class MovieListScreen extends React.Component<RouteComponentProps, IState> {
+  public state: IState = { movies: [], searchKeyword: "" }; // 不加这个":IState", 后面使用时就会报错. 试一下就知道了
 
   public componentDidMount(): void {
     fetch("https://www.mocky.io/v2/5caa6095300000440790457c")
@@ -30,11 +30,21 @@ export class MovieListScreen extends React.Component<{}, IState> {
       .catch(err => console.error(err));
   }
 
+  public static getDerivedStateFromProps(props: RouteComponentProps, state: IState) {
+    const params = new URLSearchParams(props.location.search);
+    const keyword = params.get("search") || "";
+    return {
+      movies: state.movies,
+      searchKeyword: keyword
+    };
+  }
+
   public render(): React.ReactNode {
-    const { movies } = this.state;
+    const { movies, searchKeyword } = this.state;
     return (
       <div>
         <p>Welcome!</p>
+        <p> search = {searchKeyword}</p>
         <ul>
           {movies ? movies.map(movie => (
               <li key={movie.id}>
