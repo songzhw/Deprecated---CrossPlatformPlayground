7c6ea11ecc35e31f46b29dc23462ed1c2869bd2a
songzhw <songzhw2012@gmail.com>
2019-04-10 18:58:28 -0400

    TS - String liberty type

--- React/react101/src/__tests__/App.test.tsx
+++ React/react101/src/__tests__/App.test.tsx
@@ -1,9 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../app/App';
+import React from "react";
+import ReactDOM from "react-dom";
+// import App from '../app/App';
+//
+// it('renders without crashing', () => {
+//   const div = document.createElement('div');
+//   ReactDOM.render(<App />, div);
+//   ReactDOM.unmountComponentAtNode(div);
+// });
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+type Robot = "Android";
+// const robot : Robot = "ios";  // error
+let robot: Robot;
+robot = "Android";
+// robot = null;    // error
+// robot = undefined; // error
+
+
+type Platform = "Android" | "iOS" | "Web"
+let platform : Platform
+platform  = "Android";
+platform = 'iOS'
+// platform = undefined  // error
+
