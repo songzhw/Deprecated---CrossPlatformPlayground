ba9a1a0fc173b8919579e13cd0240327e82bf946
songzhw <songzhw2012@gmail.com>
2019-04-13 23:27:49 -0400

    error : "Property 'href' does not exist on type 'never'.  TS2339"

--- React/ereader/src/epubView/EpubView.tsx
+++ React/ereader/src/epubView/EpubView.tsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import Book from "epubjs/types/book";
+import { Book } from "epubjs";
+import { NavItem } from "epubjs/types/navigation";
+import Rendition from "epubjs/types/rendition";
 
 interface IProps {
   url: string;
@@ -7,17 +9,19 @@ interface IProps {
 
 interface IState {
   isLoaded: boolean;
-  tableOfContents: any; //TODO
+  tableOfContents: NavItem[];
 }
 
 export class EpubView extends Component<IProps, IState> {
   public state = { isLoaded: false, tableOfContents: [] };
   private book!: Book;
+  private viewRef!: any;
+  private rendition!: Rendition;
 
 
   public componentDidMount() {
     const { url } = this.props;
-    this.book = ePub(url, {});
+    this.book = new Book(url, {});
     this.book.loaded.navigation
       .then(({ toc }) => {
         this.setState({ isLoaded: true, tableOfContents: toc }, () => {
@@ -27,13 +31,20 @@ export class EpubView extends Component<IProps, IState> {
   }
 
   public populateRender = () => {
-
+    const { tableOfContents } = this.state;
+    console.log(`szw populate ${JSON.stringify(tableOfContents)}`);
+    const renderEpubOption = {
+      contained: true,
+      width: "100%",
+      height: "100%"
+    };
+    this.rendition = this.book.renderTo(this.viewRef, renderEpubOption);
+    this.rendition.display(tableOfContents[0].href);
   };
 
   public render(): React.ReactNode {
     return (
-      <div>
-        230
+      <div ref={c => (this.viewRef = c)}>
       </div>
     );
   }
