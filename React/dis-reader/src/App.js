5df21970ef5cd30f69d843184fe0a1b81be310c8
songzhw <songzhw2012@gmail.com>
2019-04-15 10:17:03 -0400

    download epub on server

--- React/dis-reader/src/App.js
+++ React/dis-reader/src/App.js
@@ -1,25 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import "./index.css";
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="appRootContainer">
+        <button className="buttonInHome" onClick={this.onDownload}>download epub</button>
+        <button className="buttonInHome">unzip epub</button>
+        {/*TODO read xml*/}
+        {/*TODO render xhtml*/}
       </div>
     );
   }
+
+  onDownload = () => {
+    const url = "https://gerhardsletten.github.io/react-reader/files/alice.epub";
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "arraybuffer";
+    xhr.onload = () => {
+      if (xhr.status && xhr.status < 400) {
+        console.log(`szw got resposne : ${xhr.status}`);
+        this.arraybufferResponse = xhr.response;
+      } else {
+        console.log(`szw error on downloading : ${xhr.status}`);
+      }
+    };
+    xhr.send();
+  };
 }
 
 export default App;
