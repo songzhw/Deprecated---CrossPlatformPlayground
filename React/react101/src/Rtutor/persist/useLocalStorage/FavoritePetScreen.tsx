495b631dda8cb3dea1a7210081f8b4d07b0d2333
songzhw <songzhw2012@gmail.com>
2019-04-16 15:09:47 -0400

    will two component effect each other? : the value in localStorage is same, but the two component shows different if it can

--- React/react101/src/Rtutor/persist/useLocalStorage/FavoritePetScreen.tsx
+++ React/react101/src/Rtutor/persist/useLocalStorage/FavoritePetScreen.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useContext } from "react";
 import { useLocalStorage } from "./useLocalStorage";
+import { Pet2 } from "./Pet2";
 
 export const FavoritePetScreen = () => {
   const [pet, setPet] = useLocalStorage("key1");
@@ -16,6 +17,8 @@ export const FavoritePetScreen = () => {
       <p> favorite = {pet}</p>
       <button data-pet="cat" data-color="ginger" onClick={onClick}> cat</button>
       <button data-pet="dog" data-color="white" onClick={onClick}> dog</button>
+      {/*another component*/}
+      <Pet2/>
     </div>
   );
 };
--- React/react101/src/Rtutor/persist/useLocalStorage/Pet2.tsx
+++ React/react101/src/Rtutor/persist/useLocalStorage/Pet2.tsx
@@ -1,1 +1,19 @@
 import React from "react";
+import { useLocalStorage } from "./useLocalStorage";
+
+export const Pet2 = () => {
+  const [pet, setPet] = useLocalStorage("key1");
+
+  function onClick(event: React.MouseEvent<HTMLButtonElement>) {
+    const name = event.currentTarget.dataset.pet!;
+    setPet(name);
+  }
+
+  return (
+    <div>
+      <p> favorite = {pet}</p>
+      <button data-pet="cat" data-color="ginger" onClick={onClick}> cat</button>
+      <button data-pet="dog" data-color="white" onClick={onClick}> dog</button>
+    </div>
+  );
+};
