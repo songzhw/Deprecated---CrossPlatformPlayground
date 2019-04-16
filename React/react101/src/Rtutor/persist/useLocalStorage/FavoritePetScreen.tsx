b694d36ef7ccab6c3cbedeffced1884449454d6b
songzhw <songzhw2012@gmail.com>
2019-04-16 14:12:58 -0400

    exited. Finally got to know how to pass arguments into the onClick's value (fuction)

--- React/react101/src/Rtutor/persist/useLocalStorage/FavoritePetScreen.tsx
+++ React/react101/src/Rtutor/persist/useLocalStorage/FavoritePetScreen.tsx
@@ -3,15 +3,18 @@ import React, { useState } from "react";
 export const FavoritePetScreen = () => {
   const [pet, setPet] = useState("");
 
-  function onClick(name: string) {
+  function onClick(event: React.MouseEvent<HTMLButtonElement>) {
+    const name = event.currentTarget.dataset.pet!;
+    const color = event.currentTarget.dataset.color!;
+    console.log(`szw button click ${name} - ${color}`);
     setPet(name);
   }
 
   return (
     <div>
       <p> favorite = {pet}</p>
-      <button onClick={onClick.bind(self,"cat")}> cat</button>
-      <button onClick={onClick.bind(self,"dog")}> dog</button>
+      <button data-pet="cat" data-color="ginger" onClick={onClick}> cat</button>
+      <button data-pet="dog" data-color="white" onClick={onClick}> dog</button>
     </div>
   );
 };
