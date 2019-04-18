9edd9d2af3bad416c65ce99c9988c3bc0a988954
songzhw <songzhw2012@gmail.com>
2019-04-17 21:20:22 -0400

    why generics does not work here?

--- React/react101/src/reacts_book/chap6/withLoader.tsx
+++ React/react101/src/reacts_book/chap6/withLoader.tsx
@@ -4,18 +4,28 @@ interface IProps {
   loading: boolean;
 }
 
-const withLoader = <P extends object>(
-  InputComponent: React.ComponentType<P>
-): React.FC<P & IProps> => (props: P & IProps) =>
-  props.loading ? (
-    <div className="loader-overlay">
-      <div className="loader-circle-wrap">
-        <div className="loader-circle"/>
-      </div>
-    </div>
-  ) : (
-    <InputComponent {...props} />
-  );
+// const withLoader = <P extends object>(
+//   InputComponent: React.ComponentType<P>
+// ): React.FC<P & IProps> => (props: P & IProps) =>
+//   props.loading ? (
+//     <div className="loader-overlay">
+//       <div className="loader-circle-wrap">
+//         <div className="loader-circle"/>
+//       </div>
+//     </div>
+//   ) : (
+//     <InputComponent {...props} />
+//   );
+
+
+const withLoader = <P>(InputComponent : React.ComponentType<P>): React.ComponentType<P> => {
+  return (props : P) => (
+    <InputComponent {...props}/>
+  )
+};
+
+
+
 export default withLoader;
 
 
