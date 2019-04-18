435571a6bd5b2bbc5f252990ab1e5cb3aa374390
songzhw <songzhw2012@gmail.com>
2019-04-18 13:54:53 -0400

    add the animation (right now the loading circle is not spining)

--- React/react101/src/index.tsx
+++ React/react101/src/index.tsx
@@ -4,7 +4,8 @@ import "./index.css";
 import { RouterWrap } from "./Rtutor/router/RoutesForFirst";
 import { RouterForNetwork } from "./Rtutor/network_zip/RouterForNetwork";
 import RouterChap6 from "./reacts_book/chap6/Routes";
+import RouterChap7 from "./reacts_book/chap7/Routes";
 import { FavoritePetScreen } from "./Rtutor/persist/useLocalStorage/FavoritePetScreen";
 
-ReactDOM.render(<FavoritePetScreen/>, document.getElementById("root"));
+ReactDOM.render(<RouterChap7/>, document.getElementById("root"));
 
