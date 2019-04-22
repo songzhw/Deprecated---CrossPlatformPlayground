76dd7ee6b37ea0b4d2a52c50a92f3c5617f5767d
songzhw <songzhw2012@gmail.com>
2019-04-22 08:40:45 -0400

    axios: header, timeout

--- React/react101/src/reacts_book/chap9/AxiosClass.tsx
+++ React/react101/src/reacts_book/chap9/AxiosClass.tsx
@@ -10,24 +10,39 @@ interface IPost {
 
 interface IState {
   posts: IPost[];
+  error: string;
 }
 
 export class AxiosClass extends React.Component<{}, IState> {
-  public state = { posts: [] };
+  public state = { posts: [], error: "" };
 
   public componentDidMount(): void {
-    axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts")
-      .then(resp => this.setState({ posts: resp.data }));
+    axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
+      headers: {
+        "Content-Type": "application/json"
+      },
+      timeout: 3000
+    })
+      .then(resp => this.setState({ posts: resp.data }))
+      .catch(err => {
+        const error = err.code === "ECONNABORTED" ? "A timeout occured" :
+          err.response.status === 404 ? "Resource not found" : "An unexpected error has occurred";
+        this.setState({ error });
+      });
   }
 
   public render(): React.ReactNode {
     return (
       <div className="App">
+
         <ul className="posts">
           {this.state.posts.map((post: IPost) => (
             <li key={post.id}><h6>{post.title} </h6></li>
           ))}
         </ul>
+
+        {this.state.error && <p className="error">{this.state.error}</p>}
+
       </div>
     );
   }
