d96d6e9c53b57e47942f0b7b5ebce79cd5dce541
songzhw <songzhw2012@gmail.com>
2019-04-22 08:58:18 -0400

    axios: cancel

--- React/react101/src/reacts_book/chap9/AxiosClass.tsx
+++ React/react101/src/reacts_book/chap9/AxiosClass.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import axios from "axios";
+import axios, { CancelTokenSource } from "axios";
 
 interface IPost {
   id?: number;
@@ -11,29 +11,41 @@ interface IPost {
 interface IState {
   posts: IPost[];
   error: string;
+  cancelTokenSource?: CancelTokenSource;
+  isLoading: boolean;
 }
 
 export class AxiosClass extends React.Component<{}, IState> {
-  public state = { posts: [], error: "" };
+  public state: IState = { posts: [], error: "", isLoading: true };
 
   public componentDidMount(): void {
+    const cancelTokenSource = axios.CancelToken.source();
+    this.setState({ cancelTokenSource });
+
+
     axios.get<IPost[]>("https://jsonplaceholder.typicode.com/posts", {
       headers: {
         "Content-Type": "application/json"
       },
-      timeout: 3000
+      timeout: 3000,
+      cancelToken: cancelTokenSource.token
     })
-      .then(resp => this.setState({ posts: resp.data }))
+      .then(resp => this.setState({ posts: resp.data, isLoading: false }))
       .catch(err => {
-        const error = err.code === "ECONNABORTED" ? "A timeout occured" :
-          err.response.status === 404 ? "Resource not found" : "An unexpected error has occurred";
-        this.setState({ error });
+        const error = axios.isCancel(err) ? "Request Cancelled" :
+          err.code === "ECONNABORTED" ? "A timeout occured" :
+            err.response.status === 404 ? "Resource not found" : "An unexpected error has occurred";
+        this.setState({ error, isLoading: false });
       });
   }
 
   public render(): React.ReactNode {
     return (
       <div className="App">
+
+        {this.state.isLoading && (
+          <button onClick={this.onCancelClick}>Cancel</button>
+        )}
 
         <ul className="posts">
           {this.state.posts.map((post: IPost) => (
@@ -46,4 +58,10 @@ export class AxiosClass extends React.Component<{}, IState> {
       </div>
     );
   }
+
+  public onCancelClick = () => {
+    if (this.state.cancelTokenSource) {
+      this.state.cancelTokenSource.cancel("User cancelled operation");
+    }
+  };
 }
