9b477cdcf4b43bd2e05044df3a2e3cc2a5f7832e
songzhw <songzhw2012@gmail.com>
2019-04-24 11:26:38 -0400

    test channel and saga.

--- ReactNative/RN5/js/redux/saga/channel/__tests__/actionChannelSaga.test.js.ts
+++ ReactNative/RN5/js/redux/saga/channel/__tests__/actionChannelSaga.test.js.ts
@@ -11,6 +11,10 @@ describe("Testing channelSaga", () => {
     const saga = actionChannelSaga();
     saga.next(); //开始执行. 执行到 yield actionChannel()完了为止
 
+    const mockChannel = createMockChannel();
+    const takenItemFromChannel = saga.next(mockChannel); //执行到yield take(channnel)为止
+    expect(takenItemFromChannel.value).toEqual(take(mockChannel));
+
 
   });
 });
