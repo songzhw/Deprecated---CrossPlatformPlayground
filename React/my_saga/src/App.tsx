a7405683a4f0dec56baad722356bbca8c8101f21
songzhw <songzhw2012@gmail.com>
2019-04-27 12:48:57 -0400

    add and config necessary libraries

--- React/my_saga/src/App.tsx
+++ React/my_saga/src/App.tsx
@@ -1,23 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const App: React.FC = () => {
+  function fetchId() {
+
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={fetchId}>fetch id</button>
       </header>
     </div>
   );
-}
+};
 
 export default App;
