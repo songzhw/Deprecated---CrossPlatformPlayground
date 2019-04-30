1189578f3eed7c47903c9ce601c0a8b1214b5492
songzhw <songzhw2012@gmail.com>
2019-04-30 12:52:57 -0400

    refactor

--- React/dis-reader/src/utils/download/HttpEngine.js
+++ React/dis-reader/src/utils/download/HttpEngine.js
@@ -5,12 +5,16 @@ export const http = (url) => {
     xhr.responseType = "arraybuffer";
     xhr.onload = () => {
       if (xhr.status && xhr.status < 400) {
-        console.log(`szw got resposne : ${xhr.status}`);
+        console.log(`szw got status : ${xhr.status}`);
+        console.log(`szw got resp = `, xhr.response);
         resolve(xhr.response);
       } else {
         console.log(`szw error on downloading : ${xhr.status}`);
         reject(new Error(`error ${xhr.status}`));
       }
+    };
+    xhr.onerror = (ev) => {
+      console.log(`szw onerror`, ev);
     };
     xhr.open("GET", url, true);
     xhr.send();
