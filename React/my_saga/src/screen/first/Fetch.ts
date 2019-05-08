349959b4ec182b3f62777a0175db27ba5d175f8f
songzhw <songzhw2012@gmail.com>
2019-05-08 15:37:19 -0400

    sagalet - process async

--- React/my_saga/src/screen/first/Fetch.ts
+++ React/my_saga/src/screen/first/Fetch.ts
@@ -1,3 +1,11 @@
+// 备注: 下面的两个fetchNumberForAdding, 不管返回Promise, 还是async (本质上也是返回Promise), sagalet都是能处理的. 我实践过.
+
 export const fetchNumberForAdding = () => {
   return Promise.resolve(4);
 };
+
+
+// export const fetchNumberForAdding = async (): Promise<number> => {
+//   const value = await 4;
+//   return value;
+// };
