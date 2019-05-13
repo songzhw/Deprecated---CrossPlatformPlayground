5ba08dba8d6f5efbc5caccd66f4731b15c3b130e
songzhw <songzhw2012@gmail.com>
2019-05-13 10:06:34 -0400

    type incompatibility error

---
+++ React/react101/quick.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+interface IQuickProps {
+  id?: string,
+  center?: [number, number],
+  radius?: number,
+  zoom?: number;
+  camera?: string;
+}
+
+export const QuickScreen = (props: IQuickProps) => {
+  return (
+    <div>
+      <p>Quick</p>
+    </div>
+  );
+};
+
+const initValue = {
+  id: "20sdadsdfkajsdklasda",
+  center: [0, 0],
+  radius: 100
+};
+export const QuicksUser = () => {
+  return (
+    <QuickScreen {...initValue} />
+  );
+};
