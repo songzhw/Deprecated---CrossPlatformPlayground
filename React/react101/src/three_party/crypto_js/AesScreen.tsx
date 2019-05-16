43488343c2aa1561325dacdaa8c92bd94d476147
songzhw <songzhw2012@gmail.com>
2019-05-16 15:21:56 -0400

    AES/CBC/PKCS7 encrypt

---
+++ React/react101/src/three_party/crypto_js/AesScreen.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import CryptoJS, { WordArray } from "crypto-js";
+import Base64 from "crypto-js/enc-base64";
+import Utf8 from "crypto-js/enc-utf8";
+import SHA256 from "crypto-js/sha256";
+import AES from "crypto-js/aes";
+import Pkcs7 from "crypto-js/pad-pkcs7";
+import Hex from "crypto-js/enc-hex";
+
+
+interface IProps {
+}
+
+export const AesScreen = (props: IProps) => {
+  const [result, setResult] = useState("...");
+
+  // AES-128 : key需要得是128位, 换成16进制, 就是128 = 16 * 8, 即得有16个string在这里
+  const key = Utf8.parse("0123456789abcdef");
+  const iv = Utf8.parse("abcdef9876543210");
+
+  const src = `<html><h1>I'm happy</h1></html>`;
+  let encrypted: WordArray;
+  let decrypted = "";
+
+  function onClickEcbNoPadding1() {
+
+  }
+
+  function onClickEcbNoPadding2() {
+
+  }
+
+  function onClickEcbPKCS71() {
+
+  }
+
+  function onClickEcbPKCS72() {
+
+  }
+
+  function onClickCbcPKCS71() {
+    const utfIn = Utf8.parse(src);
+    encrypted = AES.encrypt(utfIn, key, { iv, mode: CryptoJS.mode.CBC, padding: Pkcs7 });
+
+    // encrypted是WordArray类型, 不适合阅读, 所以转一下
+    // astring = Base64.stringify(encrypted);// 但不能直接用base64转, 会有问题 - TypeError: wordArray.clamp is not a function
+    setResult(encrypted.ciphertext.toString());
+  }
+
+  function onClickCbcPKCS72() {
+
+  }
+
+  return (
+    <div>
+      <p>{result}</p>
+      <button onClick={onClickEcbNoPadding1}>AES/ECB/NoPadding encrypt</button>
+      <button onClick={onClickEcbNoPadding2}>AES/ECB/NoPadding decrypt</button>
+      <p/>
+      <button onClick={onClickEcbPKCS71}>AES/ECB/PKCS7 encrypt</button>
+      <button onClick={onClickEcbPKCS72}>AES/ECB/PKCS7 decrypt</button>
+      <p/>
+      <button onClick={onClickCbcPKCS71}>AES/CBC/PKCS7 encrypt</button>
+      <button onClick={onClickCbcPKCS72}>AES/CBC/PKCS7 decrypt</button>
+    </div>
+  );
+};
--- React/react101/src/three_party/crypto_js/CryptoRoutes.tsx
+++ React/react101/src/three_party/crypto_js/CryptoRoutes.tsx
@@ -4,15 +4,14 @@ import "./Crypto.css";
 import { Base64Screen } from "./Base64Screen";
 import { CryptoScreen } from "./CryptoScreen";
 import { JwtScreen } from "./JwtScreen";
+import { AesScreen } from "./AesScreen";
 
 const Header = () => {
   return (
     <div className={"header"}>
       <NavLink to="/base64" className="headerLink" activeClassName="activeHeaderLink"> Base64 </NavLink>
       <NavLink to="/jwt" className="headerLink" activeClassName="activeHeaderLink"> jwt </NavLink>
-      <NavLink to="/sha256" className="headerLink" activeClassName="activeHeaderLink"> sha256 </NavLink>
-      <NavLink to="/AES_no" className="headerLink" activeClassName="activeHeaderLink"> AES_No </NavLink>
-      <NavLink to="/AES_pkcs7" className="headerLink" activeClassName="activeHeaderLink"> AES_PKCS7 </NavLink>
+      <NavLink to="/aes" className="headerLink" activeClassName="activeHeaderLink"> AES </NavLink>
       <NavLink to="/file" className="headerLink" activeClassName="activeHeaderLink"> read_file </NavLink>
       <NavLink to="/decryptf" className="headerLink" activeClassName="activeHeaderLink"> decrypt_file </NavLink>
     </div>
@@ -28,6 +27,7 @@ export const CryptoRouter = () => {
         <Route path="/all" component={CryptoScreen}/>
         <Route path="/base64" component={Base64Screen}/>
         <Route path="/jwt" component={JwtScreen}/>
+        <Route path="/aes" component={AesScreen}/>
       </Switch>
     </BrowserRouter>
   );
