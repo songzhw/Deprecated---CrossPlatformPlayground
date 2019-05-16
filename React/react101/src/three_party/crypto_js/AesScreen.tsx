9632e626dd3a196772eda1bc32355064d18f9db4
songzhw <songzhw2012@gmail.com>
2019-05-16 15:54:32 -0400

    still failed

--- React/react101/src/three_party/crypto_js/AesScreen.tsx
+++ React/react101/src/three_party/crypto_js/AesScreen.tsx
@@ -39,25 +39,43 @@ export const AesScreen = (props: IProps) => {
   }
 
   function onClickCbcPKCS71() {
-    const utfIn = Utf8.parse(src);
-    const rawEncrypted = AES.encrypt(utfIn, key, { iv, mode: CryptoJS.mode.CBC, padding: Pkcs7 });
+    const rawEncrypted = AES.encrypt(src, key, { iv, mode: CryptoJS.mode.CBC, padding: Pkcs7 });
 
     // encrypted是WordArray类型, 不适合阅读与传递给他人, 所以转一下
     // astring = Base64.stringify(encrypted);// 但不能直接用base64转, 会有问题 - TypeError: wordArray.clamp is not a function
-    encrypted = rawEncrypted.ciphertext.toString().toUpperCase();
-    encrypted = Base64.parse(encrypted);
+    encrypted = rawEncrypted.ciphertext.toString();
     setResult(encrypted);
   }
 
   function onClickCbcPKCS72() {
-    // 对wordArray.ciphertext.toString(), 要还原为utf格式, 得先用hex, 再用base64
-    const hexIn = Hex.parse(encrypted);
-    const bytes = Base64.stringify(hexIn);
-    const rawDecrpted = AES.decrypt(bytes, key, { iv, mode: CryptoJS.mode.CBC, padding: Pkcs7 });
-    decrypted = Utf8.stringify(rawDecrpted);
+    // // 对wordArray.ciphertext.toString(), 要还原为utf格式, 得先用hex, 再用base64
+    const encryptedHexStr = CryptoJS.enc.Hex.parse(encrypted);
+// 将密文转为Base64的字符串
+// 只有Base64类型的字符串密文才能对其进行解密
+    const encryptedBase64Str = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+    const rawDecrpted = AES.decrypt(encryptedBase64Str, key, { iv, mode: CryptoJS.mode.CBC, padding: Pkcs7 });
+    decrypted = rawDecrpted.toString(Utf8);
     console.log(`de = `, decrypted);
     setResult(decrypted);
   }
+
+
+  /**
+   * 生成密钥字节数组, 原始密钥字符串不足128位, 补填0.
+   * @return Buffer
+   */
+  const fillKey = (key2: string) => {
+    const keySize = key2.length;
+    const filledKey = Buffer.alloc(keySize / 8);
+    const keys = Buffer.from(key);
+    let index = 0;
+    while (index < filledKey.length) {
+      filledKey[index] = keys[index];
+      index += 1;
+    }
+
+    return filledKey;
+  };
 
   return (
     <div>
