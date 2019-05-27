2632ae8baab17680d9659fff09ee4e2c81b0fb44
songzhw <songzhw2012@gmail.com>
2019-05-27 14:52:41 -0400

    add another util function

--- React/react101/src/biz/adrm/CryptoUtils.ts
+++ React/react101/src/biz/adrm/CryptoUtils.ts
@@ -55,6 +55,16 @@ export function byteArrayToWordArray(u8Array: number[]) {
   };
 }
 
+export function base64ToArrayBuffer(base64: string) {
+  const binary_string = window.atob(base64);
+  const len = binary_string.length;
+  const bytes = new Uint8Array(len);
+  for (let i = 0; i < len; i++) {
+    bytes[i] = binary_string.charCodeAt(i);
+  }
+  return bytes;
+}
+
 export function arrayBufferToBase64(arrayBuffer: ArrayBuffer) {
   let base64 = "";
   const encodings = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
