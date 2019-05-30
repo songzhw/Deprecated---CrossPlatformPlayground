2748611998a703d50d8aa38b88012bdedeb3bab1
songzhw <songzhw2012@gmail.com>
2019-05-30 15:28:05 -0400

    implement the "startActivityForResult"

--- ReactNative/rn101/src/biz/navigators/pass_value/PassA1Screen.tsx
+++ ReactNative/rn101/src/biz/navigators/pass_value/PassA1Screen.tsx
@@ -1,16 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, Button } from "react-native";
 import { NavigationScreenProps } from "react-navigation";
 
 export const PassA1Screen = (props: NavigationScreenProps) => {
+  const [valueFromB, setValueFromB] = useState({});
+
+  function onActivityResult(result: any) {
+    console.log(`szw onActivityResult = `, result);
+    setValueFromB(result);
+  }
+
   function onGo2B() {
-    console.log(`szw click go2B`)
-    props.navigation.navigate("PassB1Screen", { value: "what" });
+    console.log(`szw click go2B`);
+    props.navigation.navigate("PassB1Screen",
+      { value: "what", callback: onActivityResult });
   }
 
   return (
     <View>
-      <Text>A1</Text>
+      <Text>A1 {JSON.stringify(valueFromB)}</Text>
       <Button title="go to B1" onPress={onGo2B}/>
     </View>
   );
--- ReactNative/rn101/src/biz/navigators/pass_value/PassB1Screen.tsx
+++ ReactNative/rn101/src/biz/navigators/pass_value/PassB1Screen.tsx
@@ -1,13 +1,21 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import { NavigationScreenProps } from "react-navigation";
 
 export const PassB1Screen = (props: NavigationScreenProps) => {
-  const value = props.navigation.getParam("value", "empty/default");
+  const value = props.navigation.getParam("value", "empty/default value");
+  const callback = props.navigation.getParam("callback", () => console.log(`empty/default callback`));
+
+  function onPickedImage() {
+    const image = { src: "00.jpg", width: 100, height: 30 };
+    callback(image);
+    props.navigation.goBack();
+  }
 
   return (
     <View>
       <Text>B1 -- {value}</Text>
+      <Button title="pick images" onPress={onPickedImage}/>
     </View>
   );
 };
