16a14f15bb57dcfa799108ee95b18ccc7355b903
songzhw <songzhw2012@gmail.com>
2019-05-31 15:39:36 -0400

    但类似的代码对于TextInput来说, ios平台有效, android平台无效

--- ReactNative/rn101/index.js
+++ ReactNative/rn101/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppRegistry, Text, TextInput } from "react-native";
+import { AppRegistry, Text, TextInput, PixelRatio, Platform } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
 
@@ -10,10 +10,23 @@ Text.render = function(...args) {
   return React.cloneElement(originalText, { allowFontScaling: false });
 };
 
-const TextInputRender = TextInput.render;
-TextInput.render = function(...args) {
-  const originalTextInput = TextInputRender.apply(this, args);
-  return React.cloneElement(originalTextInput, { allowFontScaling: false });
-};
+// 全局定制字体2: 下面代码设置<TextInput>, 但是没成功
+// const TextInputRender = TextInput.render;
+// TextInput.render = function(...args) {
+//   const originalTextInput = TextInputRender.apply(this, args);
+//   const { style } = originalTextInput.props;
+//   const currentFontSize = style.fontSize;
+//   const newFontSize = setResponsiveFontForTextInput(currentFontSize);
+//   console.log(`szw , curr = ${currentFontSize}, new = ${newFontSize}`);
+//   return React.cloneElement(
+//     originalTextInput,
+//     { allowFontScaling: Platform.OS === "android", style: { ...style, fontSize: newFontSize } }
+//   ); //为iOS平台, 就设为false;
+// };
+
+// @parm fontSize: number
+function setResponsiveFontForTextInput(fontSize) {
+  return Platform.OS === "android" ? fontSize / PixelRatio.getFontScale() : fontSize;
+}
 
 AppRegistry.registerComponent(appName, () => App);
