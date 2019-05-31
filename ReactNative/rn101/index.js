19de3e1aadef25a24c5a091ed4a1517ee8d5ac01
songzhw <songzhw2012@gmail.com>
2019-05-31 15:26:18 -0400

    但类似的代码对于TextInput来说, ios平台有效, android平台无效

--- ReactNative/rn101/index.js
+++ ReactNative/rn101/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { AppRegistry, Text } from "react-native";
+import { AppRegistry, Text, TextInput } from "react-native";
 import App from "./App";
 import { name as appName } from "./app.json";
 
@@ -7,7 +7,13 @@ import { name as appName } from "./app.json";
 const TextRender = Text.render;
 Text.render = function(...args) {
   const originalText = TextRender.apply(this, args);
-  return React.cloneElement(originalText, {allowFontScaling: false})
+  return React.cloneElement(originalText, { allowFontScaling: false });
+};
+
+const TextInputRender = TextInput.render;
+TextInput.render = function(...args) {
+  const originalTextInput = TextInputRender.apply(this, args);
+  return React.cloneElement(originalTextInput, { allowFontScaling: false });
 };
 
 AppRegistry.registerComponent(appName, () => App);
