d2ece9adf45a72059be22cb4dc61b2103c2d5101
songzhw <songzhw2012@gmail.com>
2019-05-31 15:18:58 -0400

    how to set font in RN projects independent of phone settings

--- ReactNative/rn101/index.js
+++ ReactNative/rn101/index.js
@@ -1,5 +1,14 @@
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import React from "react";
+import { AppRegistry, Text } from "react-native";
+import App from "./App";
+import { name as appName } from "./app.json";
+
+// 全局定制字体1: 不让字体大小跟随手机字体设置的变化
+const TextRender = Text.render;
+Text.render = function(...args) {
+  const originalText = TextRender.apply(this, args);
+  const {style} = originalText.props;
+  return React.cloneElement(originalText, {allowFontScaling: false})
+};
 
 AppRegistry.registerComponent(appName, () => App);
