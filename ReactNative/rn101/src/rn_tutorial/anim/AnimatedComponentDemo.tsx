a674b1f970a984f1ab62d2be0bacfb0a1489b1ee
songzhw <songzhw2012@gmail.com>
2019-06-04 13:36:50 -0400

    fix the previous error

--- ReactNative/rn101/src/rn_tutorial/anim/AnimatedComponentDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/anim/AnimatedComponentDemo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component, useState } from "react";
 import { Animated, Button, View, Text } from "react-native";
 
 interface IProps {
@@ -14,22 +14,24 @@ export const AnimatedComponentDemo = () => {
   function startAnim() {
     const anim1 = Animated.timing(width, { toValue: 200, duration: 2500 });
     const anim2 = Animated.timing(height, { toValue: 300, duration: 4000 });
-    Animated.parallel(([anim1, anim2]))
-      .start();
+    Animated.parallel(([anim1, anim2])).start();
   }
 
   return (
     <View>
       <Button title="start" onPress={startAnim}/>
-      <Text>Hello</Text>
+      <AnimatedChild width={width} height={height}/>
     </View>
   );
 };
 
-const Child = (props: IProps) => {
-  const { width, height } = props;
-  return (
-    <View style={{ width, height, backgroundColor: "powderblue" }}/>
-  );
-};
+
+class Child extends Component<IProps> {
+  render() {
+    const { width, height } = this.props;
+    return (
+      <View style={{ width, height, backgroundColor: "powderblue" }}/>
+    );
+  }
+}
 
