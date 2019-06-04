c585460c99abeb1c6ef8c7400409d0f672f917e3
songzhw <songzhw2012@gmail.com>
2019-06-04 12:54:57 -0400

    the reason it does not work, it is the extra root <View> which has no styleing at all

--- ReactNative/rn101/src/rn_tutorial/layout/FlexLayoutDemo1.tsx
+++ ReactNative/rn101/src/rn_tutorial/layout/FlexLayoutDemo1.tsx
@@ -7,12 +7,11 @@ interface IProps {
 export class FlexLayoutDemo1 extends Component {
   render() {
     return (
-      <View>
-        <View style={{ width: 60, flex: 1 }}>
-          <View style={{ flex: 2, backgroundColor: "pink" }}/>
-          <View style={{ flex: 1, backgroundColor: "blue" }}/>
-        </View>
+      <View style={{ width: 60, flex: 1 }}>
+        <View style={{ flex: 2, backgroundColor: "pink" }}/>
+        <View style={{ flex: 1, backgroundColor: "blue" }}/>
       </View>
     );
   }
 }
+
