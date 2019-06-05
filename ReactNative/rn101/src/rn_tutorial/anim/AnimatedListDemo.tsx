7ff8b63ad417db09788778abebcdf751c3f487b3
songzhw <songzhw2012@gmail.com>
2019-06-05 17:13:12 -0400

    get an animationsuccessfully

--- ReactNative/rn101/src/rn_tutorial/anim/AnimatedListDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/anim/AnimatedListDemo.tsx
@@ -1,16 +1,27 @@
-import React, { Component, useState } from "react";
-import { FlatList, View, Text, Animated, Button } from "react-native";
+import React, { Component } from "react";
+import { FlatList, View, Text, Animated, Button, StyleSheet } from "react-native";
 
 
-//TODO ERROR! (运行在android上没动画; 运行在ios上直接crash)
+// 因为contentOffset只在ios上有效, 所以运行在android上没动画; 运行在ios上有动画
 
 export class AnimatedListDemo extends Component {
   state = {
-    offset: new Animated.Value(0)
+    offset: 0
+  };
+  private interval: number | null = null;
+
+  startAnim() {
+    // Animated.timing(this.state.offset, { toValue: 1000, duration: 7000 }).start();
+    this.interval = setInterval(() => {
+      this.setState({ offset: this.state.offset + 50 });
+    }, 500);
+    // this.setState({ offset: 200 });
   }
 
-   startAnim() {
-    Animated.timing(this.state.offset, { toValue: 1000, duration: 7000 }).start();
+  componentWillUnmount(): void {
+    if (this.interval !== null) {
+      clearInterval(this.interval);
+    }
   }
 
   color = (item: any) => {
@@ -21,18 +32,28 @@ export class AnimatedListDemo extends Component {
     }
   };
 
-  render(){
+  render() {
     const AnimatedFlatList = Animated.createAnimatedComponent(FlatList);
     // @ts-ignore
     return (
       <View>
-        <Button title="start" onPress={this.startAnim}/>
-        <AnimatedFlatList
+        <Button title="start" onPress={()=> this.startAnim()}/>
+        <FlatList
           data={[{ key: "a" }, { key: "b" }, { key: "a" }, { key: "b" }, { key: "a" }, { key: "b" }, { key: "a" }, { key: "b" }]}
-          renderItem={({item, index}) => <Text style={{ width: 300, height: 200, fontSize: 35, backgroundColor: this.color(item) }}>{index}. {item.key}</Text>}
+          renderItem={({ item, index }) => <Text
+            style={[styles.text, { backgroundColor: this.color(item) }]}>{index}. {item.key}</Text>}
           keyExtractor={(item, index) => index + ""}
-          contentOffset={{y: this.state.offset}}
+          contentOffset={{ x: 0, y: this.state.offset }}
         />
       </View>
-  );}
+    );
+  }
 };
+
+const styles = StyleSheet.create({
+  text: {
+    width: 300,
+    height: 200,
+    fontSize: 35
+  }
+});
