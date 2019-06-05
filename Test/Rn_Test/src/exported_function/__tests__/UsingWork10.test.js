c5f4f72d3b5e296b2284e39775615f56918f1239
songzhw <songzhw2012@gmail.com>
2019-06-05 15:26:21 -0400

    same error: "TypeError: tree.find() is not a function"

--- Test/Rn_Test/src/exported_function/__tests__/UsingWork10.test.js
+++ Test/Rn_Test/src/exported_function/__tests__/UsingWork10.test.js
@@ -2,6 +2,7 @@ import React from "react";
 import { work10 } from "../Worker10";
 import { shallow } from "react-native/jest/renderer";
 import { UsingWork10Screen } from "../UsingWork10Screen";
+import { Button } from "react-native";
 
 jest.mock("../Worker10");
 
@@ -9,6 +10,6 @@ test("test exported function", () => {
   work10.mockReturnThis();
 
   const tree = shallow(<UsingWork10Screen/>);
-  tree.find("Button").simulate("press");
+  tree.find(Button).simulate("press");
 
 });
