38a574db3afb887ebaf6fa6bd46faf2ac7f25b30
songzhw <songzhw2012@gmail.com>
2019-06-07 18:50:07 -0400

    beashell - button

--- ReactNative/Temp/App.tsx
+++ ReactNative/Temp/App.tsx
@@ -16,8 +16,20 @@ export default class App extends Component<Props> {
   render() {
     return (
       <SafeAreaView style={styles.container}>
+        {/*黄底白字*/}
         <Button type="primary" size="md" style={styles.item}>Primary 11</Button>
+        {/*黄底黑字*/}
         <Button type="primary" size="md" textColorInverse style={styles.item}>Primary 22</Button>
+
+        <Button type="primary" size="md" style={styles.item}>
+          <View>
+            <Text>Custom</Text>
+            <Text>Components</Text>
+          </View>
+        </Button>
+
+        <Button type="info" size={"sm"} disabled style={styles.item}> Info </Button>
+
       </SafeAreaView>
     );
   }
