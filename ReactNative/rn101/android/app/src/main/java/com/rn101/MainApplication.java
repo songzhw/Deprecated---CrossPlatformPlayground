59eac8a7537ad67012451f423f99d5e5b0512d52
songzhw <songzhw2012@gmail.com>
2019-06-11 14:07:12 -0400

    r-n-zip-archieve : unzip successfully

--- ReactNative/rn101/android/app/src/main/java/com/rn101/MainApplication.java
+++ ReactNative/rn101/android/app/src/main/java/com/rn101/MainApplication.java
@@ -3,6 +3,7 @@ package com.rn101;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.rnziparchive.RNZipArchivePackage;
 import com.RNFetchBlob.RNFetchBlobPackage;
 import com.futurepress.staticserver.FPStaticServerPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new RNZipArchivePackage(),
             new RNFetchBlobPackage(),
             new FPStaticServerPackage(),
--- ReactNative/rn101/src/lib3rd/ZipArchiveDemo.tsx
+++ ReactNative/rn101/src/lib3rd/ZipArchiveDemo.tsx
@@ -1,12 +1,27 @@
 import React from "react";
-import { View } from "react-native";
+import { Button, View } from "react-native";
+import { zip, unzip, unzipAssets, subscribe } from "react-native-zip-archive";
+import { MainBundlePath, DocumentDirectoryPath } from "react-native-fs";
 
 export const ZipArchiveDemo = () => {
+  // DocumentDirectoryPath : '/data/user/0/com.rn101/files'
 
+  function unzipIt() {
+    const sourcePath = `${DocumentDirectoryPath}/RNFetchBlobTmp_k4cm76lvnxbd5298gs0njc.zip`;
+    const targetPath = DocumentDirectoryPath+"/alice";
+
+    unzip(sourcePath, targetPath)
+      .then((path) => {
+        console.log(`unzip completed at ${path}`);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 
   return (
     <View>
-
+      <Button title={"unzip"} onPress={unzipIt}/>
     </View>
-  )
+  );
 };
