f5cf1aca7fd45625cd91ee324842a38f619b3dc6
songzhw <songzhw2012@gmail.com>
2019-06-11 13:28:48 -0400

    download a image - failed create a text file

--- ReactNative/rn101/src/lib3rd/FetchBlobDemo.tsx
+++ ReactNative/rn101/src/lib3rd/FetchBlobDemo.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-import { Button, View, Text } from "react-native";
+import { Button, View, Text, Image, Platform } from "react-native";
 import RNFetchBlob from "react-native-fetch-blob";
 
 export const FetchBlobDemo = () => {
   const [json, setJson] = useState("");
+  const [uri, setUri] = useState("");
 
   function downloadJson() {
     const url = "https://www.mocky.io/v2/5caa5b8e3000001607904577";
@@ -32,11 +33,58 @@ export const FetchBlobDemo = () => {
       .catch((error: any) => console.log(`szw error2 = `, error));
   }
 
+  function downloadImage() {
+    const url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+    RNFetchBlob
+      .config({
+        trusty: true,
+        fileCache: true,
+        appendExt: "png"
+      })
+      .fetch("GET", url)
+      .then(resp => {
+        const path = resp.path();
+        console.log(`szw saved image = ${path}`);
+        const url = Platform.OS === "android" ? "file://" + path : "" + path;
+        setUri(url);
+      })
+      .catch((error: any) => console.log(`szw error3 = `, error));
+  }
+
+  const preparePath = async (targetPath: any) => {
+    const exists = await RNFetchBlob.fs.exists(targetPath);
+    if (!exists) {
+      await RNFetchBlob.fs.mkdir(targetPath);
+    }
+  };
+
+  function createTxtFile() {
+    const targetPath = RNFetchBlob.fs.dirs.DocumentDir+"/work.txt";
+    RNFetchBlob.fs
+      .exists(targetPath)
+      .then( async isExist => {
+        if (isExist) {
+          console.log(`szw file exist`)
+        } else {
+          await RNFetchBlob.fs.createFile(targetPath, "test from file", "utf8")
+        }
+      })
+  }
+
+  function readTxtFile(){
+
+  }
+
+
   return (
     <View>
       <Text>{json}</Text>
+      <Image source={{ uri: uri }}/>
       <Button title={"download json"} onPress={downloadJson}/>
       <Button title={"download zip"} onPress={downloadZip}/>
+      <Button title={"download Image"} onPress={downloadImage}/>
+      <Button title={"create a file locally"} onPress={createTxtFile}/>
+      <Button title={"read from a local file"} onPress={readTxtFile}/>
     </View>
   );
 };
