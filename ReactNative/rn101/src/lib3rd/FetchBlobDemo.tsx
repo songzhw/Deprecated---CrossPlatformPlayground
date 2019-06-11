8401c08b2da24e83d64cbaf8e11a4254b5ce0128
songzhw <songzhw2012@gmail.com>
2019-06-11 10:43:10 -0400

    a demo of downloading using r-n-fetch-blob

--- ReactNative/rn101/src/lib3rd/FetchBlobDemo.tsx
+++ ReactNative/rn101/src/lib3rd/FetchBlobDemo.tsx
@@ -1,12 +1,24 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { Button, View, Text } from "react-native";
+import RNFetchBlob from "react-native-fetch-blob";
 
 export const FetchBlobDemo = () => {
+  const [json, setJson] = useState("");
 
+  function downloadJson() {
+    const url = "http://www.mocky.io/v2/5caa5b8e3000001607904577";
+    RNFetchBlob.fetch("GET", url)
+      .then(resp => {
+        setJson(resp.text());
+        console.log("szw json = ", resp.json());
+      })
+      .catch((error: any) => console.log(`szw error = `, error));
+  }
 
   return (
     <View>
-
+      <Text>{json}</Text>
+      <Button title={"download json"} onPress={downloadJson}/>
     </View>
-  )
+  );
 };
