7a4f8379a6d0e3dc5b86b22a59e730985a2962b7
songzhw <songzhw2012@gmail.com>
2019-06-11 10:58:15 -0400

    download a file

--- ReactNative/rn101/src/lib3rd/FetchBlobDemo.tsx
+++ ReactNative/rn101/src/lib3rd/FetchBlobDemo.tsx
@@ -7,20 +7,36 @@ export const FetchBlobDemo = () => {
 
   function downloadJson() {
     const url = "https://www.mocky.io/v2/5caa5b8e3000001607904577";
-    RNFetchBlob.config({trusty: true})
+    RNFetchBlob.config({ trusty: true })
       .fetch("GET", url)
       .then(resp => {
         const text = resp.text();
         setJson(text as string);
         console.log("szw json = ", resp.json());
       })
-      .catch((error: any) => console.log(`szw error = `, error));
+      .catch((error: any) => console.log(`szw error1 = `, error));
+  }
+
+  function downloadZip() {
+    const url = "https://gerhardsletten.github.io/react-reader/files/alice.epub";
+    RNFetchBlob.config({ fileCache: true })
+      .fetch("GET", url)
+      .progress((received, total) => {
+        const progress = received / total;
+        setJson("" + progress);
+      })
+      .then(resp => {
+        console.log(`szw zip is saved to ${resp.path()}`);
+        setJson(resp.path());
+      })
+      .catch((error: any) => console.log(`szw error2 = `, error));
   }
 
   return (
     <View>
       <Text>{json}</Text>
       <Button title={"download json"} onPress={downloadJson}/>
+      <Button title={"download zip"} onPress={downloadZip}/>
     </View>
   );
 };
