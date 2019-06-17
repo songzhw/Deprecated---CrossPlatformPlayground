3be574b377c7043560dc9073c715f3e5e8b834c3
songzhw <songzhw2012@gmail.com>
2019-06-16 21:13:37 -0400

    config nav bar

--- ReactNative/sha3/src/core/Routes.tsx
+++ ReactNative/sha3/src/core/Routes.tsx
@@ -1,25 +1,23 @@
 import React from "react";
-import { Platform, Text } from "react-native";
+import { Text, View } from "react-native";
 import { createAppContainer, createStackNavigator } from "react-navigation";
 import { NavigationBar } from "beeshell";
 import { GameScreen } from "../ui/GameScreen";
 import { HomeScreen } from "../ui/HomeScreen";
 
-const TITLE_OFFSET = Platform.OS === 'ios' ? 70 : 56;
 const RouteList = createStackNavigator({
   HomeScreen, GameScreen
 }, {
   defaultNavigationOptions: {
-    headerStyle: {
-      backgroundColor: "#1b5e20"
-    },
-    headerTintColor: "#fff",
-    headerTitleStyle:{
-      alignSelf:'center',
-      textAlign: 'center',
-      flex:1,
-    },
-    headerTitle: "Sha3"
+    headerTitle: (
+      <NavigationBar
+        title="Sha3"
+        titleStyle={{ color: "white" }}
+        backLabelText=""
+        backLabelIcon={(<View/>)}
+        style={{ backgroundColor: "#1b5e20" }}
+      />
+    )
   }
 });
 
