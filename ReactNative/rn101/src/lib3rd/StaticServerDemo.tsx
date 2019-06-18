b3fa89f7f05829c3faa091740f2cb0501d4eccef
songzhw <songzhw2012@gmail.com>
2019-06-18 16:11:25 -0400

    static server

--- ReactNative/rn101/src/lib3rd/StaticServerDemo.tsx
+++ ReactNative/rn101/src/lib3rd/StaticServerDemo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Button, View, Text, Image, Platform } from "react-native";
 import RNFetchBlob from "react-native-fetch-blob";
 import { unzip } from "react-native-zip-archive";
@@ -9,7 +9,19 @@ import StaticServer from "react-native-static-server";
 export const StaticServerDemo = () => {
   const [isLoaded, setIsLoaded] = useState(false);
   const [uri, setUri] = useState("");
+  var server: StaticServer;
 
+  // works on iOS; fails on Android because of the "ERR_ACCESS_DENIED" error
+  function readLocal() {
+    server = new StaticServer(8090, RNFetchBlob.fs.dirs.DocumentDir, { localOnly: true });
+    server.start()
+      .then(() => {
+        setIsLoaded(true);
+        console.log(`szw started a server 2`);
+      });
+  }
+
+  // fails
   function downloadAndUnzip() {
     const url = "https://gerhardsletten.github.io/react-reader/files/alice.epub";
     RNFetchBlob
@@ -29,7 +41,7 @@ export const StaticServerDemo = () => {
       .then(zipPath => {
         // this.startServer(this.port, RNFetchBlob.fs.dirs.DocumentDir + "/");
         const serverRootDir = RNFetchBlob.fs.dirs.DocumentDir + "/alice";
-        const server = new StaticServer(8089, serverRootDir, { localOnly: true });
+        server = new StaticServer(8090, serverRootDir, { localOnly: true });
         server.start()
           .then(() => setUri(serverRootDir))
           .catch((error: any) => console.log("szw start server error: ", error));
@@ -38,11 +50,22 @@ export const StaticServerDemo = () => {
       .catch(error => console.log(`szw error6 = `, error));
   }
 
+  useEffect(() => {
+    return () => {
+      if (server) {
+        server.kill();
+      }
+    };
+  }, []);
+
 
   return (
     <View style={{ flex: 1 }}>
+      <Button title={"scan local"} onPress={readLocal}/>
       <Button title={"download"} onPress={downloadAndUnzip}/>
-      <WebView source={{ uri }} style={{ width: 300, height: 500, backgroundColor: "powderblue" }}/>
+      <WebView source={{ uri: "http://localhost:8090/" }}
+               style={{ flex: 1, backgroundColor: "powderblue" }}
+               allowFileAccess={true}/>
     </View>
   );
 };
