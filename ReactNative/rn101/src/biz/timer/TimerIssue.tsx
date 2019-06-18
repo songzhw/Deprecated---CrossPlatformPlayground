24a7f662b5a79f5dda9d56744d08bf7be3693ddb
songzhw <songzhw2012@gmail.com>
2019-06-18 16:36:49 -0400

    make it a class component, the interval is now normall now. but the exit func is not getting called

--- ReactNative/rn101/src/biz/timer/TimerIssue.tsx
+++ ReactNative/rn101/src/biz/timer/TimerIssue.tsx
@@ -1,33 +1,57 @@
 import React, { useEffect, useState } from "react";
 import { View, Text, Button } from "react-native";
 
-export const TimerIssue = () => {
-  const [time, setTime] = useState(-1);
-  let timerHandler: number | null = null;
+// export const TimerIssue = () => {
+//   const [time, setTime] = useState(-1);
+//   let timerHandler: number | null = null;
+//
+//   function work() {
+//     console.log(`szw interval : ${time}`);
+//     const result = time + 1;
+//     setTime(result);
+//   }
+//
+//   function start() {
+//     timerHandler = setInterval(work, 1000);
+//   }
+//
+//   useEffect(() => {
+//     console.log(`szw useEffect start`);
+//     return () => {
+//       console.log(`szw useEffect cancel`);
+//       if (timerHandler) {
+//         clearInterval(timerHandler);
+//       }
+//     };
+//   }, []);
+//
+export class TimerIssue extends React.Component {
+  timerHandler: number | null = null;
+  state = {
+    time: -1
+  };
 
-  function start() {
-    timerHandler = setInterval(() => {
-      console.log(`szw interval : ${time}`);
-      const result = time + 1;
-      setTime(result);
-    }, 1000);
+  componentWillUnmount() {
+    if (this.timerHandler) {
+      clearInterval(this.timerHandler);
+    }
   }
 
-  useEffect(() => {
-    console.log(`szw useEffect start`);
-    return () => {
-      console.log(`szw useEffect cancel`);
-      if (timerHandler) {
-        clearInterval(timerHandler);
-      }
-    };
-  }, []);
 
+  start = () => {
+    this.timerHandler = setInterval(() => {
+      console.log(`szw interval : ${this.state.time}`);
+      const result = this.state.time + 1;
+      this.setState({ time: result });
+    }, 1000);
+  };
 
-  return (
-    <View>
-      <Text> {time} </Text>
-      <Button title="start" onPress={start}/>
-    </View>
-  );
+  render() {
+    return (
+      <View>
+        <Text> {this.state.time} </Text>
+        <Button title="start" onPress={this.start}/>
+      </View>
+    );
+  };
 };
