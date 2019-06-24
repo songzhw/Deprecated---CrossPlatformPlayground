dfd57c1f7930f38f63460765e6d10aa2b9661a08
songzhw <songzhw2012@gmail.com>
2019-06-24 15:51:43 -0400

    change case

--- ReactNative/rn101/scripts/fulfill.ts
+++ ReactNative/rn101/scripts/fulfill.ts
@@ -1,15 +1,16 @@
-// @ts-ignore
+const changeCase = require("change-case");
+
 console.log(`szw fulfill `);
 
 const cwd = process.cwd(); //current working dict
 console.log(`cwd = ${cwd}`);
 //=> /Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101
 
-console.log("process.argv[2...] = ", process.argv.slice(2));
-const argv = require("minimist")(process.argv.slice(2));
-console.dir(argv);
+const argv = process.argv.slice(2);
+console.log("process.argv[2...] = ", argv);
+const arg = argv[0];
+console.log(`kebab = ${changeCase.kebab("testString")}`);//=> test-string
+console.log(`const = ${changeCase.constant(arg)}`); //=> SERVICE_DRM
+console.log(`camel = ${changeCase.camel(arg)}`); //=> serviceDrm (转化后第一个一定是小写)
+console.log(`pascal= ${changeCase.pascal(arg)}`); //=> ServiceDrm (转化后第一个一定是大写)
 
-console.log(`length = `, argv._.length); //=> 0
-console.log(argv._[0]); //=> undefined
-console.log(argv.action); //=> hello
-console.log(argv.i);  //=> 30
