52b479f6b003372011db12855e0514b0dc875e33
songzhw <songzhw2012@gmail.com>
2019-06-24 14:52:22 -0400

    minimist

--- ReactNative/rn101/scripts/fulfill.ts
+++ ReactNative/rn101/scripts/fulfill.ts
@@ -5,23 +5,11 @@ const cwd = process.cwd(); //current working dict
 console.log(`cwd = ${cwd}`);
 //=> /Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101
 
-console.log(process.argv);
-/*
-1). 结果比较复杂
-比如说:  $ yarn run fulfill hello 20 true
-[ '/usr/local/bin/node',
-  '/Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101/scripts/fulfill.ts',
-  'hello',
-  '20',
-  'true' ]
+console.log("process.argv[2...] = ", process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
+console.dir(argv);
 
-2). 不能带k-v对 (带k-v对使用这script的人更友好, 因为不用去记住顺序与每个参数值代表name还是descrption)
-又比如说: $ yarn run fulfill -action hello -id 20
-[ '/usr/local/bin/node',
-  '/Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101/scripts/fulfill.ts',
-  '-action',
-  'hello',
-  '-id',
-  '20' ]
- */
-
+console.log(`length = `, argv._.length); //=> 0
+console.log(argv._[0]); //=> undefined
+console.log(argv.action); //=> hello
+console.log(argv.i);  //=> 30
