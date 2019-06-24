16b31d9b73c4c1d59dfa0e9a4dcf9b991ed465e1
songzhw <songzhw2012@gmail.com>
2019-06-24 14:21:32 -0400

    current working dir

--- ReactNative/rn101/scripts/fulfill.ts
+++ ReactNative/rn101/scripts/fulfill.ts
@@ -1,2 +1,6 @@
 // @ts-ignore
 console.log(`szw fulfill `);
+
+const cwd = process.cwd(); //current working dict
+console.log(`cwd = ${cwd}`);
+//=> /Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101
