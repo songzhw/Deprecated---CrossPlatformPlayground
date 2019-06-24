0b6affcab04d8510127db10917a2ad9f7d719d5e
songzhw <songzhw2012@gmail.com>
2019-06-24 14:29:37 -0400

    node.js 自带的读取参数

--- ReactNative/rn101/scripts/fulfill.ts
+++ ReactNative/rn101/scripts/fulfill.ts
@@ -4,3 +4,24 @@ console.log(`szw fulfill `);
 const cwd = process.cwd(); //current working dict
 console.log(`cwd = ${cwd}`);
 //=> /Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101
+
+console.log(process.argv);
+/*
+1). 结果比较复杂
+比如说:  $ yarn run fulfill hello 20 true
+[ '/usr/local/bin/node',
+  '/Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101/scripts/fulfill.ts',
+  'hello',
+  '20',
+  'true' ]
+
+2). 不能带k-v对 (带k-v对使用这script的人更友好, 因为不用去记住顺序与每个参数值代表name还是descrption)
+又比如说: $ yarn run fulfill -action hello -id 20
+[ '/usr/local/bin/node',
+  '/Users/zsong/code/mine/CrossPlatformPlayground/ReactNative/rn101/scripts/fulfill.ts',
+  '-action',
+  'hello',
+  '-id',
+  '20' ]
+ */
+
