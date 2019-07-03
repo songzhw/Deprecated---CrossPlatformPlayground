49f0e77dd3226fa82fa66dadd05766ea0457fb1f
songzhw <songzhw2012@gmail.com>
2019-07-03 16:41:48 -0400

    nav options

--- ReactNative/rn101/src/biz/splash/SplashScreen.tsx
+++ ReactNative/rn101/src/biz/splash/SplashScreen.tsx
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import { SafeAreaView, Text, View } from "react-native";
 import { NavigationScreenProps } from "react-navigation";
+import { HomeScreen } from "../../HomeScreen";
 
 type IProps = NavigationScreenProps
 
 export class SplashScreen extends Component<IProps> {
+  // static navigationOptions = () => {
+  //   return {
+  //     title: "Splash",
+  //     headerLeft: <View/>
+  //   };
+  // };
 
   componentDidMount() {
     setTimeout(
       () => this.props.navigation.navigate("app"),
-      10);
+      10000);
   }
 
   render() {
@@ -20,3 +27,4 @@ export class SplashScreen extends Component<IProps> {
     );
   }
 }
+
