53ef1d940fa654e76b003f01c7a9f6f39bb78dd7
songzhw <songzhw2012@gmail.com>
2019-07-03 16:17:57 -0400

    splash screen

--- ReactNative/rn101/src/biz/splash/SplashScreen.tsx
+++ ReactNative/rn101/src/biz/splash/SplashScreen.tsx
@@ -1,10 +1,15 @@
 import React, { Component } from "react";
 import { SafeAreaView, Text, View } from "react-native";
+import { NavigationScreenProps } from "react-navigation";
 
-export class SplashScreen extends Component {
+type IProps = NavigationScreenProps
+
+export class SplashScreen extends Component<IProps> {
 
   componentDidMount() {
-
+    setTimeout(
+      () => this.props.navigation.navigate("app"),
+      2000);
   }
 
   render() {
