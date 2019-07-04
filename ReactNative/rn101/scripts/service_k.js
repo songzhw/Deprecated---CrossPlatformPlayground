6d4e092c61200ddfb2297b20eba5ee4fa283ccbd
songzhw <songzhw2012@gmail.com>
2019-07-04 16:16:05 -0400

    fs.readFileSync

--- ReactNative/rn101/scripts/service_k.js
+++ ReactNative/rn101/scripts/service_k.js
@@ -4,6 +4,7 @@ const changeCase = require("change-case");
 const replacer = require("maxstache");
 var argv = require("yargs").argv; // 格式是: {_:[a,b,c], action: one, id: 20}
 const mkdirp = require("mkdirp");
+const fs = require("fs");
 
 const actionFromCmd = argv.action;
 const tmp = actionFromCmd.split(",");
@@ -12,12 +13,34 @@ const actionArray = tmp.map((item) => item.trim());
 const serviceFromCmd = argv.service;
 const serviceConstant = changeCase.constant(serviceFromCmd);
 
-let actionsResult = "";
+let actionTypesResult = "";
 actionArray.forEach((action) => {
   const actionConstant = changeCase.constant(action); //=> "LOAD_API"
   const actionSuffix = changeCase.path(action).toUpperCase();
-  actionsResult += `${actionConstant} = "@@_${serviceConstant}/${actionSuffix}",\n`;
+  actionTypesResult += `${actionConstant} = "@@_${serviceConstant}/${actionSuffix}",\n`;
 });
-actionsResult = actionsResult.substring(0, actionsResult.length - 2); //去除最后一个逗号与'\n'
+actionTypesResult = actionTypesResult.substring(0, actionTypesResult.length - 2); //去除最后一个逗号与'\n'
 
-console.log(actionsResult);
+let actionCreatorResult = "";
+let actionCreatorTemplate = `export const {{CAMEL}} = () => ({
+  type: ActionTypes.{{CONSTANTS}}
+ });`;
+actionArray.forEach((action) => {
+  const placeHolder = {
+    CAMEL: changeCase.camel(action),
+    CONSTANTS: changeCase.constant(action)
+  };
+  actionCreatorResult += replacer(actionCreatorTemplate, placeHolder);
+  actionCreatorResult += "\n";
+});
+
+
+const actionPlaceHolder = {
+  actions: actionTypesResult,
+  actionCreators: actionCreatorResult
+};
+
+mkdirp("./scripts/result", () => {
+  const fileContent = fs.readFileSync(`${__dirname}/templatesK/actions/index.ts`, {encoding: 'utf8'});
+  console.log(fileContent)
+});
