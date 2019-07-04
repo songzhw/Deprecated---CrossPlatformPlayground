bf8b5d0bde6f6412e42d764e5cd44eb32d4a6ae9
songzhw <songzhw2012@gmail.com>
2019-07-04 15:55:14 -0400

    change to javascript

--- ReactNative/rn101/scripts/service_k.js
+++ ReactNative/rn101/scripts/service_k.js
@@ -7,17 +7,17 @@ const mkdirp = require("mkdirp");
 
 const actionFromCmd = argv.action;
 const tmp = actionFromCmd.split(",");
-const actionArray = tmp.map((item: string) => item.trim());
+const actionArray = tmp.map((item) => item.trim());
 
 const serviceFromCmd = argv.service;
 const serviceConstant = changeCase.constant(serviceFromCmd);
 
 let actionsResult = "";
-actionArray.forEach((action: string) => {
+actionArray.forEach((action) => {
   const actionConstant = changeCase.constant(action); //=> "LOAD_API"
   const actionSuffix = changeCase.path(action).toUpperCase();
-  actionsResult += `${actionConstant} = "@@_${serviceConstant}/${actionSuffix}"\n,`;
+  actionsResult += `${actionConstant} = "@@_${serviceConstant}/${actionSuffix}",\n`;
 });
-actionsResult = actionsResult.substring(0, actionsResult.length - 1); //去除最后一个逗号
+actionsResult = actionsResult.substring(0, actionsResult.length - 2); //去除最后一个逗号与'\n'
 
 console.log(actionsResult);
