0f71419cbc9f44892d32c1a8ebab90dd9519b122
songzhw <songzhw2012@gmail.com>
2019-07-06 15:48:33 -0400

    add ART: error - cannot add a child that doesn't have a YogaNode to a parent without a measure function

--- ReactNative/rn101/src/rn_tutorial/art/ArtDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/art/ArtDemo.tsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { View, ViewProps, Text, StyleSheet, ART } from "react-native";
+import { View, ViewProps, Text, StyleSheet } from "react-native";
+// @ts-ignore
+import { Path, Shape, Surface } from "@react-native-community/art";
 
-interface IProps extends ViewProps  {}
+interface IProps extends ViewProps {
+}
 
 export const ArtDemo = (props: IProps) => {
-
-
+  const path = new Path();
+  path.moveTo(100, 100);
+  path.lineTo(300, 100);
 
   return (
     <View>
-
+        <Shape d={path} stroke="#000000" strokeWidth={4}/>
     </View>
-  )
-}
+  );
+};
 
 
 const styles = StyleSheet.create({
