fd4519b87723566f2b285e13151ff6e09f2fcadc
songzhw <songzhw2012@gmail.com>
2019-07-06 17:46:26 -0400

    circle

--- ReactNative/rn101/src/rn_tutorial/art/ArtDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/art/ArtDemo.tsx
@@ -15,11 +15,24 @@ export const ArtDemo = (props: IProps) => {
   path2.moveTo(50, 50);
   path2.lineTo(300, 50);
 
+  const circle = new Path();
+  circle.moveTo(50, 1)
+    .arc(0, 99, 25)
+    .arc(0, -99, 25)
+    .close();
+
+  const circle2 = new Path();
+  circle.moveTo(150, 1)
+    .arc(100, 199, 25)
+    .arc(100, 1, 25)
+    .close();
+
   return (
     <View>
       <Surface width={360} height={300}>
         <Shape d={path} stroke="#000000" strokeWidth={4}/>
         <Shape d={path2} stroke="#000000" strokeWidth={2} strokeDash={[8, 10]}/>
+        <Shape d={circle} stroke="#132456" strokeWidth={4} fill="#af8d2d"/>
       </Surface>
     </View>
   );
