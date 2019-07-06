66b505a4cfabd5133723f46d871edc7158fabed5
songzhw <songzhw2012@gmail.com>
2019-07-06 17:48:58 -0400

    empty text

--- ReactNative/rn101/src/rn_tutorial/art/ArtDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/art/ArtDemo.tsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
 // @ts-ignore
-import { Path, Shape, Surface } from "@react-native-community/art";
+import { Path, Shape, Surface, Text as ArtText } from "@react-native-community/art";
 
 interface IProps extends ViewProps {
 }
 
 export const ArtDemo = (props: IProps) => {
   const path = new Path();
-  path.moveTo(50, 20);
-  path.lineTo(300, 20);
+  path.moveTo(50, 420);
+  path.lineTo(300, 420);
 
   const path2 = new Path();
-  path2.moveTo(50, 50);
-  path2.lineTo(300, 50);
+  path2.moveTo(50, 450);
+  path2.lineTo(300, 450);
 
   const circle = new Path();
   circle.moveTo(50, 1)
@@ -29,10 +29,11 @@ export const ArtDemo = (props: IProps) => {
 
   return (
     <View>
-      <Surface width={360} height={300}>
+      <Surface width={360} height={600}>
         <Shape d={path} stroke="#000000" strokeWidth={4}/>
         <Shape d={path2} stroke="#000000" strokeWidth={2} strokeDash={[8, 10]}/>
         <Shape d={circle} stroke="#132456" strokeWidth={4} fill="#af8d2d"/>
+        <ArtText stroke="#ff0000" font="bold 35px Heiti SC" >React Native</ArtText>
       </Surface>
     </View>
   );
