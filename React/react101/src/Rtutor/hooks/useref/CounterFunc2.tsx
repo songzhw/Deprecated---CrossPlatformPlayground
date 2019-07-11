506b9d56b727d3d48ff5656a471850ff0b68e4a0
songzhw <songzhw2012@gmail.com>
2019-07-11 16:04:38 -0400

    function component + useRef

--- React/react101/src/Rtutor/hooks/useref/CounterFunc2.tsx
+++ React/react101/src/Rtutor/hooks/useref/CounterFunc2.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
 
+// 我们通过 useRef(initVal) 来返回一个可变的 ref 对象，其 current 属性被初始化为传递的参数 （initVal）。
 export function CounterFunc2() {
   const [count, setCount] = useState(0);
   const latestCount = useRef(count);
