6a6d9f35f824d3fcec73f37ff3b743ad9637fb45
songzhw <songzhw2012@gmail.com>
2019-07-13 13:49:49 -0400

    fix compilation error

--- React/react101/src/Rtutor/hooks/useimerative/MyInput.tsx
+++ React/react101/src/Rtutor/hooks/useimerative/MyInput.tsx
@@ -2,7 +2,7 @@ import React, { forwardRef, useImperativeHandle, useRef } from "react";
 
 // TODO 真是暂时找不到ref对应哪个属性了, 只好先用any来了
 const MyInput = ({}, ref: any) => {
-  const inRef = useRef();
+  const inRef = useRef(null);
   useImperativeHandle(ref, () => ({
     focus: () => {
       // @ts-ignore
