0b34192ed1dfef4975afaa7c13c8c318c2b4f08d
songzhw <songzhw2012@gmail.com>
2019-07-13 12:08:25 -0400

    without useRef: the <p> always display "***"

--- React/react101/src/index.tsx
+++ React/react101/src/index.tsx
@@ -24,6 +24,8 @@ import { CounterClass } from "./Rtutor/hooks/useeffect/CounterClass";
 import { CounterClass2 } from "./Rtutor/hooks/useeffect/CounterClass2";
 import { CounterFunc } from "./Rtutor/hooks/useeffect/CounterFunc";
 import { CounterFunc2 } from "./Rtutor/hooks/useref/CounterFunc2";
+import { Useref2 } from "./Rtutor/hooks/useref/useref2";
+import { WithoutRef } from "./Rtutor/hooks/useref/WithoutUseRef";
 
-ReactDOM.render(<CounterFunc2/>, document.getElementById("root"));
+ReactDOM.render(<WithoutRef/>, document.getElementById("root"));
 
