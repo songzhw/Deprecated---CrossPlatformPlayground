ab996c3dbca43a585be8d06e3cd3477b53fe01b4
songzhw <songzhw2012@gmail.com>
2019-07-18 23:28:45 -0400

    TypeScript lambda

--- React/react101/src/Rtutor/hooks/MediaDemo.tsx
+++ React/react101/src/Rtutor/hooks/MediaDemo.tsx
@@ -13,12 +13,25 @@ export const MediaDemo = (props: IProps) => {
     console.log(`szw 3 = `, list, ev);
   });
 
+  function foo(id: number, listener: (age: number, name: string) => string) {
+    listener(id, `${id}name`);
+  }
+
+  function click() {
+    // foo(23, (age: number, name: string) => {  //=> okay
+    foo(23, (age, name) => { //=> okay
+      console.log(`click() `, age, name);
+      return "";
+    });
+  }
+
   return (
     <div>
       <Media query="(max-width:600px)">
         {matches =>
           matches ? (<h1>Small</h1>) : (<h1>Large</h1>)}
       </Media>
+      <button onClick={click}>Click Me</button>
     </div>
   );
 };
