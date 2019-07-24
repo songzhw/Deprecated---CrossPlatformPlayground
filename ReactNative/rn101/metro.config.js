69347418e8bad61440d6ff12219946c6e61f7378
songzhw <songzhw2012@gmail.com>
2019-07-24 00:35:41 -0400

    fix the compilation error

--- ReactNative/rn101/metro.config.js
+++ ReactNative/rn101/metro.config.js
@@ -1,10 +1,17 @@
+const blacklist = require('metro-config/src/defaults/blacklist');
+
 module.exports = {
   transformer: {
     getTransformOptions: async () => ({
       transform: {
         experimentalImportSupport: false,
-        inlineRequires: false,
-      },
-    }),
+        inlineRequires: false
+      }
+    })
   },
+  resolver: {
+    blacklistRE: blacklist([/template.*\/.*/])
+  }
 };
+// 1. add the blacklist, to make sure scripts/templateK/***'s templates files is not need to be compiled - or I will get an compilation error
+// 2.
