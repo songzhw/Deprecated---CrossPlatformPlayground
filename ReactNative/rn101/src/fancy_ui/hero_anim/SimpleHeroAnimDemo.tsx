b313db35fd76e28788e397b022aab8d54ca8b377
songzhw <songzhw2012@gmail.com>
2019-07-25 16:53:09 -0400

    hero anim (does not work yet)

--- ReactNative/rn101/src/fancy_ui/hero_anim/SimpleHeroAnimDemo.tsx
+++ ReactNative/rn101/src/fancy_ui/hero_anim/SimpleHeroAnimDemo.tsx
@@ -1,18 +1,44 @@
-import React from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, ViewProps, Text, StyleSheet, Image, TouchableWithoutFeedback } from "react-native";
 
-interface IProps extends ViewProps  {}
+const picture = require("../../../res/images/icon_red_heart.png");
+
+interface IProps extends ViewProps {
+}
 
 export const SimpleHeroAnimDemo = (props: IProps) => {
+  const [isDetail, setIsDetail] = useState(false);
+  const imageDetail = isDetail ? picture : null;
+
+  function openDetail() {
+    setIsDetail(true);
+  }
 
   return (
     <View style={styles.container}>
+      <TouchableWithoutFeedback onPress={openDetail}>
+        <Image style={styles.iv1} source={picture}/>
+      </TouchableWithoutFeedback>
+
+      <View style={StyleSheet.absoluteFill}>
+        <Image style={styles.iv2} source={imageDetail}/>
+      </View>
 
     </View>
-  )
-}
+  );
+};
 
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {},
+  iv1: {
+    width: 134,
+    height: 125,
+    margin: 20
+  },
+  iv2: {
+    width: 320,
+    height: 300,
+    margin: 20
+  }
 });
