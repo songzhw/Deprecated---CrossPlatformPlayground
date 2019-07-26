d2202fd28c18262007977b862f0a9342cad32154
songzhw <songzhw2012@gmail.com>
2019-07-25 21:55:02 -0400

    now we have no animation at all

--- ReactNative/rn101/src/fancy_ui/hero_anim/SimpleHeroAnimDemo.tsx
+++ ReactNative/rn101/src/fancy_ui/hero_anim/SimpleHeroAnimDemo.tsx
@@ -1,64 +1,70 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import { View, Text, StyleSheet, Image, TouchableWithoutFeedback, TouchableHighlight, Animated } from "react-native";
 
 const picture = require("../../../res/images/icon_red_heart.png");
 
-export const SimpleHeroAnimDemo = () => {
-  const [isDetail, setIsDetail] = useState(false);
-  const imageDetail = isDetail ? picture : null;
-  const modalClickable = isDetail ? "auto" : "none";
-  const detailText = isDetail ? "DetailText" : null;
-  const bg = isDetail ? "#ccc" : "#0000";
-  const [width, setWidth] = useState(new Animated.Value(134));
-  const [height, setHeight] = useState(new Animated.Value(125));
-  const [left, setLeft] = useState(new Animated.Value(0));
-  const [top, setTop] = useState(new Animated.Value(0));
-  let iv1: Image | null;
-
-  useEffect(() => {
-    if (isDetail) {
-      return;
-    }
+export class SimpleHeroAnimDemo extends Component {
+  state = {
+    isDetail: false,
+    width: new Animated.Value(134),
+    height: new Animated.Value(134),
+    left: new Animated.Value(0),
+    top: new Animated.Value(0)
+  };
+  imageDetail = this.state.isDetail ? picture : null;
+  modalClickable: "auto" | "none" = this.state.isDetail ? "auto" : "none";
+  detailText = this.state.isDetail ? "DetailText" : null;
+  bg = this.state.isDetail ? "#ccc" : "#0000";
+  iv1: Image | null = null;
 
 
-    Animated.parallel([
-      Animated.timing(width, { toValue: 320, duration: 3000 }),
-      Animated.timing(height, { toValue: 300, duration: 3000 }),
-      Animated.timing(left, { toValue: 0, duration: 3000 }),
-      Animated.timing(top, { toValue: 0, duration: 3000 })
-    ]).start();
-
-  }, [isDetail]);
-
-  function openDetail() {
-    iv1!.measure((x, y, width, height, pageX, pageY) => {
+  openDetail = () => {
+    this.iv1!.measure((x, y, width, height, pageX, pageY) => {
       // 见下面, 可见x, y无用 ;  pageX, pageY是(x,y)位置 ;  width, heigth就是宽高了
       console.log("szw ", x, y, width, height, pageX, pageY); //=>  0, 0, 134, 125, 100, 256
-      setLeft(new Animated.Value(pageX));
-      setTop(new Animated.Value(pageY));
-      setIsDetail(true);
+
+      this.setState({ isDetail: true }, () => {
+        this.state.left.setValue(pageX);
+        this.state.top.setValue(pageY);
+        Animated.parallel([
+          Animated.timing(this.state.width, { toValue: 320, duration: 3000 }),
+          Animated.timing(this.state.height, { toValue: 300, duration: 3000 }),
+          Animated.timing(this.state.left, { toValue: 0, duration: 3000 }),
+          Animated.timing(this.state.top, { toValue: 0, duration: 3000 })
+        ]).start();
+      });
     });
-  }
+  };
 
-  function closeDetail() {
-    setIsDetail(false);
-    width.setValue(134);
-    height.setValue(125);
-  }
+  closeDetail = () => {
+    this.setState({ isDetail: false });
+    this.state.width.setValue(134);
+    this.state.height.setValue(125);
+    this.state.left.setValue(100);
+    // TODO this is temporary
+    this.state.top.setValue(256);
+  };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.empty}/>
-      <Image ref={view => iv1 = view} style={styles.iv1} source={picture}/>
-      <Text onPress={openDetail}> List Screen </Text>
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.empty}/>
+        <Image ref={view => this.iv1 = view} style={styles.iv1} source={picture}/>
+        <Text onPress={this.openDetail}> List Screen </Text>
 
-      <View style={[StyleSheet.absoluteFill, { backgroundColor: bg }]} pointerEvents={modalClickable}>
-        <Animated.Image style={[styles.iv2, { left, top, width, height }]} source={imageDetail}/>
-        <Text onPress={closeDetail}> {detailText} </Text>
+        <View style={[StyleSheet.absoluteFill, { backgroundColor: this.bg }]} pointerEvents={this.modalClickable}>
+          <Animated.Image source={this.imageDetail} style={[styles.iv2, {
+            left: this.state.left,
+            top: this.state.top,
+            width: this.state.width,
+            height: this.state.height
+          }]}/>
+          <Text onPress={this.closeDetail}> {this.detailText} </Text>
+        </View>
+
       </View>
-
-    </View>
-  );
+    );
+  }
 };
 
 
