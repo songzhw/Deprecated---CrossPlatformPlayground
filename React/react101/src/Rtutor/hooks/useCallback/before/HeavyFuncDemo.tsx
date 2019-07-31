278b67e07dc558a7aaf4a47d4bc8914cd48305af
songzhw <songzhw2012@gmail.com>
2019-07-30 21:30:54 -0400

    TypeError: work is not a function

--- React/react101/src/Rtutor/hooks/useCallback/before/HeavyFuncDemo.tsx
+++ React/react101/src/Rtutor/hooks/useCallback/before/HeavyFuncDemo.tsx
@@ -1,24 +1,30 @@
-import React, { memo, useMemo } from "react";
+import React, { ChangeEvent, memo, useMemo, useState } from "react";
 import { delay } from "../../../../utils/utils";
 
 
 export const HeavyFuncDemo = () => {
+  const [count, setCount] = useState(0);
 
-  function runFunc(event: React.MouseEvent<HTMLButtonElement>) {
-    const id = event.currentTarget.dataset.id!;
-    console.log(`click button : `, new Date());
-    work(id);
+  function runFunc(event: React.FormEvent<HTMLInputElement>) {
+    const value = parseInt(event.currentTarget.value, 10);
+    setCount(value);
   }
 
-  const work = useMemo(async (id: string) => {
-    await delay(3000);
-    console.log(`work: ${id} : `, new Date());
-  }, [id]);
+  const calculator = useMemo(() => {
+    return work();
+  }, [count]);
+
+  const work = () => {
+    delay(3000);
+    return count * 20;
+  };
 
   return (
     <div>
-      <p>heavy func</p>
-      <button data-id={"100"} onClick={runFunc}> run</button>
+      <p>heavy func: {calculator} </p>
+      {/* tslint:disable-next-line:jsx-no-lambda */}
+      <input type={"text"} onChange={e => runFunc(e)}
+             placeholder={"set count"}/>
     </div>
   );
 };
