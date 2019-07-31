4a779aec223ce2be66edebebe3e8847b2ac877d0
songzhw <songzhw2012@gmail.com>
2019-07-31 15:45:53 -0400

    add props, and show the active color

--- React/react101/src/views/switch/Switch.tsx
+++ React/react101/src/views/switch/Switch.tsx
@@ -8,14 +8,15 @@ interface IViewProps {
 const defaultProps = {
   isOn: false as boolean
 };
-
 type IProps = IViewProps & Partial<Readonly<typeof defaultProps>>;
 
 export const Switch = (props: IProps) => {
+  const bgColor = { background: props.isOn ? "#06d6a0" : "grey" };
+
   return (
     <>
       <input className={"switch-checkbox"} id={"switch-new"} type={"checkbox"}/>
-      <label className={"switch-label"} htmlFor={"switch-new"}>
+      <label className={"switch-label"} htmlFor={"switch-new"} style={bgColor}>
         <span className={"switch-button"}/>
       </label>
     </>
--- React/react101/src/views/switch/SwitchDemo.tsx
+++ React/react101/src/views/switch/SwitchDemo.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Switch } from "./Switch";
 
 export const SwitchDemo = () => {
+  const [isOn, setIsOn] = useState(false);
+
+  function toggle() {
+    setIsOn(!isOn);
+  }
 
   return (
-    <div style={{display: "flex", margin: "20px"}}>
+    <div style={{ display: "flex", margin: "20px" }}>
       {/* tslint:disable-next-line:jsx-no-lambda */}
-      <Switch onToggle={()=>{}}/>
+      <Switch onToggle={toggle} isOn={isOn}/>
     </div>
   );
 };
