d272eecce722776e60e5920c895c326a900f110a
songzhw <songzhw2012@gmail.com>
2019-08-02 17:14:41 -0400

    add libraries

---
+++ React/others/threep/src/todo/TodoScreen.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface IProps {
+}
+
+export const TodoScreen = (props: IProps) => {
+
+  function add() {
+
+  }
+
+  return (
+    <div>
+      <button onClick={add}>add item</button>
+    </div>
+  )
+}
+
