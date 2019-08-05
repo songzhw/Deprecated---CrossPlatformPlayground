969fbc0e31b806d905a3d9ca0492645dabce09f8
songzhw <songzhw2012@gmail.com>
2019-08-05 17:18:02 -0400

    make the component better for performance

--- React/react101/src/Rtutor/hooks/memorize/PlayerConsole_Class.tsx
+++ React/react101/src/Rtutor/hooks/memorize/PlayerConsole_Class.tsx
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import React, { useEffect } from "react";
 import iconPause from "../../../res/images/icons/icon_pause.png";
 import iconPlay from "../../../res/images/icons/icon_play.png";
@@ -10,12 +11,17 @@ interface IProps {
 
 export class PlayerConsole_Class extends React.Component<IProps> {
 
+  shouldComponentUpdate(nextProps: Readonly<IProps>, nextState: Readonly<{}>, nextContext: any): boolean {
+    console.log(`name: `, this.props.name === nextProps.name);
+    console.log(`isPause: `, this.props.isPause === nextProps.isPause);
+    console.log(`toggle: `, this.props.toggle === nextProps.toggle);
+    return this.props.name !== nextProps.name || this.props.isPause !== nextProps.isPause ||
+      this.props.toggle !== nextProps.toggle
+  }
+
   public render() {
     const imageName = this.props.isPause ? iconPause : iconPlay;
-    // useEffect(() => console.log(`name change`), [props.name]);
-    // useEffect(() => console.log(`isPause change`), [props.isPause]);
-    // useEffect(() => console.log(`toggle change`), [props.toggle]);
-    console.log(`Console render()`);
+    console.log(chalk.red(`PlayConsoler re-render!!!`));
 
     return (
       <div>
