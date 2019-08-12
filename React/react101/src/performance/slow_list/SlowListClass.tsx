997ff9322c8f4b71b465c54ee1a3a17313ecee48
songzhw <songzhw2012@gmail.com>
2019-08-12 10:42:28 -0400

    finally it gets better, the time turns to 170 ms from 570ms

--- React/react101/src/performance/slow_list/SlowListClass.tsx
+++ React/react101/src/performance/slow_list/SlowListClass.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 import { ISlowListState } from "./SlowListReducer";
@@ -7,10 +7,9 @@ type IProps = ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatch
 
 class _SlowListClass extends React.Component<IProps> {
 
-  markItem = (ev: React.MouseEvent<HTMLParagraphElement>) => {
-    const id = ev.currentTarget.dataset.id!;
+  markItem = (id: number) => {
     console.log(`szw click : id = `, id);
-    this.props.mark(parseInt(id, 10));
+    this.props.mark(id);
   };
 
   render() {
@@ -19,7 +18,7 @@ class _SlowListClass extends React.Component<IProps> {
       <div>
         {
           data.map(item =>
-            <SlowListItem key={item.id} id={item.id} isMarked={item.isMarked} data-id={""+item.id} onClick={this.markItem}/>
+            <SlowListItem key={item.id} id={item.id} isMarked={item.isMarked} onClick={() => this.markItem(item.id)}/>
           )
         }
       </div>
@@ -46,7 +45,7 @@ export default connect(mapStateToProps, mapDispatchToProps)(_SlowListClass);
 interface IItemProps {
   id: number;
   isMarked: boolean;
-  onClick: (ev: React.MouseEvent<HTMLParagraphElement>) => void;
+  onClick: () => void;
 }
 
 const _SlowListItem = (props: IItemProps) => {
@@ -56,4 +55,6 @@ const _SlowListItem = (props: IItemProps) => {
     <p style={style} onClick={props.onClick}>{props.id} - {props.isMarked ? "✔" : "x"}</p>
   );
 };
-const SlowListItem = React.memo(_SlowListItem);
+const SlowListItem = React.memo(_SlowListItem, (prevProps, nextProps) => {
+  return prevProps.isMarked === nextProps.isMarked
+});
