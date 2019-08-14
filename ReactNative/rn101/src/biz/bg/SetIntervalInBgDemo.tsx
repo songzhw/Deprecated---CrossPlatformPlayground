baac512fa78eb82596485637cbd85b422bce4371
songzhw <songzhw2012@gmail.com>
2019-08-14 14:25:19 -0400

    setInterval does have the background issue

--- ReactNative/rn101/src/biz/bg/SetIntervalInBgDemo.tsx
+++ ReactNative/rn101/src/biz/bg/SetIntervalInBgDemo.tsx
@@ -1,33 +1,43 @@
-import React, { useState } from "react";
+import React from "react";
 import { View, ViewProps, Text, StyleSheet, Button } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
 
+/*
+当在后台时, interval不再工作了! 因为发现日志不再显示了, 只有再次回到前台, clock才接着往下走
+ */
 interface IInnerProps extends ViewProps {
 }
 
 type IProps = IInnerProps & ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
-let intervalHandler = 0;
+class _SetIntervalInBgDemo extends React.Component<IProps> {
+  state = {
+    time: 60
+  };
+  intervalHandler = 0;
 
-const _SetIntervalInBgDemo = (props: IProps) => {
-  const [time, setTime] = useState(60);
-
-  function countDown() {
-    if (intervalHandler !== 0) {
-      clearInterval(intervalHandler);
+  countDown = () => {
+    if (this.intervalHandler !== 0) {
+      clearInterval(this.intervalHandler);
     } else {
-      setInterval(() => setTime(time - 1), 1000);
+      // @ts-ignore
+      this.intervalHandler = setInterval(() => {
+        console.log(`szw time =`, this.state.time);
+        this.setState({ time: this.state.time - 1 });
+      }, 1000);
     }
-  }
+  };
 
-  return (
-    <View>
-      <Text style={styles.clock}> 15:{time}</Text>
-      <Button title={"interval( clock, 1000ms)"} onPress={countDown}/>
-    </View>
-  );
-};
+  render() {
+    return (
+      <View>
+        <Text style={styles.clock}> 15:{this.state.time}</Text>
+        <Button title={"interval( clock, 1000ms)"} onPress={this.countDown}/>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
