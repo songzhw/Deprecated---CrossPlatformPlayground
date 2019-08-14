836b69341329459294c041204a1a00dcd1e2ddf6
songzhw <songzhw2012@gmail.com>
2019-08-14 16:29:56 -0400

    but the delay in the saga.takeEvery() will not execute

--- ReactNative/rn101/src/core/redux/BgJobSaga.ts
+++ ReactNative/rn101/src/core/redux/BgJobSaga.ts
@@ -1,4 +1,4 @@
-import { takeEvery, put, select, call, debounce } from "redux-saga/effects";
+import { takeEvery, put, select, call, debounce, delay } from "redux-saga/effects";
 import { BG_SAGA_INCREASE } from "./BgJobReducer";
 import { AnyAction } from "redux";
 
@@ -10,9 +10,11 @@ const onSagaIncrease = function* (action: AnyAction) {
 // @ts-ignore
 const onSagaIncreaseTaken = function* (action: AnyAction) {
   console.log(`szw saga takeEvery`, action);
+  yield delay(3000);
+  console.log(`szw saga takeEvery after delay(3s)`);
 };
 
 export const BgJobSaga = function* () {
   yield debounce(5000, BG_SAGA_INCREASE, onSagaIncrease);
-  yield takeEvery( BG_SAGA_INCREASE, onSagaIncreaseTaken);
+  yield takeEvery(BG_SAGA_INCREASE, onSagaIncreaseTaken);
 };
