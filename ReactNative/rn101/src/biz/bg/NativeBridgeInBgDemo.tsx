b0b3e769233f78dc4edcdb3b1be6a648ca925b0a
songzhw <songzhw2012@gmail.com>
2019-08-17 16:45:15 -0400

    even in bg, the js part got the message and printed the log, and re-render the screen

--- ReactNative/rn101/src/biz/bg/NativeBridgeInBgDemo.tsx
+++ ReactNative/rn101/src/biz/bg/NativeBridgeInBgDemo.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, ViewProps, Text, StyleSheet, Button, NativeModules } from "react-native";
 import { connect } from "react-redux";
 import { Dispatch } from "redux";
@@ -11,14 +11,20 @@ interface IInnerProps extends ViewProps {
 type IProps = IInnerProps & ReturnType<typeof mapStateToProps> & ReturnType<typeof mapDispatchToProps>;
 
 const _NativeBridgeInBgDemo = (props: IProps) => {
+  const [text, setText] = useState("(init)");
+
   function callNative() {
-    NativeDelayWorker.start(5000, (msg: string) => {
+    NativeDelayWorker.start(3000, (msg: string) => {
       console.log(`szw received msg from Native:`, msg);
+      const value = new Date().toTimeString();
+      setText(value);
     });
   }
 
+  console.log(`szw Screen render()`);
   return (
     <View>
+      <Text style={styles.text}> {text} </Text>
       <Button title={"native work"} onPress={callNative}/>
     </View>
   );
@@ -27,6 +33,9 @@ const _NativeBridgeInBgDemo = (props: IProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  text: {
+    fontSize: 30
   }
 });
 
