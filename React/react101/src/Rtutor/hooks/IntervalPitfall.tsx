aff6993427a6319f5e4d7e8daf402d6abdf5079a
songzhw <songzhw2012@gmail.com>
2019-08-19 14:26:10 -0400

    that's because the setState mechanism

--- React/react101/src/Rtutor/hooks/IntervalPitfall.tsx
+++ React/react101/src/Rtutor/hooks/IntervalPitfall.tsx
@@ -1,9 +1,14 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 let intervalHandler = 0;
 
 export const IntervalPitfall = () => {
   const [time, setTime] = useState(60);
+
+  useEffect(() => {
+    console.log(`szw useEffect(): time =`, time);
+    console.log(`==============`);
+  }, [time]);
 
   const countDown = () => {
     if (intervalHandler !== 0) {
@@ -12,12 +17,13 @@ export const IntervalPitfall = () => {
     } else {
       // @ts-ignore
       intervalHandler = setInterval(() => {
-        console.log(`szw time =`, time, "; interval = ", intervalHandler); //=> always : "time = 60"
+        console.log(`szw within interval() time =`, time, "; interval = ", intervalHandler); //=> always : "time = 60"
         setTime(sec => sec - 1);
       }, 1000);
     }
   };
 
+  console.log(`szw screen re-render, time = `, time);
   return (
     <div>
       <p> 15: {time}</p>
@@ -25,4 +31,15 @@ export const IntervalPitfall = () => {
     </div>
   );
 };
+
+/*
+szw within interval() time = 60 ; interval =  11
+szw screen re-render, time =  59
+szw useEffect(): time = 59
+==============
+
+szw within interval() time = 60 ; interval =  11
+szw screen re-render, time =  58
+szw useEffect(): time = 58
+ */
 
