4d55a51870887d874a372be6f81545522773d205
songzhw <songzhw2012@gmail.com>
2019-08-19 14:06:03 -0400

    this does not solve the problem (I thought it might be closure issue)

--- React/react101/src/Rtutor/hooks/IntervalPitfall.tsx
+++ React/react101/src/Rtutor/hooks/IntervalPitfall.tsx
@@ -2,24 +2,22 @@ import React, { useState } from "react";
 
 let intervalHandler = 0;
 
-interface IProps {
-}
-
-export const IntervalPitfall = (props: IProps) => {
+export const IntervalPitfall = () => {
   const [time, setTime] = useState(60);
 
   const countDown = () => {
     if (intervalHandler !== 0) {
       clearInterval(intervalHandler);
+      intervalHandler = 0;
     } else {
       // @ts-ignore
-      intervalHandler = setInterval(() => {
-        console.log(`szw time =`, time);
+      // tslint:disable-next-line:only-arrow-functions
+      intervalHandler = setInterval(function() {
+        console.log(`szw time =`, time, '; interval = ', intervalHandler);
         setTime(time - 1);
       }, 1000);
     }
   };
-
 
   return (
     <div>
