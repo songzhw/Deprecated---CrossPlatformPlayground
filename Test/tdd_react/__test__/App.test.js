56ab1d077a23f1fa7c36fd90440df3af7f6b8e5c
songzhw <songzhw2012@gmail.com>
2019-08-20 21:46:39 -0400

    test failed

--- Test/tdd_react/__test__/App.test.js
+++ Test/tdd_react/__test__/App.test.js
@@ -2,6 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "../src/App";
 
+/*
+test failed:
+    Jest encountered an unexpected token
+
+    This usually means that you are trying to import a file which Jest cannot parse, e.g. it's not plain JavaScript.
+
+ */
 test("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App/>, div);
