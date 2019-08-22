714aabfe32f32f81c4b86d5302397db921b4ecb7
songzhw <songzhw2012@gmail.com>
2019-08-21 22:41:24 -0400

    test failed     Expected: "hello world"     Received: undefined

--- Test/tdd_react/__tests__/CarouselButton.test.js
+++ Test/tdd_react/__tests__/CarouselButton.test.js
@@ -2,7 +2,15 @@ import React from "react";
 import { CarouselButton } from "../src/CarouselButton";
 import { shallow } from "enzyme";
 
-test("renders a <button>", () => {
-  const wrapper = shallow(<CarouselButton/>);
-  expect(wrapper.type()).toBe("button");
+describe("CaraouselButton", () => {
+  test("renders a <button>", () => {
+    const wrapper = shallow(<CarouselButton/>);
+    expect(wrapper.type()).toBe("button");
+  });
+
+  test("passes 'children' throught the <button>", () => {
+    const text = "hello world";
+    const wrapper = shallow(<CarouselButton>text</CarouselButton>);
+    expect(wrapper.prop("children")).toBe(text);
+  });
 });
