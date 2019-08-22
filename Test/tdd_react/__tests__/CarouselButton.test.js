5960258757a1c86ea06bc5c85984ef422141da8b
songzhw <songzhw2012@gmail.com>
2019-08-21 22:50:05 -0400

    test passes

--- Test/tdd_react/__tests__/CarouselButton.test.js
+++ Test/tdd_react/__tests__/CarouselButton.test.js
@@ -8,9 +8,23 @@ describe("CaraouselButton", () => {
     expect(wrapper.type()).toBe("button");
   });
 
-  test("passes 'children' throught the <button>", () => {
+  // 转义后,   <button>{children}</button>; 就等于 <button children={children}/>
+  test("passes 'children' through the <button>", () => {
     const text = "hello world";
     const wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
     expect(wrapper.prop("children")).toBe(text);
+  });
+
+  test("passes other props through to the <button>", () => {
+    const onClick = () => {
+    };
+    const dataAction = "prev";
+    const text = "event listener";
+
+    const wrapper = shallow(<CarouselButton>{text}</CarouselButton>);
+    wrapper.setProps({ onClick, "data-action": dataAction });
+
+    expect(wrapper.prop("onClick")).toBe(onClick());
+
   });
 });
