11fa4712e40ee47a24e246c0d5dcd6b8591308d0
songzhw <songzhw2012@gmail.com>
2019-08-23 23:27:42 -0400

    my useState (demo)

--- React/react101/src/Rtutor/hooks/my_hooks/MyState.tsx
+++ React/react101/src/Rtutor/hooks/my_hooks/MyState.tsx
@@ -18,3 +18,20 @@ function parent() {
 
 export const MyReact = parent();
 // ========================================
+export const MyStateScreen = () => {
+  const [count, setCount] = MyReact.myState(10);
+  const [timeToRefresh, setTimeToRefresh] = React.useState(false); //辅助用, 主要让页面来更新
+
+  function click() {
+    const newValue = count + 10;
+    setCount(newValue);
+    setTimeToRefresh(!timeToRefresh); //不加这个, 我们自制的myState并不能强制页面更新啊!
+  }
+
+  return (
+    <div>
+      <p style={{ fontSize: 30 }}> {count}</p>
+      <button onClick={click}> increase</button>
+    </div>
+  );
+};
