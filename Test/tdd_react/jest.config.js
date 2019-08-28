b9bfb7fa337d64210c01007f251e8a961227783b
songzhw <songzhw2012@gmail.com>
2019-08-27 21:36:14 -0400

    jest config

--- Test/tdd_react/jest.config.js
+++ Test/tdd_react/jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
-  coverageDirectory: "coverage",
-  setupFilesAfterEnv: ["<rootDir>/enzyme.setup.js"],
   setupFiles: ["<rootDir>/jest.config.js"],
-  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/enzyme.setup.js"],
   moduleNameMapper: {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileMock.js",
     "\\.(css|less)$": "<rootDir>/fileMock.js"
