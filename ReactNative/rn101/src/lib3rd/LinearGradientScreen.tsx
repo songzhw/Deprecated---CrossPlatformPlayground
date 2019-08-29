11f5c3c9eec1aeb03bf79c6078b52355e060ddb7
songzhw <songzhw2012@gmail.com>
2019-08-29 10:22:51 -0400

    linear gradient - clickable, activeOpacity

--- ReactNative/rn101/src/lib3rd/LinearGradientScreen.tsx
+++ ReactNative/rn101/src/lib3rd/LinearGradientScreen.tsx
@@ -1,18 +1,23 @@
 import React from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
+import { View, ViewProps, Text, StyleSheet, TouchableOpacity, TouchableHighlight } from "react-native";
 import LinearGradientView from "react-native-linear-gradient";
 
 interface IProps extends ViewProps {
 }
 
-// TODO 可点击
 export const LinearGradientScreen = (props: IProps) => {
+  const click = () => {
+    console.log(`szw click me : `, new Date());
+  };
 
+  // TouchableOpacity.activeOpacity默认0.7.  当值为0.2时, 按下去就是一片白了, 看不见字了. 0.7还能看见字.
   return (
     <View style={styles.container}>
-      <LinearGradientView colors={["#91004B", "#C60C30"]} locations={[0, 0.8859]} style={styles.gradientParent}>
-        <Text style={styles.text}> Yes, that's what I need </Text>
-      </LinearGradientView>
+      <TouchableHighlight onPress={click} activeOpacity={0.8}>
+        <LinearGradientView colors={["#91004B", "#C60C30"]} locations={[0, 0.8859]} style={styles.gradientParent}>
+          <Text style={styles.text}> Yes, that's what I need </Text>
+        </LinearGradientView>
+      </TouchableHighlight>
     </View>
   );
 };
