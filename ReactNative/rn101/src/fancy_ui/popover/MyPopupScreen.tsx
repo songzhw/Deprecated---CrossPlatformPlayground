72ccb62c9c39f190707d43f5cc2ab2f73f246f74
songzhw <songzhw2012@gmail.com>
2019-09-01 11:24:05 -0400

    still no full-screen translucent effect

--- ReactNative/rn101/src/fancy_ui/popover/MyPopupScreen.tsx
+++ ReactNative/rn101/src/fancy_ui/popover/MyPopupScreen.tsx
@@ -2,15 +2,12 @@ import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
 import { MyPopupView } from "./MyPopupView";
 
-interface IProps extends ViewProps {
-}
-
-export const MyPopupScreen = (props: IProps) => {
+export const MyPopupScreen = () => {
 
   return (
     <View style={styles.container}>
       <Text> Hello Popup View</Text>
-      <MyPopupView style={{ flex: 1 }} isVisible={true} onClose={() => console.log("szw close")}/>
+      <MyPopupView isVisible={true} onClose={() => console.log("szw close")}/>
     </View>
   );
 };
--- ReactNative/rn101/src/fancy_ui/popover/MyPopupView.tsx
+++ ReactNative/rn101/src/fancy_ui/popover/MyPopupView.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, StyleSheet, TouchableOpacity, ViewProps } from "react-native";
 
-interface IProps extends ViewProps{
+interface IProps {
   isVisible: boolean;
   onClose: () => void;
 }
@@ -20,7 +20,9 @@ export class MyPopupView extends React.Component<IProps> {
 
 const styles = StyleSheet.create({
   translucentBg: {
-    flex: 1,
+    left: 0,
+    top: 0,
+    position: "absolute",
     backgroundColor: "#000000aa"
   }
 });
