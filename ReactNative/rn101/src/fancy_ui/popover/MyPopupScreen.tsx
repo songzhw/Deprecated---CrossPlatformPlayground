9715181a795d3d734248fb9e0bb8eda948ed4f82
songzhw <songzhw2012@gmail.com>
2019-09-01 19:08:03 -0400

    measure, onLayout

--- ReactNative/rn101/src/fancy_ui/popover/MyPopupScreen.tsx
+++ ReactNative/rn101/src/fancy_ui/popover/MyPopupScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, ViewProps, Text, StyleSheet, Button, Image } from "react-native";
+import { View, ViewProps, Text, StyleSheet, Button, Image, LayoutChangeEvent } from "react-native";
 import { MyPopupView } from "./MyPopupView";
 
 export const MyPopupScreen = () => {
@@ -8,24 +8,25 @@ export const MyPopupScreen = () => {
 
   useEffect(() => {
     // @ts-ignore
-    // x = 0, y = 0, width = 100, height = 100, pageX = 130, pageY = 156
+    // x = 0, y = 0, width = 100, height = 100, pageX = 130, pageY = 156.5
     iv.current!.measure((x, y, width, height, pageX, pageY) => {
       console.log("szw measure: ", x, y, width, height, pageX, pageY);
-    });
-
-    // @ts-ignore
-    iv.current!.onLayout((x, y, width, height) => {
-      console.log(`szw onLayout: `, x, y, width, height);
     });
   }, []);
 
   const showPopupView = () => {
   };
 
+  const onImageLayout = (ev: LayoutChangeEvent) => {
+    const { x, y, width, height } = ev.nativeEvent.layout;
+    console.log(`szw onLayout: `, x, y, width, height); //=> 130, 55, 100, 100
+  };
+
   return (
     <View style={styles.container}>
       <Button title={"click me"} onPress={showPopupView}/>
-      <Image ref={iv} source={require("../../../res/images/category_app.png")} style={styles.image}/>
+      <Image ref={iv} source={require("../../../res/images/category_app.png")} style={styles.image}
+             onLayout={ev => onImageLayout(ev)}/>
 
       <MyPopupView isVisible={true} onClose={() => console.log("szw close")} fromReact={rect}>
         <Text> I'm context menu~ </Text>
