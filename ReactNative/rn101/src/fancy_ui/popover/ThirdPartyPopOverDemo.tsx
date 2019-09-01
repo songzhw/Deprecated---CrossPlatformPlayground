cbf959b6465bcc48ea32599ef5874ba7a35592dc
songzhw <songzhw2012@gmail.com>
2019-09-01 09:39:28 -0400

    got a MutableObject, but "fromView" expected a Component

--- ReactNative/rn101/src/fancy_ui/popover/ThirdPartyPopOverDemo.tsx
+++ ReactNative/rn101/src/fancy_ui/popover/ThirdPartyPopOverDemo.tsx
@@ -1,11 +1,13 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { View, ViewProps, Text, StyleSheet, Button, Image } from "react-native";
+import Popover from "react-native-popover-view";
 
 interface IProps extends ViewProps {
 }
 
 export const ThirdPartyPopOverDemo = (props: IProps) => {
   const [isVisible, setVisible] = useState(false);
+  const iv = useRef(null);
 
   function showPopOver() {
     setVisible(true);
@@ -19,7 +21,16 @@ export const ThirdPartyPopOverDemo = (props: IProps) => {
   return (
     <View style={styles.container}>
       <Button title={"click me"} onPress={showPopOver}/>
-      <Image source={require("../../../res/images/category_app.png")}/>
+      <Image ref={iv} source={require("../../../res/images/category_app.png")}/>
+
+      <Popover
+        isVisible={isVisible}
+        fromView={iv}
+        onRequestClose={closePopOver}
+      >
+        <Text>Here is Context Menu</Text>
+      </Popover>
+
     </View>
   );
 };
