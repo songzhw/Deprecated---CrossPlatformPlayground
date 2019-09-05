c0ac521a5b7c7e1693e94c3d36c79dec8cbdbdd6
songzhw <songzhw2012@gmail.com>
2019-09-05 07:12:11 -0400

    test slides - failed at first

--- Test/tdd_react/__tests__/Carousel.test.js
+++ Test/tdd_react/__tests__/Carousel.test.js
@@ -2,13 +2,22 @@ import { shallow } from "enzyme";
 import React from "react";
 import { Carousel } from "../src/Carousel";
 import { CarouselButton } from "../src/CarouselButton";
+import { CarouselSlide } from "../src/CarouselSlide";
 
 describe("Carousel", () => {
+  const slides = [
+    { image: "https://a.com/1.png", description: "hello", author: "zzz" },
+    { image: "https://a.com/2.png", description: "world", author: "xxx" },
+    { image: "https://a.com/3.png", description: "js", author: "yyy" }
+  ];
+
+
   test.skip("has slide and button children", () => {
     const wrapper = shallow(<Carousel/>);
     console.log(`szw ,`, wrapper.debug());
     expect(wrapper.childAt(0).type()).toBe("CarouselSlide");
     expect(wrapper.childAt(1).type()).toBe("CarouselButton");
+    // 其实可以用 wrapper.find(CarouselSlide).toBeNotNull()来测试!
   });
 
   test("initial state", () => {
@@ -24,6 +33,12 @@ describe("Carousel", () => {
     const nextButton = wrapper.find(CarouselButton).at(1);
     nextButton.simulate("click");
     expect(wrapper.state("index")).toBe(1);
+  });
+
+  test("renders the current slide as a CarouselSlide", () => {
+    const wrapper = shallow(<Carousel/>);
+    let slideProps = wrapper.find(CarouselSlide).props();
+    expect(slideProps).toEqual(slides[0]);
   });
 
 
