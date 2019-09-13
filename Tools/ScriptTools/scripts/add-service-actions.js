dc28b8f7eb596f015d2bc37c2c4a4614d0a60294
songzhw <songzhw2012@gmail.com>
2019-09-13 18:42:19 -0400

    argv and parameters

--- Tools/ScriptTools/scripts/add-service-actions.js
+++ Tools/ScriptTools/scripts/add-service-actions.js
@@ -2,3 +2,11 @@
 [usage]
 yarn run add-service-actions --dest "./result" --add "play music, pause music"
  */
+
+const fs = require("fs");
+const changeCase = require("change-case");
+const argv = require("yargs").argv; // 格式是: {_:[a,b,c], action: one, id: 20}
+
+const argDestination = argv.dest;  //=> "./result"
+const argAddedActon = argv.add;    //=> "play music, pause music"
+const actionArrayFromArg = argAddedActon.split(",").map(item => item.trim());
