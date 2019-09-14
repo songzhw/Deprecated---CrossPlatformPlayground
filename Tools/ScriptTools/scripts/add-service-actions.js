3393d4f9f69c6f9fc8d1cbcd2555a85d6b40da2f
songzhw <songzhw2012@gmail.com>
2019-09-14 11:01:28 -0400

    oh, I can not use expression inside the regexp

--- Tools/ScriptTools/scripts/add-service-actions.js
+++ Tools/ScriptTools/scripts/add-service-actions.js
@@ -1,6 +1,6 @@
 /*
 [usage]
-yarn run add-service-actions --dest "./result" --add "play music, pause music"
+yarn run add-service-actions --dest "scripts/result" --add "play music, pause music" --service "read life"
  */
 
 const fs = require("fs");
@@ -10,9 +10,38 @@ const argv = require("yargs").argv; // 格式是: {_:[a,b,c], action: one, id: 2
 const argDestination = argv.dest;  //=> "./result"
 const argAddedActon = argv.add;    //=> "play music, pause music"
 const actionArrayFromArg = argAddedActon.split(",").map(item => item.trim());
-
+const serviceFromCmd = argv.service;
+const serviceConstant = changeCase.constant(serviceFromCmd);
 
 // ================== 1. add actions ==================
-fs.readFile(`scripts/result/actions/index.ts`, (error, data) => {
-  console.log(`async `, data.toString());
+const data = fs.readFileSync(`${argDestination}/actions/index.ts`);
+const OrigActionFileContent = data.toString();
+let stringBuffer = "";
+actionArrayFromArg.forEach((action) => {
+  const actionConstant = changeCase.constant(action); //=> "LOAD_API"
+  const actionSuffix = changeCase.path(action).toUpperCase();
+  stringBuffer += `  ${actionConstant} = "@@_${serviceConstant}/${actionSuffix}",\n`;
 });
+stringBuffer = stringBuffer.slice(0, -1); //remove the last "\n"
+let updatedActionsFileContent = OrigActionFileContent.replace(/(export enum ActionTypes {)/, `$1\n${stringBuffer}`);
+const length = updatedActionsFileContent.length;
+updatedActionsFileContent = updatedActionsFileContent.replace(/(.{length-1}/, "$1\nHELLO");
+console.log(updatedActionsFileContent);
+
+
+//1. fs.readFile(), 路径是project root为base dir
+
+/*
+let str = "abcdefg";
+str = str.replace(/[a-c]{3}/, "XYZ");
+console.log(str);  //=> XYZdefg
+
+str = "abcdefg";
+str = str.replace(/[a-c]{3}/, "$1XYZ");
+console.log(str);  //=> $1XYZdefg
+
+str = "abcdefg";
+str = str.replace(/([a-c]{3})/, "$1XYZ");
+console.log(str);  //=> abcXYZdefg
+
+ */
