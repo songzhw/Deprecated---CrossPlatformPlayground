6b4c9c18b9fd3bb34839314eb7aa9e4c9e030ead
songzhw <songzhw2012@gmail.com>
2019-09-25 14:01:18 -0400

    Once I bring "const styles = parseStyle(textStyle, props);" back, I will get an error : "Invariant Violation '0' is not a valid style property"

--- ReactNative/rn101/src/rn_tutorial/theme/DynamicStyleRulsDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/theme/DynamicStyleRulsDemo.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
 
 interface IProps extends ViewProps {
+  fontSize?: number
 }
 
 export const DynamicStyleRulesDemo = (props: IProps) => {
@@ -10,17 +11,25 @@ export const DynamicStyleRulesDemo = (props: IProps) => {
     fontSize: props.fontSize * 2,
     color: "blue"
   });
+  const styles = parseStyle(textStyle, props);
 
   return (
     <View>
-      <Text> 动态样式 </Text>
+      <Text style={{ fontSize: 20, color: "blue" }}> 动态样式 </Text>
     </View>
   );
 };
 
+DynamicStyleRulesDemo.defaultProps = {
+  fontSize: 10
+};
 
-const styles = StyleSheet.create({
-  container: {}
-});
 
+// const styles = StyleSheet.create({
+//   container: {}
+// });
 
+function parseStyle(fn: (...args: any[]) => any, props: any) {
+  const result = fn(props);  //=>  { fontSize: 20, color: 'blue' }
+  return StyleSheet.create(result);
+}
