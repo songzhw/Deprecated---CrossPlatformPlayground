96cd4b9daf615c2d6af00ad2d95e8f02728b88e4
songzhw <songzhw2012@gmail.com>
2019-09-25 09:27:12 -0400

    what the results will be when we don't have enzyme-to-json

--- Test/tdd_react/jest.config.js
+++ Test/tdd_react/jest.config.js
@@ -4,6 +4,5 @@ module.exports = {
   moduleNameMapper: {
     "\\.(jpg|ico|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/fileMock.js",
     "\\.(css|less)$": "<rootDir>/fileMock.js"
-  },
-  snapshotSerializers: ["enzyme-to-json/serializer"]
+  }
 };
