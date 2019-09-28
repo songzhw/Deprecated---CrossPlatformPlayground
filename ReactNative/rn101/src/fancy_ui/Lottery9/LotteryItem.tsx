0e7d98224dea1ac6de4ec7060e3e18a1441e1401
songzhw <songzhw2012@gmail.com>
2019-09-28 13:22:44 -0400

    now we have unselected UI

--- ReactNative/rn101/src/fancy_ui/Lottery9/LotteryItem.tsx
+++ ReactNative/rn101/src/fancy_ui/Lottery9/LotteryItem.tsx
@@ -1,19 +1,32 @@
 import React from "react";
-import { View, StyleSheet, Text, Image } from "react-native";
+import { View, StyleSheet, Text, Image, ImageBackground } from "react-native";
 
 interface IProps {
   source: string;
   index: number;
+  isSelected: boolean;
 }
 
 export class LotteryItem extends React.Component<IProps> {
   state = {};
 
   render() {
-    const { index, source } = this.props;
-    return (
-      <Image source={{ uri: source }} style={styles.unselected} key={`item${index}`}/>
-    );
+    const { index, source, isSelected } = this.props;
+
+    let child = null;
+
+    if (isSelected) {
+      child = <Image source={{ uri: source }} style={styles.selected} key={`item${index}`}/>;
+    } else {
+      child = (
+        <ImageBackground source={{ uri: source }} style={styles.unselected}>
+          <View style={[styles.unselected, styles.size]}/>
+        </ImageBackground>
+      );
+    }
+
+
+    return child;
   }
 }
 
@@ -24,16 +37,16 @@ const styles = StyleSheet.create({
     justifyContent: "center"
   },
   unselected: {
-    width: 96,
-    height: 80,
-    marginLeft: 3,
     backgroundColor: "rgba(0,0,0,0.5)"
   },
   selected: {
     width: 96,
     height: 80,
     marginLeft: 3
-
+  },
+  size: {
+    width: 96,
+    height: 80
   }
 });
 
