bf72d2eee6e07a219dfc2e4d3a935a1411f1552e
songzhw <songzhw2012@gmail.com>
2019-09-28 12:51:37 -0400

    add a button for lottery

--- ReactNative/rn101/src/fancy_ui/Lottery9/Lottery9Demo.tsx
+++ ReactNative/rn101/src/fancy_ui/Lottery9/Lottery9Demo.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, ViewProps, Text, StyleSheet, ImageBackground } from "react-native";
+import { View, ViewProps, Text, StyleSheet, ImageBackground, Button } from "react-native";
 import Lottery9 from "./Lottery9";
 import { LotteryData } from "./LotteryData";
 
@@ -8,8 +8,14 @@ interface IProps extends ViewProps {
 
 export const Lottery9Demo = (props: IProps) => {
 
+  const start = () => {
+    console.log(`szw start`);
+  };
+
   return (
     <View style={styles.container}>
+      <Button title={"start"} onPress={start}/>
+      <View style={{ height: 50 }}/>
       <ImageBackground source={require("../../../res/images/lottery_console.png")} style={styles.center}>
         <Lottery9 data={LotteryData}/>
       </ImageBackground>
@@ -29,6 +35,6 @@ const styles = StyleSheet.create({
     width: 345,
     height: 293,
     justifyContent: "center",
-    alignItems: "center",
+    alignItems: "center"
   }
 });
