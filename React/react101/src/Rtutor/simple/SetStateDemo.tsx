01b44ea42b19faef2752e945de5d2f231a87eede
songzhw <songzhw2012@gmail.com>
2019-10-04 12:51:05 -0400

    setstate (perfect version)

--- React/react101/src/Rtutor/simple/SetStateDemo.tsx
+++ React/react101/src/Rtutor/simple/SetStateDemo.tsx
@@ -6,7 +6,8 @@ interface IProps {
 export class SetStateDemo extends React.Component<IProps> {
   state = {
     val: 0,
-    count: 200
+    count: 200,
+    color: 0
   };
 
   constructor(props: IProps) {
@@ -21,33 +22,40 @@ export class SetStateDemo extends React.Component<IProps> {
     this.setState({ count: this.state.count - 10 });
     console.log(this.state);
 
+    this.setState({ count: this.state.count - 5, color: this.state.color + 1111 });
+    console.log(this.state);
+
     this.setState({ val: this.state.val + 3 });
     console.log(this.state);
 
     this.setState({ count: this.state.count - 30 });
     console.log(this.state);
 
+    this.setState({ val: this.state.val + 4, color: this.state.color + 2222 });
+    console.log(this.state);
+
     setTimeout(() => {
-      console.log("  ******",this.state);  //=> {val: 3, count: 170}
+      console.log("  ******", this.state);  //=> {val: 3, count: 170}
 
       this.setState({ val: this.state.val + 1 });
-      console.log("  =>",this.state);
+      console.log("  =>", this.state);
 
       this.setState({ count: this.state.count - 20 });
-      console.log("  =>",this.state);
+      console.log("  =>", this.state);
 
       this.setState({ val: this.state.val + 2 });
-      console.log("  =>",this.state);
+      console.log("  =>", this.state);
 
       this.setState({ count: this.state.count - 40 });
-      console.log("  =>",this.state);
+      console.log("  =>", this.state);
     }, 0);
   }
 
   render() {
     return <div><p/>
       <strong>value = {this.state.val}</strong><p/>
-      <strong>count = {this.state.count}</strong>
+      <strong>count = {this.state.count}</strong><p/>
+      <strong>color = {this.state.color}</strong>
     </div>;
   }
 }
