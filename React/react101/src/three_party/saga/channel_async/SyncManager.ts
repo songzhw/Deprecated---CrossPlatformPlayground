7d9b2f9c474543251d8c2e3fc67d091f6aaea3fb
songzhw <songzhw2012@gmail.com>
2019-10-06 11:00:20 -0400

    这是正常的

--- React/react101/src/three_party/saga/channel_async/SyncManager.ts
+++ React/react101/src/three_party/saga/channel_async/SyncManager.ts
@@ -17,11 +17,9 @@ function* sync(id: string) {
   const resp = yield fetch(`http://www.mocky.io/v2/${id}`);
   const str = yield resp.json();
   console.log(`==================== szw: ${str.data.id}`);
-  for (let i = 0; i < 10; i++) {
     if (listener) {
-      listener({ type: ACTION_CONTINUE_SYNC, payload: { id: i, data: str } });
+      listener({ type: ACTION_CONTINUE_SYNC, payload: { id: 1, data: str } });
     }
-  }
 }
 
 export default { subscribe, unsubscribe, sync, fetchApi };
