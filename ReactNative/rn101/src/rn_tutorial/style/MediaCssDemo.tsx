c6d7077c345e723a8f6f6afaecc042a2426ef342
songzhw <songzhw2012@gmail.com>
2019-10-08 11:22:45 -0400

    nothing happens

--- ReactNative/rn101/src/rn_tutorial/style/MediaCssDemo.tsx
+++ ReactNative/rn101/src/rn_tutorial/style/MediaCssDemo.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
 
-interface IProps extends ViewProps  {}
+/*
+React Native does not support css media query
+
+If you insist, you can instal `react-native-css-media-query-processor`
+https://github.com/kristerkari/react-native-css-media-query-processor
+
+ */
+interface IProps extends ViewProps {
+}
 
 export const MediaCssDemo = (props: IProps) => {
 
@@ -9,10 +17,20 @@ export const MediaCssDemo = (props: IProps) => {
     <View style={styles.container}>
 
     </View>
-  )
-}
+  );
+};
 
 
 const styles = StyleSheet.create({
-  container: {}
+// @ts-ignore
+  container: props => ({
+    flex: 1,
+    backgroundColor: 'green',
+    "@media (orientation: landscape)": {
+      backgroundColor: "red"
+    },
+    "@media (orientation: portrait)": {
+      backgroundColor: "blue"
+    }
+  })
 });
