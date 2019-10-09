837f2a8943d81d96a1ddb1ef996e499b9cde885d
songzhw <songzhw2012@gmail.com>
2019-10-09 16:46:08 -0400

    add @community/r-n-localize

--- ReactNative/rn06x/src/core/i18n/i18n.ts
+++
@@ -1,5 +0,0 @@
-import LocalizedStrings from "react-native-localization";
-
-const i18nStrings = require("./strings.json");
-/* make it "any", so we could get no warnings when we use `strings.hello` */
-export const strings : any = new LocalizedStrings(i18nStrings);
