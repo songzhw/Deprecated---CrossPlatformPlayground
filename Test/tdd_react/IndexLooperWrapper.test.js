2d7d79d35c46640f9519774d0d62354b5a0829f8
songzhw <songzhw2012@gmail.com>
2019-10-22 09:34:31 -0400

    test hoc

--- Test/tdd_react/IndexLooperWrapper.test.js
+++ Test/tdd_react/IndexLooperWrapper.test.js
@@ -17,5 +17,11 @@ describe("[HoC] IndexLooperWrapper", () => {
     expect(wrapper.state("index")).toBe(0);
   });
 
+  // 传入index:2的props不成功, 是因为我们传递属性时只传递this.state.index, 没有管props
+  test("passes in `index` props would not work", () => {
+    wrapper.setProps({ index: 2 });
+    expect(wrapper.state("index")).toBe(0);
+  });
+
 
 });
