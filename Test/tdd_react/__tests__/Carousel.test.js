ec1dc47abf465d14a5629f6945cfc0a9697bdfa6
songzhw <songzhw2012@gmail.com>
2019-10-22 09:03:21 -0400

    fix the code

--- Test/tdd_react/__tests__/Carousel.test.js
+++ Test/tdd_react/__tests__/Carousel.test.js
@@ -37,26 +37,26 @@ describe("Carousel", () => {
 
 });
 
-// describe("with the first/last slide selected (edge case)", () => {
-//   test("wraps `index` to the max value when the first slide is selected and Prev is clicked", () => {
-//     const wrapper = shallow(<Carousel slides={slides}/>);
-//     wrapper.setState({ index: 0 });
-//
-//     const prevButton = wrapper.find("[data-testId='btnPrev']");
-//     prevButton.simulate("click");
-//
-//     const lastIndex = slides.length - 1;
-//     expect(wrapper.state("index")).toBe(lastIndex);
-//   });
-//
-//   test("wraps `index` to 0 when the last slide is selected and Next is clicked", () => {
-//     const wrapper = shallow(<Carousel slides={slides}/>);
-//     const lastIndex = slides.length - 1;
-//     wrapper.setState({ index: lastIndex });
-//
-//     const prevButton = wrapper.find("[data-testId='btnNext']");
-//     prevButton.simulate("click");
-//
-//     expect(wrapper.state("index")).toBe(0);
-//   });
-// });
+describe("with the first/last slide selected (edge case)", () => {
+  test("wraps `index` to the max value when the first slide is selected and Prev is clicked", () => {
+    const wrapper = shallow(<Carousel slides={slides}/>);
+    wrapper.setState({ index: 0 });
+
+    const prevButton = wrapper.dive().find("[data-testId='btnPrev']");
+    prevButton.simulate("click");
+
+    const lastIndex = slides.length - 1;
+    expect(wrapper.state("index")).toBe(lastIndex);
+  });
+
+  test("wraps `index` to 0 when the last slide is selected and Next is clicked", () => {
+    const wrapper = shallow(<Carousel slides={slides}/>);
+    const lastIndex = slides.length - 1;
+    wrapper.setState({ index: lastIndex });
+
+    const prevButton = wrapper.dive().find("[data-testId='btnNext']");
+    prevButton.simulate("click");
+
+    expect(wrapper.state("index")).toBe(0);
+  });
+});
