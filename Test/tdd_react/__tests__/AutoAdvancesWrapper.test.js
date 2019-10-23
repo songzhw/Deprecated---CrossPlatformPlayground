d61ed393cd83ce9a13cb9fb43cc81a22f031edd6
songzhw <songzhw2012@gmail.com>
2019-10-23 09:05:55 -0400

    TDD - make tests pass

--- Test/tdd_react/__tests__/AutoAdvancesWrapper.test.js
+++ Test/tdd_react/__tests__/AutoAdvancesWrapper.test.js
@@ -4,19 +4,23 @@ import { shallow } from "enzyme";
 
 describe("HoC AutoAdvances", () => {
   const MockComponent = () => null;
-  const Hoc = AutoAdvancesWrapper(AutoAdvancesWrapper);
+  const Hoc = AutoAdvancesWrapper(AutoAdvancesWrapper, "index");
   let wrapper;
   let incrementFunc;
   const interval = 10e3; //10,000 (10 followed by 3 zeros)
   const upperBound = 5;
 
   beforeEach(() => {
+    jest.useFakeTimers();
     incrementFunc = jest.fn();
-    wrapper = shallow(<Hoc indexIncrement={incrementFunc} interval={interval}/>);
+    wrapper = shallow(
+      <Hoc indexIncrement={incrementFunc}
+           interval={interval} upperBound={upperBound}
+      />);
   });
 
   test("calls the increment function after `interval`", () => {
-    // TODO 时间前进10秒
+    jest.advanceTimersByTime(interval);
     expect(incrementFunc).toBeCalledWith(upperBound);
   });
 });
--- Test/tdd_react/src/AutoAdvancesWrapper.js
+++ Test/tdd_react/src/AutoAdvancesWrapper.js
@@ -1,7 +1,23 @@
 import React from "react";
 
-export const AutoAdvancesWrapper = (InComponent) =>
+export const AutoAdvancesWrapper = (InComponent, advancePropName) =>
   class ComponentWithAutoAdvance extends React.PureComponent {
+
+    componentDidMount() {
+      this.startTimer();
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
+      // 第一次加载时不走componentDidUpdate
+    }
+
+    startTimer = () => {
+      const { interval, upperBound } = this.props;
+      this._timer = setTimeout(() => {
+        this.props[`${advancePropName}Increment`](upperBound);
+      }, interval);
+    };
+
     render() {
       return <InComponent {...this.props}/>;
     }
