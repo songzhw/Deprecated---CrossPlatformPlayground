1c375d2f4e7bd8ae5e79f609bbedb13e363de636
songzhw <songzhw2012@gmail.com>
2019-10-23 08:51:32 -0400

    TDD - test fails at first

--- Test/tdd_react/__tests__/AutoAdvancesWrapper.test.js
+++ Test/tdd_react/__tests__/AutoAdvancesWrapper.test.js
@@ -1,5 +1,22 @@
-describe("HoC AutoAdvances", () => {
-  test("", () => {
+import React from "react";
+import { AutoAdvancesWrapper } from "../src/AutoAdvancesWrapper";
+import { shallow } from "enzyme";
 
+describe("HoC AutoAdvances", () => {
+  const MockComponent = () => null;
+  const Hoc = AutoAdvancesWrapper(AutoAdvancesWrapper);
+  let wrapper;
+  let incrementFunc;
+  const interval = 10e3; //10,000 (10 followed by 3 zeros)
+  const upperBound = 5;
+
+  beforeEach(() => {
+    incrementFunc = jest.fn();
+    wrapper = shallow(<Hoc indexIncrement={incrementFunc} interval={interval}/>);
+  });
+
+  test("calls the increment function after `interval`", () => {
+    // TODO 时间前进10秒
+    expect(incrementFunc).toBeCalledWith(upperBound);
   });
 });
