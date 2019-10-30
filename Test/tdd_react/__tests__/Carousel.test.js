beac3c7239062a4ed28b7105372082eea8d63d9d
songzhw <songzhw2012@gmail.com>
2019-10-30 09:20:51 -0400

    fix the broken tests against Carousel

--- Test/tdd_react/__tests__/Carousel.test.js
+++ Test/tdd_react/__tests__/Carousel.test.js
@@ -1,4 +1,4 @@
-import { shallow } from "enzyme";
+import { mount, shallow } from "enzyme";
 import React from "react";
 import { CarouselButton } from "../src/CarouselButton";
 import { CarouselSlide } from "../src/CarouselSlide";
@@ -31,11 +31,14 @@ describe("Carousel", () => {
 
 describe("with the first/last slide selected (edge case)", () => {
   test("wraps `index` to the max value when the first slide is selected and Prev is clicked", () => {
-    const Component = IndexLooperWrapper(Carousel, "index");
-    const wrapper = shallow(<Component slides={slides}/>);
+    const wrapper = shallow(<Carousel slides={slides}/>);
+    console.log(`1. `, wrapper.debug());   //=> <ComponentWithAutoAdvance slides={{...}} index={0} indexIncrement={[Function]} indexDecrement={[Function]} />
+    console.log(`2. `, wrapper.dive().debug());          //=>  <_Carousel slides={{...}} index={0} indexIncrement={[Function]} indexDecrement={[Function]} defaultImageHeight={500} />
+    console.log(`3. `, wrapper.dive().dive().debug());  //=> <div> <CarouselSlide/> <CarouselButton/>. ... </div>
+
     wrapper.setState({ index: 0 });
 
-    const prevButton = wrapper.dive().find("[data-testId='btnPrev']");
+    const prevButton = wrapper.dive().dive().find("[data-testId='btnPrev']");
     prevButton.simulate("click");
 
     const lastIndex = slides.length - 1;
@@ -44,10 +47,11 @@ describe("with the first/last slide selected (edge case)", () => {
 
   test("wraps `index` to 0 when the last slide is selected and Next is clicked", () => {
     const wrapper = shallow(<Carousel slides={slides}/>);
+    console.log(`4. `, wrapper.debug());
     const lastIndex = slides.length - 1;
     wrapper.setState({ index: lastIndex });
 
-    const prevButton = wrapper.dive().find("[data-testId='btnNext']");
+    const prevButton = wrapper.dive().dive().find("[data-testId='btnNext']");
     prevButton.simulate("click");
 
     expect(wrapper.state("index")).toBe(0);
