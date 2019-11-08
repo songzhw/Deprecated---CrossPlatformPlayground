d5eb5dde6f782ab8edeefdd9a1611244dc63e90d
songzhw <songzhw2012@gmail.com>
2019-11-08 15:17:09 -0500

    add a mock, so tests are not crashing anymore

--- Test/Rn_Test/src/third_party/__tests__/NetInfoDemo.test.js
+++ Test/Rn_Test/src/third_party/__tests__/NetInfoDemo.test.js
@@ -1,7 +1,15 @@
 import React from "react";
-import shallow from "enzyme/src/shallow";
+import { shallow } from "enzyme";
 import { NetInfoDemo } from "../NetInfoDemo";
 import { Button } from "react-native";
+
+jest.mock("@react-native-community/netinfo", () => {
+  return {
+    fetch: jest.fn(() => Promise.resolve("11")),
+    addEventListener: jest.fn(),
+    useNetInfo: jest.fn()
+  };
+});
 
 test("NetInfo addListener", () => {
 });
