24ff6a42a1fedf54b93e7648b666c9c93d80a87c
songzhw <songzhw2012@gmail.com>
2019-11-08 15:38:47 -0500

    now it's not easy to mock a addEventListener method

--- Test/Rn_Test/src/third_party/__tests__/NetInfoDemo.test.js
+++ Test/Rn_Test/src/third_party/__tests__/NetInfoDemo.test.js
@@ -3,21 +3,31 @@ import { shallow } from "enzyme";
 import { NetInfoDemo } from "../NetInfoDemo";
 import { Button } from "react-native";
 
+//TODO how to test `NetInfo.addEventListener(state=>{}) ???
+const wifi = { type: "wifi" };
+const mobile = { type: "5G" };
+let temp = wifi;
+
 jest.mock("@react-native-community/netinfo", () => {
   return {
     fetch: jest.fn(() => new Promise((resolve, reject) => {
       resolve({ type: "wifi" });
     })),
-    addEventListener: jest.fn(),
+    addEventListener: jest.fn((state) => {
+      console.log(`szw listener (mock)`);
+    }),
     useNetInfo: jest.fn()
   };
 });
 
-test("NetInfo addListener", () => {
-});
+// test("NetInfo fetch", async () => {
+//   const tree = shallow(<NetInfoDemo/>);
+//   await tree.find(Button).simulate("press");
+//   expect(tree.state("net")).toEqual({ type: "wifi" }); // toEqual用来比较对象的内容!
+// });
 
-test("NetInfo fetch", async () => {
+
+test("NetInfo addListener", () => {
   const tree = shallow(<NetInfoDemo/>);
-  await tree.find(Button).simulate("press");
-  expect(tree.state("net")).toEqual({ type: "wifi" }); // toEqual用来比较对象的内容!
+  tree.mount();
 });
