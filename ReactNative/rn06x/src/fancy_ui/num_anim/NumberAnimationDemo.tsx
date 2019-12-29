1dae2ff1135e6a5628c374a03be924843912ecc4
songzhw <songzhw2012@gmail.com>
2019-12-28 22:02:17 -0500

    refactor a common component

--- ReactNative/rn06x/src/fancy_ui/num_anim/NumberAnimationDemo.tsx
+++ ReactNative/rn06x/src/fancy_ui/num_anim/NumberAnimationDemo.tsx
@@ -1,47 +1,28 @@
 import React from "react";
 import { View, StyleSheet, Animated, Text, Button } from "react-native";
+import { NumberScroller } from "./NumberScroller";
 
 export class NumberAnimationDemo extends React.Component {
-  state = {
-    animation: new Animated.Value(0)
+
+  startAnimation = () => {
+    // @ts-ignore
+    this.refs.number1.startAnimation()
   };
 
-  numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  resetAnimation = () => {
+
+  };
 
   render() {
-    const transformStyle = {
-      transform: [{
-        translateY: this.state.animation
-      }]
-    };
-
     return (
       <View style={styles.container}>
         <Button title="start " onPress={this.startAnimation}/>
         <Button title="rest " onPress={this.resetAnimation}/>
-        <View style={styles.numberBox}>
-          <Animated.View style={[transformStyle]}>
-            {this.numbers.map(num => (
-              <Text key={num} style={{ height: 40 }}>{num}</Text>
-            ))}
-          </Animated.View>
-        </View>
+        <NumberScroller ref="number1"/>
       </View>
     );
   }
 
-  private startAnimation = () => {
-    Animated.timing(this.state.animation, {
-      toValue: -310,
-      duration: 1000
-    }).start();
-  };
-
-  private resetAnimation = () => {
-    this.setState({
-      animation: new Animated.Value(0)
-    });
-  };
 
 }
 
@@ -49,12 +30,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row"
-  },
-  numberBox: {
-    width: 40,
-    height: 40,
-    backgroundColor: ("#2E7D32"),
-    overflow: "hidden"
   }
 });
 
---
+++ ReactNative/rn06x/src/fancy_ui/num_anim/NumberScroller.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { Animated, StyleSheet, Text, View } from "react-native";
+
+export class NumberScroller extends React.Component {
+  numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+  state = {
+    animation: new Animated.Value(0)
+  };
+
+  transformStyle = {
+    transform: [{
+      translateY: this.state.animation
+    }]
+  };
+
+  render() {
+    return (
+      <View style={styles.numberBox}>
+        <Animated.View style={[(this.transformStyle)]}>
+          {this.numbers.map(num => (
+            <Text key={num} style={{ height: 40 }}>{num}</Text>
+          ))}
+        </Animated.View>
+      </View>
+    );
+  }
+
+  startAnimation = () => {
+    Animated.timing(this.state.animation, {
+      toValue: -310,
+      duration: 1000
+    }).start();
+  };
+
+  resetAnimation = () => {
+    this.setState({
+      animation: new Animated.Value(0)
+    });
+  };
+}
+
+const styles = StyleSheet.create({
+  numberBox: {
+    width: 40,
+    height: 40,
+    backgroundColor: ("#2E7D32"),
+    overflow: "hidden"
+  }
+});
