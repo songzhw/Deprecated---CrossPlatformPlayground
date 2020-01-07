d3c5e76675fc747a4ab3cd326647e52fa1d4f5d1
songzhw <songzhw2012@gmail.com>
2020-01-07 10:43:11 -0500

    make a toast view

--- React/react101/src/biz/toast/Toast.tsx
+++ React/react101/src/biz/toast/Toast.tsx
@@ -1,13 +1,14 @@
 // 不使用class Toast, 因为使用起来就得是new Toast()才能用.
 // 我想要的是static这样的: Toast.info()
 
+import React from "react";
 import ReactDOM from "react-dom";
 import { ToastView } from "./ToastView";
 
 export class Toast {
-  show() {
+  static show(text: string) {
     const container = document.createElement("div");
     document.body.appendChild(container); //也加到本组件中来. 没写在render()里, 所以这是动态添加的
-    const view = ReactDOM.render(<ToastView text=""/>, container)
+    const view = ReactDOM.render(<ToastView text={text}/>, container);
   }
 }
--- React/react101/src/biz/toast/ToastDemo.tsx
+++ React/react101/src/biz/toast/ToastDemo.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { PreToastView } from "./split/PreToastView";
+import { Toast } from "./Toast";
 
 export class ToastDemo extends React.Component {
   state = {};
 
+  onClick = () => {
+    Toast.show("toast bing")
+  };
+
   render() {
-    return super.render();
+    return (
+      <div>
+        <button onClick={this.onClick}>add element dynamically</button>
+      </div>
+    );
   }
 }
 
