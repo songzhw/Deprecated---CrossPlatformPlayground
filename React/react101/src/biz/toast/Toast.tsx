41fe4ca53b0b999c827bd69face1f5b5ae9559cb
songzhw <songzhw2012@gmail.com>
2020-01-07 10:44:25 -0500

    static function

--- React/react101/src/biz/toast/Toast.tsx
+++ React/react101/src/biz/toast/Toast.tsx
@@ -1,6 +1,12 @@
-// 不使用class Toast, 因为使用起来就得是new Toast()才能用.
-// 我想要的是static这样的: Toast.info()
+/*
+要想要 Toast.info(). 方法有二:
 
+1. 不使用class Toast, 因为使用起来就得是new Toast()才能用.
+    所以可以定义几个函数, 然后export default {f1, f2}
+
+2. 要么就是使用class, 然后定义为static方法.
+
+*/
 import React from "react";
 import ReactDOM from "react-dom";
 import { ToastView } from "./ToastView";
