3858352adbb52ebef48fbb76b7d6fbff5f65f6d6
songzhw <songzhw2012@gmail.com>
2020-01-07 10:40:24 -0500

    error? Because this is not a tsx file!

--- React/react101/src/biz/toast/Toast.ts
+++ React/react101/src/biz/toast/Toast.ts
@@ -1,4 +1,13 @@
 // 不使用class Toast, 因为使用起来就得是new Toast()才能用.
 // 我想要的是static这样的: Toast.info()
+
+import ReactDOM from "react-dom";
+import { ToastView } from "./ToastView";
+
 export class Toast {
+  show() {
+    const container = document.createElement("div");
+    document.body.appendChild(container); //也加到本组件中来. 没写在render()里, 所以这是动态添加的
+    const view = ReactDOM.render(<ToastView text=""/>, container)
+  }
 }
