7260e069ea1f7b910377f08ffc95ceeb13c99188
songzhw <songzhw2012@gmail.com>
2020-01-08 10:54:41 -0500

    adjust the UI

--- React/react101/src/biz/toast/ToastView.tsx
+++ React/react101/src/biz/toast/ToastView.tsx
@@ -14,7 +14,7 @@ export class ToastView extends React.Component<IProps> {
       <div className="toastContainer">
         <ToastText text={this.props.text}/>
         <ToastText text={"second"}/>
-        <ToastText text={"third"}/>
+        <ToastText text={"super long and very long, hello world, this is js and react and toast playground"}/>
       </div>
     );
   }
