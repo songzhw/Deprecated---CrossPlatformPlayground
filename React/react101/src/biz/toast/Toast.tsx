c9ae215f0930646580bedf0c65a0dbaa6e8f8be2
songzhw <songzhw2012@gmail.com>
2020-01-08 10:02:15 -0500

    flex is not good

--- React/react101/src/biz/toast/Toast.tsx
+++ React/react101/src/biz/toast/Toast.tsx
@@ -14,7 +14,7 @@ import { ToastView } from "./ToastView";
 export class Toast {
   static show(text: string) {
     const container = document.createElement("div");
-    container.style.cssText = `position:absolute;left:50%;top:50%;transform: translate(-50%,-50%); `;
+    container.style.cssText = `display:flex;justify-content:center;align-items:center; background-color:green`;
     document.body.appendChild(container); //也加到本组件中来. 没写在render()里, 所以这是动态添加的
     const view = ReactDOM.render(<ToastView text={text}/>, container);
   }
