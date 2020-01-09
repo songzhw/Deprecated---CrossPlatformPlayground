f296ba58d5e4667b03e372ed66e9197c3437fc69
songzhw <songzhw2012@gmail.com>
2020-01-09 10:28:20 -0500

    no timeout log?

--- React/react101/src/biz/toast/ToastText.tsx
+++ React/react101/src/biz/toast/ToastText.tsx
@@ -1,11 +1,21 @@
 import React from "react";
-import "./Toast.css"
+import "./Toast.css";
 
 interface IProps {
   text: string;
+  duration: number;
+  onHide: () => void;
 }
 
 export class ToastText extends React.Component<IProps> {
+  public static defaultProps = {
+    duration: 2000
+  };
+
+  componentDidMount(): void {
+    setTimeout(() => this.props.onHide, this.props.duration);
+  }
+
   render() {
     return (
       <div className="text">
--- React/react101/src/biz/toast/ToastView.tsx
+++ React/react101/src/biz/toast/ToastView.tsx
@@ -9,12 +9,16 @@ interface IProps {
 export class ToastView extends React.Component<IProps> {
   state = {};
 
+  onHideOneText = () => {
+    console.log(`szw time to hide`);
+  };
+
   render() {
     return (
       <div className="toastContainer">
-        <ToastText text={this.props.text}/>
-        <ToastText text={"second"}/>
-        <ToastText text={"super long and very long, hello world, this is js and react and toast playground"}/>
+        <ToastText text={this.props.text} onHide={this.onHideOneText}/>
+        {/*<ToastText text={"second"}/>*/}
+        {/*<ToastText text={"super long and very long, hello world, this is js and react and toast playground"}/>*/}
       </div>
     );
   }
