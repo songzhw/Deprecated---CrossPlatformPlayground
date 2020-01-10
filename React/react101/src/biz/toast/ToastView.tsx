484c402bc31a3f432fec94b0de913fa49dd9b15f
songzhw <songzhw2012@gmail.com>
2020-01-10 16:38:39 -0500

    disappear toast

--- React/react101/src/biz/toast/ToastView.tsx
+++ React/react101/src/biz/toast/ToastView.tsx
@@ -15,15 +15,19 @@ interface ToastItem {
   isShowing: boolean;
 }
 
-export class ToastView extends React.Component<IProps> {
-  queue: ToastItem[] = [];
-  state = { queue: this.queue };
+interface IState {
+  queue: ToastItem[];
+}
+
+export class ToastView extends React.Component<IProps, IState> {
+  state = { queue: [] };
 
   add(text: string) {
     const id = weakUuid();
     const item = { id, text, onClose: this.onHideOneText, isShowing: true };
-    this.queue.push(item);
-    this.setState({ queue: this.queue });
+    const { queue } = this.state;
+    queue.push(item);
+    this.setState({ queue });
   }
 
   onHideOneText = (toastId: string) => {
@@ -37,7 +41,7 @@ export class ToastView extends React.Component<IProps> {
     return (
       <div className="toastContainer">
 
-        {this.queue.reverse()
+        {this.state.queue.reverse()
           .filter(item => item.isShowing)
           .map(item =>
             (<ToastText key={item.id} id={item.id} text={item.text} onHide={item.onClose}/>)
