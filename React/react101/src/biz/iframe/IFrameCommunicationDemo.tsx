2a0fe45b394d8eb512afc868199102aa32452593
songzhw <songzhw2012@gmail.com>
2020-01-12 12:50:37 -0500

    react receive msg from iframe pages -- but it failed

--- React/react101/src/biz/iframe/IFrameCommunicationDemo.tsx
+++ React/react101/src/biz/iframe/IFrameCommunicationDemo.tsx
@@ -3,11 +3,19 @@ import React from "react";
 export class IFrameCommunicationDemo extends React.Component {
   state = {};
 
+  componentDidMount() {
+    const callback = (event: any) => {
+      console.log(`szw get value from <iframe> page: `, event);
+    };
+    window.addEventListener("change", callback, false);
+  }
+
+
   render() {
     return (
       <div>
         <h1> {"Iframe <=> React"} </h1>
-        <iframe src="./loaded.html" id="ifa"/>
+        <iframe src="loaded.html" id="ifa"/>
       </div>
     );
   }
