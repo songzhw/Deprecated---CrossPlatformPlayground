3e66302c20a56bc0b824c3941dad85f70c1104e2
songzhw <songzhw2012@gmail.com>
2020-01-15 06:35:25 -0500

    add another iframe. but the style is wrong?

--- React/react101/src/biz/iframe/IFrameCommunicationDemo.tsx
+++ React/react101/src/biz/iframe/IFrameCommunicationDemo.tsx
@@ -29,6 +29,8 @@ export class IFrameCommunicationDemo extends React.Component {
         <h1> {"Iframe <=> React"} </h1>
         <button onClick={this.passMessage}>{"Pass msg to <iframe>"}</button>
         <p/>
+        <iframe src="loaded.html" style="height: 200"/>
+        <p/>
         <iframe src="loaded.html" ref={el => this.webview = el} className="webview" frameBorder={1}/>
       </div>
     );
