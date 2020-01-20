37e49e748bcdcd6092ef740d2bb769a80db08672
songzhw <songzhw2012@gmail.com>
2020-01-20 16:03:57 -0500

    now the drop zone is here (although we could only select file, can't drag&drop it)

--- React/react101/src/three_party/dropzone/DropZoneDemo.tsx
+++ React/react101/src/three_party/dropzone/DropZoneDemo.tsx
@@ -16,18 +16,16 @@ export const DropZoneDemo = (props: IProps) => {
       reader.readAsText(file);
     });
   }, []);
-  const value = useDropzone({
-    onDrop,
-    accept: "text/*,application/epub+zip"
-  });
-  console.log(`szw v = `, value)
-  const { getRootProps, getInputProps, isDragActive } = value
+  const value = useDropzone({ onDrop });
+  const { getRootProps, getInputProps, isDragActive } = value;
+  const dropHere = isDragActive ? <p>Drop files here</p> : <p>click to select files</p>;
 
   return (
     <div>
       <p>drop files here</p>
-      <div {...getRootProps()} style={{backgroundColor: "red"}}>
+      <div {...getRootProps({})} style={{ backgroundColor: "red" }}>
         <input {...getInputProps()}/>
+        {dropHere}
       </div>
     </div>
   );
