991394b661d2afb8cddc9d4bc48c84de66168c4f
songzhw <songzhw2012@gmail.com>
2020-02-06 10:27:32 -0500

    odd, the first visible item = undefined

--- React/react101/src/ui/node_tree/IframeNodeTreeDemo.tsx
+++ React/react101/src/ui/node_tree/IframeNodeTreeDemo.tsx
@@ -5,9 +5,37 @@ export class IframeNodeTreeDemo extends React.Component {
   state = {};
   webview: HTMLIFrameElement | null = null;
 
+  onClick = () => {
+    if (!this.webview) {
+      return;
+    }
+    console.log(`iframe size = `, this.webview.getBoundingClientRect());
+    const item = this.findFirstVisibleItem(this.webview);
+    console.log(`fistVisible = `, item);
+  };
+
+  findFirstVisibleItem = (parent: Element) => {
+    const length = parent.childNodes.length;
+    for (let index = 0; index < length; index++) {
+      const child = parent.childNodes[index] as Element; // ChildNode类型没有getBoundingClientRect()方法
+      if (this.isNodeInRoot(child.getBoundingClientRect(), parent.getBoundingClientRect())) {
+        console.log(`szw ${index} is in: `, child);
+        break;
+      }
+    }
+  };
+
+  isNodeInRoot = (nodeRect: ClientRect, rootRect: ClientRect) => {
+    return nodeRect.left < rootRect.right
+      && nodeRect.right > rootRect.left
+      && nodeRect.bottom > rootRect.top
+      && nodeRect.top < rootRect.bottom;
+  };
+
   render() {
     return (
       <div className="webViewContainer">
+        <button onClick={this.onClick}>first item</button>
         <iframe ref={el => this.webview = el} src="longText.html" className="webview"/>
       </div>
     );
