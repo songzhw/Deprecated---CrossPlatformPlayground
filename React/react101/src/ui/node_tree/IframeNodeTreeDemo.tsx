4ac6f04a3800358b55c6d5be26dab8748e4ff15b
songzhw <songzhw2012@gmail.com>
2020-02-06 10:12:56 -0500

    now the iframe is full-screen - it requires the parent and iframe itself to set some css rules

--- React/react101/src/ui/node_tree/IframeNodeTreeDemo.tsx
+++ React/react101/src/ui/node_tree/IframeNodeTreeDemo.tsx
@@ -1,13 +1,15 @@
 import React from "react";
-import "../UiDemos.css"
+import "../UiDemos.css";
 
 export class IframeNodeTreeDemo extends React.Component {
   state = {};
-  webview : HTMLIFrameElement | null = null;
+  webview: HTMLIFrameElement | null = null;
 
   render() {
     return (
-      <iframe ref={el => this.webview = el} src="loaded.html" />
-    )
+      <div className="webViewContainer">
+        <iframe ref={el => this.webview = el} src="loaded.html" className="webview"/>
+      </div>
+    );
   }
 }
