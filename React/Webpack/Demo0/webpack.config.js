9f8fb19810155d6cb8e810a5108837c6dbf449fd
songzhw <songzhw2012@gmail.com>
2020-03-04 10:44:59 -0500

    another way to add parameters to a loader

--- React/Webpack/Demo0/webpack.config.js
+++ React/Webpack/Demo0/webpack.config.js
@@ -10,7 +10,11 @@ module.exports = {
     rules: [
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
+        // use: ["style-loader", "css-loader"]
+        use: ["style-loader", {
+          loader: "css-loader",
+          options: { minimize: true }
+        }]
       }
     ]
   }
