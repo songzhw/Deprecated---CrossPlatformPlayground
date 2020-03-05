3ab6ece304ea1fa3bbd8f92da12506174cbfee59
songzhw <songzhw2012@gmail.com>
2020-03-05 10:51:40 -0500

    now we run devServer completely

--- React/Webpack/Demo0/src/index.js
+++ React/Webpack/Demo0/src/index.js
@@ -4,7 +4,7 @@ import "../css/index.css"
 function component() {
   const element = document.createElement('div');
   element.setAttribute("id", "app");
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.innerHTML = _.join(['Hello', 'webpack', "222"], ' ');
   return element;
 }
 
