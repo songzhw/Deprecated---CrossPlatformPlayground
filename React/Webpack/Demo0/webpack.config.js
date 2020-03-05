13950b7b0ec9d664095532eb40fc431f19bd441b
songzhw <songzhw2012@gmail.com>
2020-03-05 10:02:43 -0500

    mini-css-extract-plugin does not work

--- React/Webpack/Demo0/webpack.config.js
+++ React/Webpack/Demo0/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,15 +7,16 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'index.css',
+      chunkFilename: 'index.css'
+    })],
   module: {
     rules: [
       {
         test: /\.css$/i,
-        // use: ["style-loader", "css-loader"]
-        use: ["style-loader", {
-          loader: "css-loader",
-          options: { minimize: true }
-        }]
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
       }
     ]
   }
