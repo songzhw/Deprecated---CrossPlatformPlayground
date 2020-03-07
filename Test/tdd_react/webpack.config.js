c680a9e94ea61637a5b45ff0a19a40f8fed56d3a
songzhw <songzhw2012@gmail.com>
2020-03-07 16:17:14 -0500

    different config

--- Test/tdd_react/webpack.config.js
+++ Test/tdd_react/webpack.config.js
@@ -1,12 +1,16 @@
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ZipPlugin = require("zip-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   mode: "development",
   entry: {
     carousel: "./src/index.js",
     example: "./src/webpack_demo.js"
+  },
+  output:{
+    path: path.resolve(__dirname, "dist_[hash]")
   },
   module: {
     rules: [
