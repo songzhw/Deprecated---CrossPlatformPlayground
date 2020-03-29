ce675572a91d4539753f2c7f4b9926c541e89a49
songzhw <songzhw2012@gmail.com>
2020-03-28 22:14:11 -0400

    menu and menuItems

--- React/react101/src/three_party/material_ui/C01Basic.tsx
+++ React/react101/src/three_party/material_ui/C01Basic.tsx
@@ -16,13 +16,22 @@ export const C01Basic = (props: IProps) => {
     setAnchor(event.currentTarget);
   };
 
+  const closePopup = () => {
+    setAnchor(null);
+  };
 
   return (
     <div>
       {/*variant有三种可选值: text(边框都没有), outlined(只有边框), container(实心)*/}
       <Button variant="contained" color="primary" onClick={clickMe}>Click Me</Button>
 
-
+      {/*弹出菜单. (我测试过, 下面Button的两个aria属性不加也没问题*/}
+      <Button variant="text" aria-controls="menu-toc" aria-haspopup="true" onClick={showPopup}>TOC</Button>
+      <Menu id="menu-toc" anchorEl={anchor} open={Boolean(anchor)} onClose={closePopup}>
+        <MenuItem onClick={closePopup}>Chapter1. Hello</MenuItem>
+        <MenuItem onClick={closePopup}>Chapter2. Headache</MenuItem>
+        <MenuItem onClick={closePopup}>Chapter3. New Home</MenuItem>
+      </Menu>
 
     </div>
   );
