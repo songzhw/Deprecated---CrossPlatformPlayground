1dd4053a0ca711f42bccdadbb7092cc6407e1d8a
songzhw <songzhw2012@gmail.com>
2020-03-28 22:26:32 -0400

    now the issue is fixed

--- React/react101/src/three_party/material_ui/C01Basic.tsx
+++ React/react101/src/three_party/material_ui/C01Basic.tsx
@@ -27,7 +27,8 @@ export const C01Basic = (props: IProps) => {
 
       {/*弹出菜单. (我测试过, 下面Button的两个aria属性不加也没问题*/}
       <Button variant="text" aria-controls="menu-nav" aria-haspopup="true" onClick={showPopup}>Navigation</Button>
-      <Menu id="menu-nav" anchorEl={anchor} open={Boolean(anchor)} onClose={closePopup}>
+      <Menu id="menu-nav" anchorEl={anchor} open={anchor ? anchor.textContent === "Navigation" : false}
+            onClose={closePopup}>
         <MenuItem onClick={closePopup}>Settings</MenuItem>
         <MenuItem onClick={closePopup}>Profile</MenuItem>
         <MenuItem onClick={closePopup}>Home</MenuItem>
@@ -35,7 +36,7 @@ export const C01Basic = (props: IProps) => {
 
       {/*弹出菜单 -- 选择菜单(即弹出popover menu时, 是会记忆并突出显示上一次选的内容的*/}
       <Button variant="outlined" aria-controls="menu-toc" aria-haspopup="true" onClick={showPopup}>TOC</Button>
-      <Menu id="menu-toc" anchorEl={anchor} open={Boolean(anchor)} onClose={closePopup}>
+      <Menu id="menu-toc" anchorEl={anchor} open={anchor ? anchor.textContent === "TOC" : false} onClose={closePopup}>
         <MenuItem onClick={closePopup}>Chapter1. Hello</MenuItem>
         <MenuItem onClick={closePopup}>Chapter2. Headache</MenuItem>
         <MenuItem onClick={closePopup}>Chapter3. New Home</MenuItem>
