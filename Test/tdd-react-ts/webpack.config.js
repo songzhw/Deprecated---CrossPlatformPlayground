cf3ced04b3cf3fb9be8beea341d8f9b27fefd511
songzhw <songzhw2012@gmail.com>
2020-04-02 15:43:44 -0400

    error: Cannot find module 'babel-loader'

---
+++ Test/tdd-react-ts/webpack.config.js
@@ -0,0 +1,47 @@
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    carousel: "./src/index.tsx"
+  },
+  devtool: "source-map",
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "ts-loader"
+          }
+        ]
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader"
+      },
+      {
+        test: /\.js$/,
+        loader: require.resolve("babel-loader")
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({ title: "Carousel", chunks: ["carousel"] })
+  ],
+  externals: {
+    "react": "React",
+    "react-dom": "ReactDOM"
+  }
+};
