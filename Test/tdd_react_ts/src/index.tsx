723b14c820ca20e5aa8b25fbd6fdd0ea3123e056
songzhw <songzhw2012@gmail.com>
2020-04-06 14:38:46 -0400

    add files

--- Test/tdd_react_ts/src/index.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import {App} from "./App";
-
-const container = document.createElement("div");
-document.body.appendChild(container);
-ReactDOM.render(<App/>, container);
-
