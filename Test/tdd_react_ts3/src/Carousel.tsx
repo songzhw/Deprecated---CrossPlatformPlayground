c01ef12b1aa557743841b695360835ce7c42f895
songzhw <songzhw2012@gmail.com>
2020-04-07 19:22:22 -0400

    improve the UI

--- Test/tdd_react_ts3/src/Carousel.tsx
+++ Test/tdd_react_ts3/src/Carousel.tsx
@@ -2,22 +2,39 @@ import React from "react";
 import { CarouselButton } from "./CarouselButton";
 import { ISlideData } from "./data";
 import { CarouselSlide } from "./CarouselSlide";
-import "./carousel.css"
+import "./carousel.css";
 
 interface IProps {
   slides: ISlideData[]
 }
 
-export class Carousel extends React.Component<IProps> {
+interface IState {
+  index: number
+}
+
+export class Carousel extends React.Component<IProps, IState> {
+  state = { index: 0 };
+
   onNext = () => {
+    const size = this.props.slides.length;
+    this.setState(({ index }) => {
+      const pos = (index + 1) % size;
+      return { index: pos };
+    });
+
   };
   onPrev = () => {
+    const size = this.props.slides.length;
+    this.setState(({ index }) => {
+      const pos = (index - 1 + size) % size;
+      return { index: pos };
+    });
   };
 
   render() {
     return (
       <div className="carousel">
-        <CarouselSlide slide={this.props.slides[0]}/>
+        <CarouselSlide slide={this.props.slides[this.state.index]}/>
         <div className="controlBar">
           <CarouselButton className="buttons" onClick={this.onPrev}>Prev</CarouselButton>
           <CarouselButton className="buttons" onClick={this.onNext}>Next</CarouselButton>
