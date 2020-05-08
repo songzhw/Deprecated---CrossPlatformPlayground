0d0c8e7327eed31755bb56f54e20f73c4a2a93b2
songzhw <songzhw2012@gmail.com>
2020-05-08 10:20:32 -0400

    not displaying TextInput?!

--- ReactNative/RN101/src/persist/realm/RealDemo1.tsx
+++ ReactNative/RN101/src/persist/realm/RealDemo1.tsx
@@ -1,42 +1,65 @@
 import React from "react";
-import { View, StyleSheet, Text, ViewProps } from "react-native";
+import {View, StyleSheet, Text, ViewProps, TextInput} from "react-native";
 import Realm from "realm";
-import { CarSchema, OwnerSchema } from "./RealmScheme";
+import {CarSchema, OwnerSchema} from "./RealmScheme";
 
 interface IProps extends ViewProps {
 }
 
-export class RealDemo1 extends React.Component<IProps> {
-  state = { realm: null };
+interface IState {
+  realm: Realm | null
+}
+
+export class RealDemo1 extends React.Component<IProps, IState> {
+  state = {realm: null};
+  carName = ""
+  carMiles = 0
 
   componentDidMount() {
-    Realm.open({ schema: [CarSchema, OwnerSchema] })
+    Realm.open({schema: [CarSchema, OwnerSchema]})
       .then(realm => {
-        // realm.write(() => realm.create("Car", { name: "HQ" }));
-        console.log(`realm = `, Realm.defaultPath)
-        this.setState({ realm });
+        // console.log(`realm = `, Realm.defaultPath)
+        this.setState({realm});
       });
   }
 
   componentWillUnmount() {
-    const { realm } = this.state;
-    // @ts-ignore
-    if (realm && !realm.isClosed) {
-      // @ts-ignore
-      realm.close();
+    if (this.state.realm !== null) {
+      const realm = this.state.realm as any //可再优化. 这里实在是TS无法推断realm非空, 才不得已用any
+      if (!realm.isClosed) {
+        realm.close();
+      }
     }
   }
 
   render() {
     // @ts-ignore
-    const info = this.state.realm ? "Number of cars : " + this.state.realm.objects("Car").length : "Loading...";
+    const info = this.state.realm ? "Realm Database:" : "Loading...";
     return (
       <View>
-        <Text style={{ fontSize: 30 }}>{info}</Text>
+        <Text style={{fontSize: 30}}>{info}</Text>
+        <View style={styles.carContainer}>
+          <TextInput style={styles.inputs} onChangeText={text=>this.carName = text} defaultValue="car name"/>
+        </View>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  carContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: "space-around",
+    height: 100
+  },
+  inputs: {
+    width: 100,
+  }
+});
 
+/*
+1. this.state.realm.objects('Car')的值是: {"0": {"miles": 0, "name": "HQ"}}
+
+2.
+ */
