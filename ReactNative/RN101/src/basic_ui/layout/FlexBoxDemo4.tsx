a351650cfac723eec211605922590aba08e757bd
songzhw <songzhw2012@gmail.com>
2020-05-12 17:13:48 -0400

    rn layout issue

--- ReactNative/RN101/src/basic_ui/layout/FlexBoxDemo4.tsx
+++ ReactNative/RN101/src/basic_ui/layout/FlexBoxDemo4.tsx
@@ -4,9 +4,12 @@ import {View, ViewProps, Text, StyleSheet, SafeAreaView, Button} from "react-nat
 interface IProps extends ViewProps {
 }
 
+// "FlexBoxDemo4" is more of an issue to show you what it can be wrong
+// The fix for this issue is "FlexBoxDemo5"
 export const FlexBoxDemo4 = (props: IProps) => {
 
   return (
+    <SafeAreaView >
       <View style={styles.container}>
         <View style={styles.row1}>
           <Text style={styles.text}>First Line</Text>
@@ -15,14 +18,18 @@ export const FlexBoxDemo4 = (props: IProps) => {
         <View style={styles.row2}>
           <Text style={styles.text}>Second Line</Text>
         </View>
+
+        <Text style={styles.row3}>Third Third ThirdThirdThirdThirdThirdThird</Text>
       </View>
+    </SafeAreaView>
   )
 }
 
 
 const styles = StyleSheet.create({
   container: {backgroundColor: 'gray', flex: 1},
-  row1: {flexDirection: "row", flex: 1, backgroundColor: 'yellow'},
-  row2: {flexDirection: "row", flex: 1, backgroundColor: 'pink'},
+  row1: {flexDirection: "row", flex:1, backgroundColor: 'yellow'},
+  row2: {flexDirection: "row", flex:1, backgroundColor: 'pink'},
+  row3: {flexDirection: "row", flex: 1, backgroundColor: 'green'},
   text: {fontSize: 23, height: 38}
 });
