f020d17832bc34621b1db756edc7209e06a74464
songzhw <songzhw2012@gmail.com>
2020-05-25 15:24:13 -0400

    still not working

--- React/storybook_demo/src/stories/TaskView.stories.tsx
+++ React/storybook_demo/src/stories/TaskView.stories.tsx
@@ -1,9 +1,8 @@
 import { ITask, TaskState, TaskView } from "../ui/TaskView";
 import { action } from "@storybook/addon-actions";
-import { storiesOf } from "@storybook/react";
 import React from "react";
 
-const task: ITask = {
+const task = {
   id: 100,
   title: "hello world",
   state: TaskState.normal
@@ -14,7 +13,15 @@ const actions = {
   onDone: action("done")
 };
 
-storiesOf("TaskView", module)
-  .add("default", () => <TaskView task={task} {...actions}/>)
-  .add("pinned", () => <TaskView task={{ ...task, state: TaskState.pinned }} {...actions}/>)
-  .add("default", () => <TaskView task={{ ...task, state: TaskState.done }} {...actions}/>);
+// storiesOf("TaskView", module)
+//   .add("default", () => <TaskView task={task} {...actions}/>)
+//   .add("pinned", () => <TaskView task={{ ...task, state: TaskState.pinned }} {...actions}/>)
+//   .add("default", () => <TaskView task={{ ...task, state: TaskState.done }} {...actions}/>);
+
+export default {
+  title: "TaskView",
+  component: TaskView
+};
+
+export const NormalTask = () => <TaskView task={task} {...actions}/>;
+export const PinnedTask = () => <TaskView task={{ ...task, state: TaskState.pinned }} {...actions}/>
