9a02cb9ccebccfcf31120de7f85f498308295436
songzhw <songzhw2012@gmail.com>
2020-05-25 16:21:56 -0400

    this does not work, we have to add more to make sotryboard support typescript

--- React/storybook_demo/.storybook/main.js
+++ React/storybook_demo/.storybook/main.js
@@ -1,5 +1,5 @@
 module.exports = {
-  stories: ['../src/**/*.stories.js'],
+  stories: ['../src/**/*.stories.[js|tsx]'],
   addons: [
     '@storybook/preset-create-react-app',
     '@storybook/addon-actions',
