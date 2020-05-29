a62deba0bff192e9a080d069bcbf8a94768c0105
songzhw <songzhw2012@gmail.com>
2020-05-29 11:22:02 -0400

    problem solved

--- React/react101/src/Rtutor/hooks/useMemo/usememo4.tsx
+++ React/react101/src/Rtutor/hooks/useMemo/usememo4.tsx
@@ -1,19 +1,24 @@
 import React, { useCallback, useEffect, useState } from "react";
 
-/*@deprecated(因为点击parent的button, child仍会刷新)*/
+/*完美, child再也不会无谓刷新了*/
 interface IProps {
   fn: () => {};
   values: number[]
 }
 
-function MemoChild3(props: IProps) {
+function _MemoChild4(props: IProps) {
   console.log(`szw child re-render`);
   return <div> Child </div>;
 }
 
+const MemoChild4 = React.memo(_MemoChild4, (prevProps, nextProps) => {
+  // console.log("1. ", prevProps.fn === nextProps.fn)  //=> true
+  // console.log("2. ", prevProps.values === nextProps.values) //=> true
+  return prevProps.fn === nextProps.fn && prevProps.values === nextProps.values;
+});
 
 
-export const UseMemo3 = () => {
+export const UseMemo4 = () => {
   const [count, setCount] = useState(1);
   // const fn = () => ({}); // ()=>{}是指()=>void; 而()=>({})表示返回值是一个空对象{}
   const fn = React.useCallback(() => ({}), []);
@@ -25,7 +30,7 @@ export const UseMemo3 = () => {
 
   return (
     <div>
-      <MemoChild3 fn={fn} values={va}/>
+      <MemoChild4 fn={fn} values={va}/>
       <button onClick={onClick}>{count}</button>
     </div>
   );
