d29c4bb430d5c339de2edde7c1d69bcafdffbb42
songzhw <songzhw2012@gmail.com>
2020-07-19 13:34:23 -0400

    downloadImageAndSave

--- ReactNative/RN101/src/persist/fetch_blob/FilesPage.tsx
+++ ReactNative/RN101/src/persist/fetch_blob/FilesPage.tsx
@@ -11,19 +11,31 @@ export const FilesPage = (props: IProps) => {
   console.log(`DownloadDir = `, RNFetchBlob.fs.dirs.DownloadDir); //=>  /storage/emulated/0/Download
   console.log(`LibraryDir = `, RNFetchBlob.fs.dirs.LibraryDir); //=>  undefined
 
-  const download = () => {
+  const downloadHtml = () => {
     const url = "https://songzhw.github.io/repo/index.html";
     RNFetchBlob.fetch("GET", url)
       .then((res) => {
-        console.log(`base64 = `, res.base64())
-        console.log(`text = `, res.text())
+        console.log(`base64 = `, res.base64());
+        console.log(`text = `, res.text());
         // console.log(`json = `, res.json()) // html不是json格式, 所以这行会报错的
+      });
+  };
+
+  const downloadImageAndSave = () => {
+    const url = "https://songzhw.github.io/repo/android.png";
+    const path = RNFetchBlob.fs.dirs.DocumentDir+"/downloads/my.png"
+    RNFetchBlob
+      .config({path: path})
+      .fetch("GET", url)
+      .then((res) => {
+        console.log(`path = `, res.path()); //=> /data/user/0/com.rn101/files/downloads/my.png
       });
   };
 
   return (
     <View>
-      <Button title="download" onPress={download}/>
+      <Button title="download html" onPress={downloadHtml}/>
+      <Button title="download image" onPress={downloadImageAndSave}/>
     </View>
   );
 };
