5b8be381520d36d4dcb2873b715607a21b59c332
songzhw <songzhw2012@gmail.com>
2020-07-20 15:56:38 -0400

    now it's working

--- ReactNative/RN101/src/biz.server/BridgeDemo.tsx
+++ ReactNative/RN101/src/biz.server/BridgeDemo.tsx
@@ -8,10 +8,15 @@ interface IProps extends ViewProps {
 
 export const BridgeDemo = (props: IProps) => {
 
+/*
+[Mon Jul 20 2020 15:55:56.635] szw intercept!!! : url =  /
+[Mon Jul 20 2020 15:55:56.825] szw intercept!!! : url =  /favicon.ico
+ */
   useEffect(() => {
     HttpBridge.start(8321, "szw1", request => {
       console.log(`szw intercept!!! : url = `, request.url);
-      HttpBridge.respond(request.requestId, 200, "application/text", "<html><body><h1>hello world</h1></body></html>");
+      // HttpBridge.respond(request.requestId, 200, "application/text", "<html><body><h1>hello world</h1></body></html>");
+      HttpBridge.respond(request.requestId, 200, "application/json", `{"message":"ok","id":200}`);
     });
 
     return () => HttpBridge.stop();
