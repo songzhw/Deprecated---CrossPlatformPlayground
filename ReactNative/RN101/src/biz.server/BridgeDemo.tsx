ae942def20f1b05aad3c91dc71395d1fdbf6d053
songzhw <songzhw2012@gmail.com>
2020-07-21 09:14:04 -0400

    now it seems work

--- ReactNative/RN101/src/biz.server/BridgeDemo.tsx
+++ ReactNative/RN101/src/biz.server/BridgeDemo.tsx
@@ -1,54 +1,57 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { View, ViewProps, Text, StyleSheet, Button } from "react-native";
 import { WebView } from "react-native-webview";
 import HttpBridge from "react-native-http-bridge";
 
-export class BridgeDemo extends React.Component {
+export const BridgeDemo = () => {
   /*
   [Mon Jul 20 2020 15:55:56.635] szw intercept!!! : url =  /
   [Mon Jul 20 2020 15:55:56.825] szw intercept!!! : url =  /favicon.ico
    */
 
-  state = { url: "https://www.google.com" };
+  const [url, setUrl] = useState("https://www.google.com");
 
-  componentWillUnmount() {
-    this.stop();
-  }
+  useEffect(()=>{
+    console.log(`szw component unmount`)
+    return stop()
+  }, [])
 
-  start = () => {
+  const start = () => {
     console.log(`szw start server => `);
     HttpBridge.start(8321, "szw1", request => {
-      console.log(`szw intercept!!! : url = `, request.url);
+      console.log(`szw intercept!!! : url = `, request.url, "; id = "+request.requestId);
       HttpBridge.respond(request.requestId, 200, "text/html", "<html><body><h1>hello world</h1></body></html>");
       // HttpBridge.respond(request.requestId, 200, "application/json", `{"message":"ok","id":200}`);
     });
-    this.setState({ url: "http://localhost:8321" });
+    setUrl("http://localhost:8321");
   };
 
-  stop = () => {
+  const stop = () => {
     console.log(`szw stop server`);
     HttpBridge.stop();
-    this.google()
+    google();
   };
 
-  google = () => {
-    this.setState({ url: "https://www.google.com" });
+  const google = () => {
+    setUrl("https://www.google.com");
   };
 
-  render() {
-    return (
-      <View style={{ flex: 1 }}>
-        <View style={{height: 10}}/>
-        <Button title="start server" onPress={this.start}/>
-        <View style={{height: 10}}/>
-        <Button title="stop server" onPress={this.stop}/>
-        <WebView
-          source={{ uri: this.state.url }}
-          style={{ flex: 1 }}
-        />
-      </View>
-    );
+  const mine = ()=>{
+    setUrl("http://localhost:8321");
   }
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View style={{ height: 10 }}/>
+      <Button title="start server" onPress={start}/>
+      <View style={{ height: 10 }}/>
+      <Button title="stop server" onPress={stop}/>
+      <WebView
+        source={{ uri: url }}
+        style={{ flex: 1 }}
+      />
+    </View>
+  );
 };
 
 
