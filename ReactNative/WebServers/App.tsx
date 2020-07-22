53553943fc801b5b3ea71fcad0f83ae680bc99cb
songzhw <songzhw2012@gmail.com>
2020-07-22 10:54:18 -0400

    add files

---
+++ ReactNative/WebServers/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  StyleSheet,
+} from 'react-native';
+
+
+declare const global: { HermesInternal: null | {} };
+
+const App = () => {
+  return (
+    <>
+
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
