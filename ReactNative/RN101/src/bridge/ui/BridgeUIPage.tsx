de7ed6905dbbc2e53edaace8153278b8ec42b7a8
songzhw <songzhw2012@gmail.com>
2020-08-17 10:58:44 -0400

    okay, this is the correct to ignore error

--- ReactNative/RN101/src/bridge/ui/BridgeUIPage.tsx
+++ ReactNative/RN101/src/bridge/ui/BridgeUIPage.tsx
@@ -2,19 +2,21 @@ import React from "react";
 import { View, ViewProps, Text, StyleSheet, SafeAreaView } from "react-native";
 import { RippleBgView } from "../bridges";
 
-interface IProps extends ViewProps  {}
+interface IProps extends ViewProps {
+}
 
 export const BridgeUIPage = (props: IProps) => {
 
+
   return (
-    <SafeAreaView>
-      <RippleBgView
-        style={{width:140, height: 450, backgroundColor:"black"}}
-        stretchMode=""
+    <SafeAreaView style={{ flex: 1 }}>
+      {/*@ts-ignore*/}
+      <RippleBgView style={{ flex: 1, backgroundColor: "black" }}
+                    stretchMode=""
       />
     </SafeAreaView>
-  )
-}
+  );
+};
 
 
 const styles = StyleSheet.create({
