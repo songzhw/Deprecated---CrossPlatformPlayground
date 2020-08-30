764f25a42973226d971717622db08467cb922c5d
songzhw <songzhw2012@gmail.com>
2020-08-29 21:33:03 -0400

    add anim for ring

--- ReactNative/RN101/src/biz/svg/SvgAnimDemo2.tsx
+++ ReactNative/RN101/src/biz/svg/SvgAnimDemo2.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { View, ViewProps, Text, StyleSheet, Button, Animated } from "react-native";
+import React, { useState } from "react";
+import { View, ViewProps, Text, StyleSheet, Button, Animated, Easing } from "react-native";
 import Svg, { Circle } from "react-native-svg";
 
 interface IProps extends ViewProps {
@@ -10,13 +10,12 @@ const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 export const SvgAnimDemo2 = (props: IProps) => {
   const radius = 65;
   const circumference = 2 * radius * Math.PI;  // 总周长
+  const [progress, setProgress] = useState(new Animated.Value(0));  // 倒计时动画进度
 
   const startAnim = () => {
-
   };
 
   const resetAnim = () => {
-
   };
 
   return (
@@ -25,15 +24,26 @@ export const SvgAnimDemo2 = (props: IProps) => {
       <View style={{ width: 1, height: 10 }}/>
       <Button title={"reset anim"} onPress={resetAnim}/>
 
-      <Svg width={360} height={360} style={{ backgroundColor: "gray" }}>
+      <Svg width={360} height={560} style={{ backgroundColor: "gray" }}>
         <Circle
           x={80} y={80} r={radius}
           fill="green"/>
         <Circle
           x={250} y={80} r={radius}
           fill="green" stroke="red" strokeWidth={5}
-          strokeDasharray={"20 6"} strokeDashoffset={-200}/>
+          strokeDasharray={"20 6"}/>
 
+        <Circle
+          x={80} y={240} r={radius}
+          fill="green" stroke="red" strokeWidth={5}
+          strokeDasharray={`${circumference} ${circumference}`}
+          strokeDashoffset={-140}/>
+
+        <Circle
+          x={240} y={240} r={radius}
+          fill="green" stroke="red" strokeWidth={5}
+          strokeDasharray={`${circumference} ${circumference}`}
+          strokeDashoffset={-140} rotation={270}/>
 
       </Svg>
 
