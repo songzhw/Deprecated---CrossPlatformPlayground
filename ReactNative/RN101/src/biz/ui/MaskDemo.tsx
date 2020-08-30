03c7571159d20035d8e9e55cf830a8207bc00a4d
songzhw <songzhw2012@gmail.com>
2020-08-30 16:09:54 -0400

    mask

--- ReactNative/RN101/src/biz/ui/MaskDemo.tsx
+++ ReactNative/RN101/src/biz/ui/MaskDemo.tsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 import MaskedView from "@react-native-community/masked-view";
 
+// MaskedView本质上就是Android中一个View, 用DST_IN来显示布局(两层交集处, 取下层)
 export class MaskDemo extends React.Component {
 
   maskElement = <View style={styles.wrapper}>
@@ -19,12 +20,12 @@ export class MaskDemo extends React.Component {
           <View style={{ flex: 1, height: "100%", backgroundColor: "#e1e1e1" }}/>
         </MaskedView>
 
-        <MaskedView maskElement={this.maskElement} style={styles.maskRoot}>
+        <MaskedView maskElement={<Image source={require("../../../res/img/ic_star_full.png")} style={{width:"100%", height:"100%"}}/>} style={styles.maskRoot}>
           {/* Shows behind the mask, you can put anything here, such as an image */}
-          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
-          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
-          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
-          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#324376" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#F5DD90" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#F76C5E" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#e1e1e1" }}/>
         </MaskedView>
 
         <MaskedView maskElement={this.maskElement} style={styles.maskRoot}>
