8831f400cb892cb173af44222aee66890ba0f74b
songzhw <songzhw2012@gmail.com>
2020-08-30 15:41:05 -0400

    fail to show mask text

--- ReactNative/RN101/src/biz/ui/MaskDemo.tsx
+++ ReactNative/RN101/src/biz/ui/MaskDemo.tsx
@@ -10,19 +10,40 @@ export class MaskDemo extends React.Component {
 
   render() {
     return (
-      <MaskedView maskElement={this.maskElement} style={styles.maskRoot}>
-        {/* Shows behind the mask, you can put anything here, such as an image */}
-        <View style={{ flex: 1, height: "100%", backgroundColor: "#324376" }}/>
-        <View style={{ flex: 1, height: "100%", backgroundColor: "#F5DD90" }}/>
-        <View style={{ flex: 1, height: "100%", backgroundColor: "#F76C5E" }}/>
-        <View style={{ flex: 1, height: "100%", backgroundColor: "#e1e1e1" }}/>
-      </MaskedView>
+      <View style={styles.root}>
+        <MaskedView maskElement={this.maskElement} style={styles.maskRoot}>
+          {/* Shows behind the mask, you can put anything here, such as an image */}
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#324376" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#F5DD90" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#F76C5E" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#e1e1e1" }}/>
+        </MaskedView>
+
+        <MaskedView maskElement={this.maskElement} style={styles.maskRoot}>
+          {/* Shows behind the mask, you can put anything here, such as an image */}
+          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
+          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
+          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
+          <Text style={{ flex: 1, height: "100%"}}>what a fun day</Text>
+        </MaskedView>
+
+        <MaskedView maskElement={this.maskElement} style={styles.maskRoot}>
+          {/* Shows behind the mask, you can put anything here, such as an image */}
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#324376" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#F5DD90" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#F76C5E" }}/>
+          <View style={{ flex: 1, height: "100%", backgroundColor: "#e1e1e1" }}/>
+        </MaskedView>
+      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  maskRoot: { flex: 1, flexDirection: "row", height: "100%",  },
+  root: {
+    flex: 1
+  },
+  maskRoot: { flex: 1, flexDirection: "row", height: "100%" },
   wrapper: {
     // Transparent background because mask is based off alpha channel.
     backgroundColor: "transparent",
