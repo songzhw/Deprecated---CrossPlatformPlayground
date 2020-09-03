f764eec7a36b60fafe17d121578c284ede3ecd18
songzhw <songzhw2012@gmail.com>
2020-09-03 11:47:42 -0400

    text居中

--- ReactNative/RN101/src/biz/svg/SvgCanvas5Pie.tsx
+++ ReactNative/RN101/src/biz/svg/SvgCanvas5Pie.tsx
@@ -9,14 +9,17 @@ export const SvgCanvas5Pie = (props: IProps) => {
 
   return (
     <View>
-      <Svg>
+      <Svg width={200} height={200} style={{backgroundColor: "gray"}}>
         <Defs>
           <Circle id="shape" cx="100" cy="100" r="80" fill="none" strokeWidth={28}/>
         </Defs>
 
         {/*越长的环, 越要在下方; 要是在上亠, 就会遮住其它环*/}
+        <Use href="#shape" x={0} y={0} stroke={"green"} strokeDasharray="600 3000"/>
+        <Use href="#shape" x={0} y={0} stroke={"red"} strokeDasharray="210 3000"/>
+        <Use href="#shape" x={0} y={0} stroke={"blue"} strokeDasharray="100 3000"/>
 
-        <Text x="150" y="50" fontSize={40}>Pie</Text>
+        <Text x="50%" y="50%" fontSize={40} fill={"blue"} textAnchor={"middle"}>Pie</Text>
       </Svg>
     </View>
   );
