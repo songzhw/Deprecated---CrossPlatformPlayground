11fd5236bae0c7fdf5bbc349f22cb844522675eb
songzhw <songzhw2012@gmail.com>
2020-09-03 11:45:44 -0400

    no text at all: because "fill" is null

--- ReactNative/RN101/src/biz/svg/SvgCanvas5Pie.tsx
+++ ReactNative/RN101/src/biz/svg/SvgCanvas5Pie.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
-import Svg, { Circle, Defs, Use } from "react-native-svg";
+import { View, ViewProps, StyleSheet } from "react-native";
+import Svg, { Circle, Defs, Use, Text } from "react-native-svg";
 
 interface IProps extends ViewProps {
 }
@@ -11,14 +11,12 @@ export const SvgCanvas5Pie = (props: IProps) => {
     <View>
       <Svg>
         <Defs>
-          <Circle id="shape" cx="50" cy="50" r="80" fill="none" strokeWidth={28}/>
+          <Circle id="shape" cx="100" cy="100" r="80" fill="none" strokeWidth={28}/>
         </Defs>
 
         {/*越长的环, 越要在下方; 要是在上亠, 就会遮住其它环*/}
-        <Use href="#shape" x={90} y={90} stroke={"green"} strokeDasharray="600 3000"/>
-        <Use href="#shape" x={90} y={90} stroke={"red"} strokeDasharray="210 3000"/>
-        <Use href="#shape" x={90} y={90} stroke={"blue"} strokeDasharray="100 3000"/>
 
+        <Text x="150" y="50" fontSize={40}>Pie</Text>
       </Svg>
     </View>
   );
