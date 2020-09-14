c0d9e38e074dc025361477130ad5c814df137f87
songzhw <songzhw2012@gmail.com>
2020-09-14 09:40:02 -0400

    refactor

--- ReactNative/RN101/src/biz/ui/BadgerDemo.tsx
+++ ReactNative/RN101/src/biz/ui/BadgerDemo.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
-import { Triangle } from "../../ui/badger/Triangle";
 
-const hyp = (a:number, b: number) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
 export const BadgerDemo = () => {
 
   return (
     <View>
-      <Triangle width={150} height={150} color={"red"}/>
       <View style={{position: "absolute", top: 50, left: -50,
         transform: [{rotate: "145deg"}],
         backgroundColor: "blue", width: 100, height: 100}}/>
---
+++ ReactNative/RN101/src/ui/badger/BadgerPosition.ts
@@ -0,0 +1,6 @@
+export enum BadgerOrientation {
+  TOP_LEFT = "Badger.Orientation.TopLeft",
+  TOP_RIGHT = "Badger.Orientation.TopRight",
+  BOTTOM_LEFT = "Badger.Orientation.BottomLeft",
+  BOTTOM_RIGHT = "Badger.Orientation.BottomRight"
+}
