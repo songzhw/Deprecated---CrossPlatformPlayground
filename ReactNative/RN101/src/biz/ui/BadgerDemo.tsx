e9cfadc39060804dddf3cbc6ab7b57498a15b38b
songzhw <songzhw2012@gmail.com>
2020-09-14 09:09:05 -0400

    rotation style

--- ReactNative/RN101/src/biz/ui/BadgerDemo.tsx
+++ ReactNative/RN101/src/biz/ui/BadgerDemo.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
 import { Triangle } from "../../ui/badger/Triangle";
 
-interface IProps extends ViewProps {
-}
+const hyp = (a:number, b: number) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
 
-export const BadgerDemo = (props: IProps) => {
+export const BadgerDemo = () => {
 
   return (
-    <View style={styles.container}>
+    <View>
       <Triangle width={150} height={150} color={"red"}/>
+      <View style={{position: "absolute", top: 50, left: -50,
+        transform: [{rotate: "145deg"}],
+        backgroundColor: "blue", width: 100, height: 100}}/>
     </View>
   );
 };
