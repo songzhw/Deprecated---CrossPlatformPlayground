f8e1137c21df8caf620ce515ab89c3209e6ac3f5
songzhw <songzhw2012@gmail.com>
2020-09-15 10:48:24 -0400

    add transform

--- ReactNative/RN101/src/ui/badger/BadgerBox.tsx
+++ ReactNative/RN101/src/ui/badger/BadgerBox.tsx
@@ -1,24 +1,28 @@
 import React, { ReactNode } from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
 
+const hyp = (a: number, b:number) => Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2));
+
 interface IProps extends ViewProps {
   // children?: ReactNode //太全面了, 我现在只要一个就行了
   children: Element
 }
 
 export const BadgerBox = (props: IProps) => {
-
+  console.log(`size1 = `, hyp(320, 200))
+  console.log(`size2 = `, hyp(160, 100))
   return (
-    <View>
+    <View style={styles.container}>
       {props.children}
-      <Text testID="label" style={[styles.label, styles.labelText]}>New</Text>
+      <Text testID="label" style={[styles.label, styles.labelText, styles.labelAnim]}>New</Text>
     </View>
   );
 };
 
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+  },
   label: {
     position: "absolute",
     top: 0,
@@ -31,6 +35,14 @@ const styles = StyleSheet.create({
     color: "white",
     fontSize: 20,
     textAlign: "center", /*水平居中*/
-    lineHeight: 50, /*垂直居中. 主要复用了lineHeight与height同样高度*/
-   }
+    lineHeight: 50 /*垂直居中. 主要复用了lineHeight与height同样高度*/
+  },
+  labelAnim: {
+    transform: [
+      { translateX: 160 },
+      { translateY: -100 },
+      { rotate: "45deg" },
+      { translateY: 130 }
+    ]
+  }
 });
