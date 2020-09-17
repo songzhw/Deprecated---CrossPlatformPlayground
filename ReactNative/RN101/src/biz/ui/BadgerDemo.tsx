0454d73d83d0086639236b1db4c2943a27127aa5
songzhw <songzhw2012@gmail.com>
2020-09-17 17:23:47 -0400

    show more exampels

--- ReactNative/RN101/src/biz/ui/BadgerDemo.tsx
+++ ReactNative/RN101/src/biz/ui/BadgerDemo.tsx
@@ -2,20 +2,40 @@ import React from "react";
 import { View, ViewProps, Text, StyleSheet, Image } from "react-native";
 import { BadgerBox } from "../../ui/badger/BadgerBox";
 
-const width = 240;
-const height = 110;
+const width = 320;
+const height = 160;
 
 export const BadgerDemo = () => {
 
   return (
     <View style={styles.container}>
       <BadgerBox width={width} height={height} gap={55}
-                 labelBg={"orange"} labelTextColor={"blue"}
+                 labelBg={"orange"} labelTextColor={"black"}
                  labelTextHeight={26} labelTextSize={16}
                  labelText="Hot!">
         <Image source={require("../../../res/img/Daimond.jpg")}
                style={styles.image}/>
       </BadgerBox>
+
+      <View style={{height:30}}/>
+      <BadgerBox width={260} height={100} gap={55}
+                 labelBg={"red"} labelTextColor={"white"}
+                 labelTextHeight={26} labelTextSize={16}
+                 labelText="new">
+        <Image source={require("../../../res/img/Daimond.jpg")}
+               style={{width:260, height:100}}/>
+      </BadgerBox>
+
+      <View style={{height:30}}/>
+      <BadgerBox width={160} height={70} gap={29}
+                 labelBg={"green"} labelTextColor={"red"}
+                 labelTextHeight={20} labelTextSize={14}
+                 labelText="new">
+        <Image source={require("../../../res/img/Daimond.jpg")}
+               style={{width:160, height:70}}/>
+      </BadgerBox>
+
+
     </View>
   );
 };
