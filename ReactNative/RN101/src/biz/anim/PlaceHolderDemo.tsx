ab080aa6bd869c3fa90a286b4ee7a3e28fb2accf
songzhw <songzhw2012@gmail.com>
2020-09-18 15:38:08 -0400

    add clipPath

--- ReactNative/RN101/src/biz/anim/PlaceHolderDemo.tsx
+++ ReactNative/RN101/src/biz/anim/PlaceHolderDemo.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import Svg, { Circle, Rect } from "react-native-svg";
+import Svg, { Circle, ClipPath, Defs, Rect } from "react-native-svg";
 
 export const PlaceHolderDemo = () => {
+  const color = "blue";
+
   return (
     <View style={styles.container}>
-      <Svg height={200} width={240} style={{backgroundColor: "#0001"}}>
-        <Rect x={10} y={10} width={60} height={20} rx={9} ry={9} fill={"gray"}/>
-        <Rect x={80} y={10} width={80} height={20} rx={9} ry={9} fill={"gray"}/>
-        <Circle x={180} y={20} r={10} fill={"gray"}/>
-        <Rect x={30} y={40} width={70} height={20} rx={9} ry={9} fill={"gray"}/>
-        <Rect x={110} y={40} width={120} height={20} rx={9} ry={9} fill={"gray"}/>
+      <Svg width={240} height={200} style={{ backgroundColor: "#0001" }}>
+        <Defs>
+          <ClipPath id="clip">
+            <Rect x={10} y={10} width={60} height={20} rx={9} ry={9} fill={color}/>
+            <Rect x={80} y={10} width={80} height={20} rx={9} ry={9} fill={color}/>
+            <Circle x={180} y={20} r={10} fill={color}/>
+            <Rect x={30} y={40} width={70} height={20} rx={9} ry={9} fill={color}/>
+            <Rect x={110} y={40} width={120} height={20} rx={9} ry={9} fill={color}/>
+          </ClipPath>
+        </Defs>
+
+        <Rect x={0} y={0} width={240} height={200}  clipPath="url(#clip)" fill={"red"}/>
       </Svg>
     </View>
   );
