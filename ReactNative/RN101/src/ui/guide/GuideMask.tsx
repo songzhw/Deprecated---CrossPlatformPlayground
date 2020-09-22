6d09410b15f2050fc3b1ed351901ac2ebe1cdf69
songzhw <songzhw2012@gmail.com>
2020-09-22 16:35:28 -0400

    圆都拉伸了, 确实不好看

--- ReactNative/RN101/src/ui/guide/GuideMask.tsx
+++ ReactNative/RN101/src/ui/guide/GuideMask.tsx
@@ -10,14 +10,14 @@ export const GuideMask = (props: IProps) => {
   // style={{backgroundColor: "blue"}}
   return (
     <View style={{ position: "absolute", top: 0, right: 0, left: 0, bottom: 0 }}>
-      <Svg height="100%" width="100%" viewBox="0 0 100 100" preserveAspectRatio={"xMaxYMax"}>
+      <Svg height="100%" width="100%" viewBox="0 0 100 100" preserveAspectRatio={"none"}>
         <Defs>
           <Mask id="mask" >
             <Circle r="45" cx="50" cy="50" fill="red"/>
           </Mask>
         </Defs>
         <Circle r={20} cx={0} cy={0} fill={"red"}/>
-        <Rect x={0} y={0} height="100%" width="100%" fill="#0007"  />
+        <Rect x={0} y={0} height="100%" width="100%" fill="#0007"  mask="url(#mask)"/>
       </Svg>
     </View>
   );
