00db8d338229ee9833333d66c06ed66bc1176b06
songzhw <songzhw2012@gmail.com>
2020-09-23 11:09:21 -0400

    compare to clippath

--- ReactNative/RN101/src/biz/svg/SvgCanvasDemo6.tsx
+++ ReactNative/RN101/src/biz/svg/SvgCanvasDemo6.tsx
@@ -1,34 +1,37 @@
 import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
-import Svg, { Defs, Mask, Rect } from "react-native-svg";
+import Svg, { ClipPath, Defs, Mask, Rect } from "react-native-svg";
 
-interface IProps extends ViewProps {
-}
 
-export const SvgCanvasDemo6 = (props: IProps) => {
+export const SvgCanvasDemo6 = () => {
 
   return (
     <View >
-      <Svg style={{backgroundColor: "gray"}}>
+      <Svg >
         <Defs>
           <Mask id="mask1">
             <Rect x={0} y={0} width={50} height={50} fill="#fff"/>
           </Mask>
           <Mask id="mask2">
-            <Rect x={0} y={0} width={50} height={50} fill="#aaa"/>
+            <Rect  x={50} y={0} width={50} height={50} fill="#aaa"/>
           </Mask>
-          <Mask id="mask3"  x={0} y={0}  height="100%" width="100%">
-            <Rect x={0} y={0} width={50} height={50} fill="white"/>
+          <Mask id="mask3">
+            <Rect x={100} y={0} width={50} height={50} fill="#888"/>
           </Mask>
           <Mask id="mask4">
-            <Rect x={0} y={0} width={50} height={50} fill="#222"/>
+            <Rect  x={150} y={0} width={50} height={50} fill="#222"/>
           </Mask>
+          <ClipPath id="mask5">
+            <Rect  x={200} y={0} width={50} height={50} fill="#111"/>
+          </ClipPath>
         </Defs>
 
-        <Rect x={50} y={30} width={50} height={50} fill="#f00" mask="url(#mask1)"  />
-        <Rect x={100} y={30} width={50} height={50} fill="#f00" />
-        <Rect x={150} y={30} width={50} height={50} fill="#f00" mask="url(#mask3)"/>
-        <Rect x={200} y={30} width={50} height={50} fill="#f00" />
+        <Rect x={0} y={0}   width={50} height={50} fill="#f00" mask="url(#mask1)"  />
+        <Rect x={50} y={0}  width={50} height={50} fill="#f00" mask="url(#mask2)"/>
+        <Rect x={100} y={0} width={50} height={50} fill="#f00" mask="url(#mask3)"/>
+        <Rect x={150} y={0} width={50} height={50} fill="#f00" mask="url(#mask4)"/>
+        {/*从这里看出Mask与ClipPath的区别!*/}
+        <Rect x={200} y={0} width={50} height={50} fill="#f00" mask="url(#mask5)"/>
 
       </Svg>
     </View>
