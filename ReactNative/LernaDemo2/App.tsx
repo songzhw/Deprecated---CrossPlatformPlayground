71028c5b194f58e00cf54bbe549cd56bec336c34
songzhw <songzhw2012@gmail.com>
2020-10-05 16:51:46 -0400

    add another pacakge

--- ReactNative/LernaDemo2/App.tsx
+++ ReactNative/LernaDemo2/App.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 import { SafeAreaView, Text, StatusBar } from "react-native";
 import { add } from "@six/calculator";
+import { multiple } from "./src/cal2";
 
 const App = () => {
   const value = add(10, 31);
+  const value2 = multiple(3, 5);
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        <Text>Hello World {value}</Text>
+        <Text>10 + 31 = {value}</Text>
+        <Text>3 x 5 = {value2}</Text>
       </SafeAreaView>
     </>
   );
