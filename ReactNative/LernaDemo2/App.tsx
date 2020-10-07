34c3c5a6ac6780f377afc508420ae93fe54d8bec
songzhw <songzhw2012@gmail.com>
2020-10-06 21:39:05 -0400

    error: "error: Error: While trying to resolve module `@six/chat` from file `/Users/songzhw/code/CrossPlatformPlayground/ReactNative/LernaDemo2/App.tsx`, the package `/Users/songzhw/code/CrossPlatformPlayground/ReactNative/LernaDemo2/packages/chat/package.json` was successfully found. However, this package itself specifies a `main` module field that could not be resolved (`/Users/songzhw/code/CrossPlatformPlayground/ReactNative/LernaDemo2/packages/chat/lib/commonjs/index`. Indeed, none of these files exist: "

--- ReactNative/LernaDemo2/App.tsx
+++ ReactNative/LernaDemo2/App.tsx
@@ -2,10 +2,15 @@ import React from "react";
 import { SafeAreaView, Text, StatusBar } from "react-native";
 import { add } from "@six/calculator";
 import { multiple } from "./src/cal2";
+import Chat from "@six/chat";
 
 const App = () => {
   const value = add(10, 31);
   const value2 = multiple(3, 5);
+
+  Chat.multiply(20, 30)
+    .then(ret => console.log(`szw promise = ${ret}`));
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
