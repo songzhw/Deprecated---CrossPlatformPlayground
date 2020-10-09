ff94933185b48c51fb88683f17ceca7363b6cd73
songzhw <songzhw2012@gmail.com>
2020-10-09 11:33:46 -0400

    refactor

---
+++ ReactNative/RN101/src/ui/svg/ImageAvatar.tsx
@@ -0,0 +1,27 @@
+import { ViewProps } from "react-native";
+import * as ReactNative from "react-native";
+import Svg, { Circle, ClipPath, Defs, Image } from "react-native-svg";
+import React from "react";
+
+interface IProps extends ViewProps {
+  size: number;
+  href?: ReactNative.ImageProps['source'];
+}
+
+export const ImageAvatar = (props: IProps) => {
+  const {size} = props;
+  const radius = size/2
+  return (
+    <Svg width={size} height={size}>
+      <Defs>
+        <ClipPath id="clip">
+          <Circle cx={radius} cy={radius} r={radius}/>
+        </ClipPath>
+      </Defs>
+      <Image href={props.href} preserveAspectRatio="xMidYMid slice"
+             width={size} height={size}
+             clipPath="url(#clip)"
+      />
+    </Svg>
+  );
+};
