ee4469fa65a30d116e4c3698016e6e09e45e2b55
songzhw <songzhw2012@gmail.com>
2020-10-11 17:04:10 -0400

    another way to align text

--- ReactNative/RN101/src/biz/svg/SvgCanvasDemo10_GradientButton.tsx
+++ ReactNative/RN101/src/biz/svg/SvgCanvasDemo10_GradientButton.tsx
@@ -1,13 +1,14 @@
 import React from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
-import Svg, { Defs, LinearGradient, Rect, Stop } from "react-native-svg";
+import { View, ViewProps, StyleSheet, Text } from "react-native";
+import Svg, { Defs, LinearGradient, Rect, Stop, ForeignObject, Text as SvgText } from "react-native-svg";
 
 
 export const SvgCanvasDemo10_GradientButton = () => {
 
   return (
-    <View style={{flex:1, alignItems:"center", justifyContent: "center"}}>
-      <LGButton startColor="yellow" endColor="red" width={200} height={90} roundCornerRadius={8}/>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <LGButton startColor="yellow" endColor="red" width={200} height={90} roundCornerRadius={8}
+                textColor="white" fontSize={42} text="Log In"/>
     </View>
   );
 };
@@ -19,9 +20,12 @@ interface IProps extends ViewProps {
   width: number;
   height: number;
   roundCornerRadius: number;
+  textColor: string;
+  fontSize: number;
+  text: string;
 }
 
-/* Linear Graident Button */
+/* Linear Gradient Button */
 export const LGButton = (props: IProps) => {
   return (
     <Svg width={props.width} height={props.height}>
@@ -33,6 +37,15 @@ export const LGButton = (props: IProps) => {
       </Defs>
       <Rect width={props.width} height={props.height} rx={props.roundCornerRadius} ry={props.roundCornerRadius}
             fill="url(#lg)"/>
+      <SvgText x="50%" y="50%"
+            fill={"white"} fontSize={12}
+            textAnchor={"middle"} alignmentBaseline={"middle"}>{props.text}</SvgText>
+
+      {/*<ForeignObject x={0} y={0} width={props.width} height={props.height}>*/}
+      {/*  <View style={{ width:props.width, height:props.height, alignItems: "center", justifyContent: "center"}}>*/}
+      {/*    <Text style={{fontSize: 12, color: "black"}}>Log In</Text>*/}
+      {/*  </View>*/}
+      {/*</ForeignObject>*/}
     </Svg>
   );
 };
