119fa23a5c916de774ab074d14fce615e566e62d
songzhw <songzhw2012@gmail.com>
2020-10-16 11:32:57 -0400

    svg text coordiante

--- ReactNative/RN101/src/biz/svg/SvgCanvas12_Text.tsx
+++ ReactNative/RN101/src/biz/svg/SvgCanvas12_Text.tsx
@@ -9,6 +9,7 @@ export const SvgCanvas12_Text = (props: IProps) => {
   return (
     <Svg style={{backgroundColor: "gray"}}>
       <Text x={0} y={0} fill="red">Invisible Font (xyz)</Text>
+      <Text x="0,20,40,60,70" y="0,20,40,60,70" fill="red">This xyz poly</Text>
     </Svg>
   )
 }
