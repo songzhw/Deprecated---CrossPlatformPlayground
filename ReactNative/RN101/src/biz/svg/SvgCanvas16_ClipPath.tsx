55f116b477b3c73471009ca0646d88c5e4ea645a
songzhw <songzhw2012@gmail.com>
2020-10-23 18:28:32 -0400

    clipath

--- ReactNative/RN101/src/biz/svg/SvgCanvas16_ClipPath.tsx
+++ ReactNative/RN101/src/biz/svg/SvgCanvas16_ClipPath.tsx
@@ -1,22 +1,27 @@
 import React from "react";
 import { View, ViewProps, Text, StyleSheet } from "react-native";
-import Svg, { Circle, ClipPath, Defs, Rect } from "react-native-svg";
+import Svg, { Circle, ClipPath, Defs, Path, Rect, Use } from "react-native-svg";
 
-interface IProps extends ViewProps {
-}
+// TODO 真的没有看出这个clipPath的clipRule有什么不同
+export const SvgCanvas16_ClipPath = () => {
 
-export const SvgCanvas16_ClipPath = (props: IProps) => {
-
+  // Defs中的Path本身是一个五角形
   return (
-    <Svg>
+    <Svg style={{marginTop: 100}}>
       <Defs>
-        <ClipPath id="clip1">
-          <Circle x={110} y={160} r={50}/>
+        <Path d="M50,0 21,90 98,35 2,35 79,90z" id="star"/>
+        <ClipPath id="emptyStar">
+          <Use href="#star" clipRule="evenodd"/>
+        </ClipPath>
+        <ClipPath id="filledStar">
+          <Use href="#star" clipRule="nonzero"/>
         </ClipPath>
       </Defs>
-      <Rect x={25} y={125} width={200} height={100} fill={"#9c6"}
-          clipPath="url(#clip1)" clipRule="nonzero"/>
-      <Rect x={25} y={125} width={200} height={100} stroke={"#9c6"}/>
+
+      <Rect width={50} height={200} x={0} y={0} fill={"#f9c"}
+            clipPath="url(#emptyStar)"/>
+      <Rect width={200} height={200} x={50} y={0} fill={"#9c6"}
+        clipPath="url(#emptyStar)"/>
     </Svg>
   );
 };
