752dd9e233b5a7182b1d6aa22d4b96aff6c03568
songzhw <songzhw2012@gmail.com>
2020-10-28 21:03:14 -0400

    the anim is now a disappear anim

--- ReactNative/RN101/src/biz/svg/SvgAnim08_check.tsx
+++ ReactNative/RN101/src/biz/svg/SvgAnim08_check.tsx
@@ -1,21 +1,31 @@
-import React from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
-import Svg, { Circle, Path } from "react-native-svg";
+import React, { useState } from "react";
+import { View, ViewProps, Text, StyleSheet, SafeAreaView, Button, Animated, Easing } from "react-native";
+import Svg, { Path } from "react-native-svg";
 
-interface IProps extends ViewProps  {}
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 
-export const SvgAnim08_check = (props: IProps) => {
+export const SvgAnim08_check = () => {
+  const [offset, setOffset] = useState(new Animated.Value(0));
+
+  function startAnim() {
+    Animated.timing(offset, {
+      toValue: 80,
+      duration: 1000,
+      useNativeDriver: true,
+      easing: Easing.linear
+    }).start();
+  }
 
   return (
-    <Svg viewBox="0 0 24 24" width={240} height={240}>
-      <Path d="M9 16.2 L4.8 12 l-1.4 1.4 L9 19 21 7 l-1.4 -1.4 z" fill={"red"}/>
-      <Circle x={9} y={16.2} r={1} fill="blue"/>
-      <Circle x={4.8} y={12} r={1} fill="yellow"/>
-      <Circle x={9} y={19} r={1} fill="green"/>
-      <Circle x={21} y={7} r={1} fill="gray"/>
-    </Svg>
-  )
-}
+    <SafeAreaView>
+      <Button title="start anim" onPress={startAnim}/>
+      <Svg viewBox="0 0 24 24" width={240} height={240}>
+        <AnimatedPath d="M9 16.2 L4.8 12 l-1.4 1.4 L9 19 21 7 l-1.4 -1.4 z" stroke={"red"} strokeWidth={1}
+              strokeDashoffset={offset} strokeDasharray="80 80"/>
+      </Svg>
+    </SafeAreaView>
+  );
+};
 
 
 const styles = StyleSheet.create({
