247deacf66c451c179eba21fb012359e6d767a82
songzhw <songzhw2012@gmail.com>
2020-11-03 17:47:49 -0500

    make the image abosulte position, so we could layout the gray bg as well

--- ReactNative/RN101/src/biz/ui/FolderDemo.tsx
+++ ReactNative/RN101/src/biz/ui/FolderDemo.tsx
@@ -5,12 +5,12 @@ import { rotateX } from "../../utils/TransformUtils";
 interface IProps extends ViewProps {
 }
 
+// const { width, height } = useWindowDimensions(); //=> 对一些1080x2160的android手机, 它的值是392.73与737.45
 const IS_ANDROID = Platform.OS === "android";
 const PERSPECTIVE = IS_ANDROID ? 25000 : 1500;
+const width = 330, height = 570, avHeight = 190;
 
 export const FolderDemo = () => {
-    // const { width, height } = useWindowDimensions(); //=> 对一些1080x2160的android手机, 它的值是392.73与737.45
-    const width = 330, height = 570, avHeight = 190;
 
     const [isAnimStart, setAnimStart] = useState(false);
     const [animValue, setAnimVlaue] = useState(new Animated.Value(0));
@@ -27,8 +27,6 @@ export const FolderDemo = () => {
 
     const startAnim = () => {
       console.log(`szw click now = ${isAnimStart}`);
-
-
       if (!isAnimStart) {
         Animated.timing(animValue, {
           toValue: -180,
@@ -47,16 +45,19 @@ export const FolderDemo = () => {
     };
 
     const animStyle = {
-      av: { width: 330, height: 570, marginTop: 50 }
+      av: { width: 330, height: 570, marginTop: 50, backfaceVisibility: "hidden", position: "absolute" }
     };
 
 
     return (
       <View style={styles.root}>
-        <Button title={"start anim"} onPress={startAnim}/>
+
+
         <Animated.View style={animStyle.av} ref={avImage}>
           <Image source={require("../../../res/img/batman.jpg")} style={styles.img}/>
         </Animated.View>
+
+        <Button title={"start anim"} onPress={startAnim}/>
       </View>
     );
   }
@@ -65,5 +66,5 @@ export const FolderDemo = () => {
 
 const styles = StyleSheet.create({
   root: { flex: 1, justifyContent: "center", alignItems: "center" },
-  img: { width: 330, height: 570 }
+  img: { width, height: height, resizeMode: "cover" }
 });
