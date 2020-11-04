b3272ee5c7b798d861eaccd5b58d632e7f682b04
songzhw <songzhw2012@gmail.com>
2020-11-04 10:07:26 -0500

    fix wrong parameters

--- ReactNative/RN101/src/biz/ui/FolderDemo.tsx
+++ ReactNative/RN101/src/biz/ui/FolderDemo.tsx
@@ -1,16 +1,19 @@
 import React, { useEffect, useRef, useState } from "react";
-import { View, ViewProps, Text, StyleSheet, Image, useWindowDimensions, Button, Animated, Platform } from "react-native";
+import { View, ViewProps, Text, StyleSheet, Image, useWindowDimensions, Button, Animated, Platform, Dimensions, PixelRatio, TouchableOpacity } from "react-native";
 import { rotateX } from "../../utils/TransformUtils";
 
 interface IProps extends ViewProps {
 }
 
-// const { width, height } = useWindowDimensions(); //=> 对一些1080x2160的android手机, 它的值是392.73与737.45
+// const { width, height } = useWindowDimensions(); //=> 对1080x2160的android手机, 它的值是392.73与737.45
 const IS_ANDROID = Platform.OS === "android";
 const PERSPECTIVE = IS_ANDROID ? 25000 : 1500;
-const width = 330, height = 570, avHeight = 190;
+const width = 330, height = 570, backHeight = 190;
 
 export const FolderDemo = () => {
+  const {height:h , width:w } = Dimensions.get('window') //=> w=392.73, h=737.45 (前面":h"是重命名)
+  const density = PixelRatio.get()  //设备密度, d=2.75
+  console.log(`szw w=${w}, h=${h}, d = ${density}`) //=> //=> 对1080x2160的android手机, w=392.73, h=737.45,d=2.75
 
     const [isAnimStart, setAnimStart] = useState(false);
     const [animValue, setAnimVlaue] = useState(new Animated.Value(0));
@@ -18,9 +21,9 @@ export const FolderDemo = () => {
     const avImage = useRef();
     const backView = useRef();
 
-    let baseline = avHeight / 6;
+    let baseline = height / 6; //backHeight是height的1/3, 这里对半折, 所以要1/6
     animValue.addListener(({ value }) => {
-      let matrix = rotateX(-1 * value, baseline, PERSPECTIVE);
+      let matrix = rotateX(-1 * value, baseline, PERSPECTIVE); //第二参为0, 就是以中间来旋转 (旋转后不会移位!)
       const transformProps = { style: { transform: [{ matrix }] } };
       // @ts-ignore
       avImage.current?.setNativeProps(transformProps);
@@ -29,7 +32,6 @@ export const FolderDemo = () => {
     });
 
     const startAnim = () => {
-      console.log(`szw click now = ${isAnimStart}`);
       if (!isAnimStart) {
         Animated.timing(animValue, {
           toValue: -180,
@@ -57,6 +59,8 @@ export const FolderDemo = () => {
     return (
       <View style={styles.root}>
 
+        {/*<Image source={require("../../../res/img/batman.jpg")} style={styles.img}/>*/}
+
         {/*纸片的背面; 灰色bg */}
         <Animated.View style={animStyle.gray} ref={backView}>
           <View style={animStyle.grayView}/>
@@ -66,7 +70,9 @@ export const FolderDemo = () => {
           <Image source={require("../../../res/img/batman.jpg")} style={styles.img}/>
         </Animated.View>
 
-        <Button title={"start anim"} onPress={startAnim}/>
+        <TouchableOpacity onPress={startAnim} style={styles.button}>
+          <Text>Start Anim</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -75,5 +81,6 @@ export const FolderDemo = () => {
 
 const styles = StyleSheet.create({
   root: { flex: 1, justifyContent: "center", alignItems: "center" },
-  img: { width, height: height, resizeMode: "cover" }
+  img: { width, height: height, resizeMode: "cover" },
+  button: {position: "absolute", left:10, top:10, width: 70, height:20, backgroundColor: "#ba68c8"}
 });
