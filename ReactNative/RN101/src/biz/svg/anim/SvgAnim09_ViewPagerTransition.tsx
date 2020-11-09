b0ca579edb063fc2422052434b898ea69fb7aa63
songzhw <songzhw2012@gmail.com>
2020-11-09 11:32:07 -0500

    add animation

--- ReactNative/RN101/src/biz/svg/anim/SvgAnim09_ViewPagerTransition.tsx
+++ ReactNative/RN101/src/biz/svg/anim/SvgAnim09_ViewPagerTransition.tsx
@@ -1,25 +1,51 @@
-import React from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { View, ViewProps, Text, StyleSheet, useWindowDimensions, Animated } from "react-native";
 import { ImageButton } from "../../../ui/button/ImageButton";
+import Svg, { Circle } from "react-native-svg";
 
-interface IProps extends ViewProps {
-}
-
-export const SvgAnim09_ViewPagerTransition = (props: IProps) => {
+export const SvgAnim09_ViewPagerTransition = () => {
+  const [r, setR] = useState(new Animated.Value(10));
+  const { width, height } = useWindowDimensions();
+  const maxSize = width > height ? width : height;
 
   const leftPressed = () => {
-    console.log(`SZW CLICK LEFT`)
   };
+
   const rightPressed = () => {
+    Animated.timing(r, {
+      toValue: maxSize,
+      duration: 500,
+      useNativeDriver: true
+    })
+      .start();
   };
+
   return (
     <View style={styles.root}>
+      <RippleBg color="#3f51b5" nextColor="#e91e63" r={r}/>
       <ImageButton size={60} source={require("../../../../res/img/ic_left.png")} onPressed={leftPressed} style={{ marginLeft: 20 }}/>
       <ImageButton size={60} source={require("../../../../res/img/ic_right.png")} onPressed={rightPressed} style={{ marginRight: 20 }}/>
     </View>
   );
 };
 
+interface IProps extends ViewProps {
+  color: string;
+  nextColor: string;
+  r: Animated.Value
+}
+
+const AnimCircle = Animated.createAnimatedComponent(Circle);
+export const RippleBg = (props: IProps) => {
+  const { color, nextColor, r } = props;
+  const { width, height } = useWindowDimensions();
+
+  return (
+    <Svg style={{ position: "absolute", left: 0, right: 0, top: 0, bottom: 0, backgroundColor: color }}>
+      <AnimCircle cx={width / 2} cy={height - 50} r={r} fill={nextColor}/>
+    </Svg>
+  );
+};
 
 const styles = StyleSheet.create({
   root: {
