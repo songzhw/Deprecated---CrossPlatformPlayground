2a5896998b704a269ef987764de873d5938494a7
songzhw <songzhw2012@gmail.com>
2020-11-10 15:03:09 -0500

    ripple

---
+++ ReactNative/RN101/src/biz/svg/anim/SvgAnim09B_ViewPager_Transition.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from "react";
+import { View, ViewProps, Text, StyleSheet, useWindowDimensions, Animated } from "react-native";
+import { ImageButton } from "../../../ui/button/ImageButton";
+import Svg, { Circle } from "react-native-svg";
+import { RippleBg } from "./RippleBg";
+
+const data = [
+  { color: "#3f51b5", next: "#e91e63" },
+  { color: "#e91e63", next: "#4caf50" },
+  { color: "#4caf50", next: "#a156c9" },
+  { color: "#a156c9", next: "#3f51b5" }
+];
+
+export const SvgAnim09B_ViewPagerTransition = () => {
+  const [r, setR] = useState(new Animated.Value(10));
+  const [index, setIndex] = useState(0);
+  const { width, height } = useWindowDimensions();
+  const maxSize = width > height ? width : height;
+
+  const leftPressed = () => {
+  };
+
+  const rightPressed = () => {
+    Animated.timing(r, {
+      toValue: maxSize,
+      duration: 250,
+      useNativeDriver: true
+    })
+      .start(() => {
+        console.log(`timing callback: index = `, index);
+        const newIndex = (index + 1) % data.length;
+        setIndex(newIndex);
+        setR(new Animated.Value(10));
+      });
+  };
+
+  const item = data[index];
+  return (
+    <View style={styles.root}>
+      <RippleBg color={item.color} nextColor={item.next} r={r}/>
+      <ImageButton size={60} source={require("../../../../res/img/ic_left.png")} onPressed={leftPressed} style={{ marginLeft: 20 }}/>
+      <ImageButton size={60} source={require("../../../../res/img/ic_right.png")} onPressed={rightPressed} style={{ marginRight: 20 }}/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1, flexDirection: "row", justifyContent: "space-between", alignItems: "center"
+  }
+});
