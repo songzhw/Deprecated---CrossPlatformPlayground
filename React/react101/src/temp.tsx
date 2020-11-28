0a38b2a48872f3c78c7fcbf5948b609decba46f5
songzhw <songzhw2012@gmail.com>
2020-11-28 11:39:47 -0500

    hoc

--- React/react101/src/temp.tsx
+++ React/react101/src/temp.tsx
@@ -1,11 +1,24 @@
-export interface ILoadingProps {
-  isLoading: boolean;
-}
+import React, { ComponentType } from "react";
+import hoistNonReactStatics from "hoist-non-react-statics";
 
-const withLoading = <P extends object>(InComponent: React.ComponentType<P>) => {
-  const loading = <div>loading...</div>;
-  return (props: P & ILoadingProps) => (
-    props.isLoading ? { loading } : <InComponent {...props}/>
-  );
-};
 
+// export interface ILoadingProps {
+//   isLoading: boolean;
+// }
+//
+// const withLoading: ComponentType = <P extends object>(InComponent: React.ComponentType<P>) => {
+//   const loading = <div>loading...</div>;
+//   return (props: P & ILoadingProps) => (
+//     props.isLoading ? { loading } : <InComponent {...props}/>
+//   );
+// };
+//
+// interface IOneProps {
+//   name?: string;
+// }
+//
+// const One = (props: IOneProps) => {
+//   return (<div>{props.name}</div>);
+// };
+//
+// export const c: ComponentType = withLoading(One);
