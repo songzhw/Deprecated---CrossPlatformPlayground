407b09fba99a20e52cda7200b1cb80273b8e3fe1
songzhw <songzhw2012@gmail.com>
2020-11-28 11:58:37 -0500

    omit

--- React/react101/src/temp.tsx
+++ React/react101/src/temp.tsx
@@ -1,10 +1,32 @@
-import React, { ComponentType } from "react";
+import React, { Component, ComponentType } from "react";
 import hoistNonReactStatics from "hoist-non-react-statics";
 
+interface IWork{
+  name: string;
+  id:number;
+}
+type One = Omit<IWork, "name">  // Omit要ts >= 3.5.1
+const one : One = {id:200}
 
 // export interface ILoadingProps {
 //   isLoading: boolean;
 // }
+//
+// export const withLoading2 = <P extends object>(InComponent: ComponentType) => {
+//   class WithLoading3 extends Component<ILoadingProps> {
+//     loading = <div>loading...</div>;
+//
+//     render() {
+//       return (
+//         this.props.isLoading ? { this.loading } : <InComponent {...this.props}/>
+//       );
+//     }
+//   }
+//
+//   return hoistNonReactStatics(withLoading2, WithLoading3);
+// };
+
+
 //
 // const withLoading: ComponentType = <P extends object>(InComponent: React.ComponentType<P>) => {
 //   const loading = <div>loading...</div>;
