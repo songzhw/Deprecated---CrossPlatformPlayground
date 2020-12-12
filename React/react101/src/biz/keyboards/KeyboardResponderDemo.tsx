6ce24e3fd3ee597beb7629b146905a344e409e2a
songzhw <songzhw2012@gmail.com>
2020-12-11 20:00:38 -0500

    this works

--- React/react101/src/biz/keyboards/KeyboardResponderDemo.tsx
+++ React/react101/src/biz/keyboards/KeyboardResponderDemo.tsx
@@ -1,34 +1,20 @@
 import React, { KeyboardEvent } from "react";
-import { ArrowKeysReact } from "./ArrowDetector";
 
 export class KeyboardResponderDemo extends React.Component {
   state = {};
 
   constructor(props: any) {
     super(props);
-    ArrowKeysReact.config({
-      left: () => {
-        console.log("left key detected.");
-      },
-      right: () => {
-        console.log("right key detected.");
-      },
-      up: () => {
-        console.log("up key detected.");
-      },
-      down: () => {
-        console.log("down key detected.");
-      }
-    });
+    document.addEventListener("keydown", this.onKeyPressed);
   }
 
-  onKeyPressed = (event: KeyboardEvent) => {
+  onKeyPressed = (event: any) => {
     console.log(`szw event : key = `, event.key, "; code = ", event.keyCode);
   };
 
   render() {
     return (
-      <div {...ArrowKeysReact.events} style={{backgroundColor: "red", display: "flex", width: "100%", height:"100%"}}>
+      <div>
         <p>keyboard</p>
       </div>
     );
