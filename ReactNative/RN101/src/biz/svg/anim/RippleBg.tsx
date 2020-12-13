058ba09db941fc30c486bdb5682d61fd12c6f584
songzhw <songzhw2012@gmail.com>
2020-12-13 10:36:34 -0500

    much much better

--- ReactNative/RN101/src/biz/svg/anim/RippleBg.tsx
+++ ReactNative/RN101/src/biz/svg/anim/RippleBg.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { View, ViewProps, Text, StyleSheet, Animated, useWindowDimensions, Dimensions } from "react-native";
 import Svg, { Circle } from "react-native-svg";
 import { rotateX } from "../../../utils/TransformUtils";
@@ -11,30 +11,39 @@ const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
 export const RippleBg = (props: IProps) => {
-  const [radius, setRadius] = useState(new Animated.Value(0.1));
-  // const [radius, setRadius] = useState(0);
-  const [strokeWidth, setStrokeWidth] = useState(10);
+  const [animValue, setAnimValue] = useState(new Animated.Value(0.1));
+  const [radius, setRadius] = useState(0);
+  const [strokeWidth, setStrokeWidth] = useState(0);
+  const lastRef = useRef(0.1)
 
   useEffect(() => {
-    // animValue.addListener(({ value }) => {
-    //   setRadius(value * windowWidth);
-    //   setStrokeWidth(value * 15);
-    // });
+    animValue.addListener(({ value }) => {
+      const last = lastRef.current
+      const bigEnoughToRunAnim = (value - last) > 0.05
+      if(bigEnoughToRunAnim) {
+        console.log(`szw : last = `, last)
+        setRadius(value * windowWidth);
+        lastRef.current = value
+      }
+
+      // setStrokeWidth(value * 15);
+    });
 
     runAnimation();
   }, []);
 
   const runAnimation = () => {
-    // animValue.setValue(0.1);
+    lastRef.current = 0
+    animValue.setValue(0.1);
 
-    Animated.timing(radius, { toValue: windowWidth, duration: 3000, useNativeDriver: true })
-      .start();
+    Animated.timing(animValue, { toValue: 1, duration: 3000, useNativeDriver: true })
+      .start(()=> runAnimation());
   };
 
   return (
     <Svg style={props.style}>
       <AnimCircle x={windowWidth / 2} y={windowHeight / 2} r={radius}
-                  strokeWidth={strokeWidth} stroke={"white"}
+                  strokeWidth={10} stroke={"white"}
       />
     </Svg>
   );
